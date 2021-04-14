c0b1ca714562b5bab03ab166e70f21b0056ecaf8
Iiro Krankka <iiro.krankka@gmail.com>
2021-04-14 18:14:50 +0300

    Add a note about using a specific Flutter version to build the website.

--- website/build.sh
+++ website/build.sh
@@ -4,7 +4,12 @@
 git clone https://github.com/flutter/flutter.git
 FLUTTER=flutter/bin/flutter
 
-# Choose the correct Flutter channel & version, enable web support
+# Choose the correct Flutter channel & version, and enable web support.
+#
+# We're using Flutter 2.0.4 on the stable channel, since that's one of
+# the versions that works fine with the SuperEditor. Newer Flutter versions
+# got rid of some of the components that the editor is currently using, so
+# it's important to lock the version down so that the build works.
 DIR=$("${FLUTTER} channel stable" >& /dev/stdout)
 (cd flutter && git checkout 2.0.4)
 $FLUTTER precache
