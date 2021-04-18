superlistapp/super_editor

commit 252fdfa27cef449c016ebe3db94dd5570a7518e4

Added support for non-English characters when entered via keyboard or alt-codes. The OS long-press popup for 'e' and 'n' not yet supported. (#95)

super_editor/lib/src/default_editor/document_keyboard_actions.dart
@@ -379,7 +379,7 @@ ExecutionInstruction deleteExpandedSelectionWhenCharacterOrDestructiveKeyPressed
 
   final isDestructiveKey =
       keyEvent.logicalKey == LogicalKeyboardKey.backspace || keyEvent.logicalKey == LogicalKeyboardKey.delete;
-  final shouldDeleteSelection = isDestructiveKey || isCharacterKey(keyEvent.logicalKey);
+  final shouldDeleteSelection = isDestructiveKey || (keyEvent.character != null && keyEvent.character != '');
   if (!shouldDeleteSelection) {
     return ExecutionInstruction.continueExecution;
   }

super_editor/lib/src/core/document_editor.dart
@@ -96,8 +96,8 @@ class DocumentEditorTransaction {
 /// An in-memory, mutable `Document`.
 class MutableDocument with ChangeNotifier implements Document {
   MutableDocument({
-    List<DocumentNode> nodes = const [],
-  }) : _nodes = nodes {
+    List<DocumentNode>? nodes,
+  }) : _nodes = nodes ?? [] {
     // Register listeners for all initial nodes.
     for (final node in _nodes) {
       node.addListener(_forwardNodeChange);

super_editor/lib/src/default_editor/document_interaction.dart
@@ -204,8 +204,9 @@ class _DocumentInteractorState extends State<DocumentInteractor> with SingleTick
   }
 
   KeyEventResult _onKeyPressed(RawKeyEvent keyEvent) {
-    _log.log('_onKeyPressed', 'EditableDocument: onKeyPressed()');
+    _log.log('_onKeyPressed', 'keyEvent: ${keyEvent.character}');
     if (keyEvent is! RawKeyDownEvent) {
+      _log.log('_onKeyPressed', ' - not a "down" event. Ignoring.');
       return KeyEventResult.handled;
     }
 

super_editor/lib/src/default_editor/editor.dart
@@ -390,8 +390,6 @@ final defaultKeyboardActions = <DocumentKeyboardAction>[
   collapseSelectionWhenDirectionalKeyIsPressed,
   deleteExpandedSelectionWhenCharacterOrDestructiveKeyPressed,
   deleteBoxWhenBackspaceOrDeleteIsPressed,
-  insertCharacterInParagraph,
-  insertCharacterInTextComposable,
   insertNewlineInParagraph,
   splitParagraphWhenEnterPressed,
   deleteCharacterWhenBackspaceIsPressed,
@@ -401,4 +399,6 @@ final defaultKeyboardActions = <DocumentKeyboardAction>[
   deleteCharacterWhenDeleteIsPressed,
   mergeNodeWithNextWhenDeleteIsPressed,
   moveUpDownLeftAndRightWithArrowKeys,
+  insertCharacterInParagraph,
+  insertCharacterInTextComposable,
 ];

super_editor/lib/src/default_editor/paragraph.dart
@@ -150,7 +150,7 @@ ExecutionInstruction insertCharacterInParagraph({
   if (node is! ParagraphNode) {
     return ExecutionInstruction.continueExecution;
   }
-  if (!isCharacterKey(keyEvent.logicalKey)) {
+  if (keyEvent.character == null || keyEvent.character == '') {
     return ExecutionInstruction.continueExecution;
   }
   if (!editContext.composer.selection!.isCollapsed) {

super_editor/lib/src/default_editor/text.dart
@@ -184,7 +184,7 @@ class _TextComponentState extends State<TextComponent> with DocumentComponent im
 
       int newOffset = textPosition.offset;
       newOffset -= 1; // we always want to jump at least 1 character.
-      while (newOffset > 0 && latinCharacters.contains(text[newOffset])) {
+      while (newOffset > 0 && text[newOffset - 1] != ' ') {
         newOffset -= 1;
       }
       return TextPosition(offset: newOffset);
@@ -241,7 +241,7 @@ class _TextComponentState extends State<TextComponent> with DocumentComponent im
 
       int newOffset = textPosition.offset;
       newOffset += 1; // we always want to jump at least 1 character.
-      while (newOffset < text.length && latinCharacters.contains(text[newOffset])) {
+      while (newOffset < text.length && text[newOffset] != ' ') {
         newOffset += 1;
       }
       return TextPosition(offset: newOffset);
@@ -552,20 +552,20 @@ ExecutionInstruction insertCharacterInTextComposable({
   required EditContext editContext,
   required RawKeyEvent keyEvent,
 }) {
-  if (keyEvent.isMetaPressed || keyEvent.isAltPressed || keyEvent.isControlPressed) {
+  if (keyEvent.isMetaPressed || keyEvent.isControlPressed) {
     return ExecutionInstruction.continueExecution;
   }
 
   if (editContext.composer.selection == null) {
     return ExecutionInstruction.continueExecution;
   }
+  if (!editContext.composer.selection!.isCollapsed) {
+    return ExecutionInstruction.continueExecution;
+  }
   if (!_isTextEntryNode(document: editContext.editor.document, selection: editContext.composer.selection!)) {
     return ExecutionInstruction.continueExecution;
   }
-  if (!isCharacterKey(keyEvent.logicalKey)) {
-    return ExecutionInstruction.continueExecution;
-  }
-  if (!editContext.composer.selection!.isCollapsed) {
+  if (keyEvent.character == null || keyEvent.character == '') {
     return ExecutionInstruction.continueExecution;
   }
 

super_editor/lib/src/default_editor/text_tools.dart
@@ -9,19 +9,6 @@ import 'package:super_editor/src/infrastructure/_logging.dart';
 
 final _log = Logger(scope: 'text_tools.dart');
 
-const latinCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-/// Returns `true` if the given `key` represents a latin character, digit, or a
-/// symbol.
-bool isCharacterKey(LogicalKeyboardKey key) {
-  // keyLabel for a character should be: 'a', 'b',...,'A','B',...
-  if (key.keyLabel.length != 1) {
-    return false;
-  }
-  return 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890.,/;\'[]\\`~!@#\$%^&*()-=_+<>?:"{}|'
-      .contains(key.keyLabel);
-}
-
 /// Returns the word of text that contains the given `docPosition`, or `null` if
 /// no text exists at the given `docPosition`.
 ///

super_editor/pubspec.yaml
@@ -13,13 +13,10 @@ dependencies:
 
   pedantic: ^1.11.0
 
-#  uuid: ^2.2.2
   uuid: ^3.0.3
 
-#  http: ^0.12.2
   http: ^0.13.1
 
-#  linkify: ^3.0.0
   linkify: ^4.0.0
 
   # TODO: move markdown serialization to a separate package and
@@ -29,6 +26,8 @@ dependencies:
 dev_dependencies:
   flutter_test:
     sdk: flutter
+
+  mockito: ^5.0.4
 
 # For information on the generic Dart part of this file, see the
 # following page: https://dart.dev/tools/pub/pubspec

commit 03e30c94d5a0f4c3169eb1b186d3014c6753301d

Fixed a destructive arrow key selection bug that was introduced in #95.

super_editor/lib/src/default_editor/document_keyboard_actions.dart
@@ -400,13 +400,22 @@ ExecutionInstruction deleteExpandedSelectionWhenCharacterOrDestructiveKeyPressed
   required EditContext editContext,
   required RawKeyEvent keyEvent,
 }) {
+  _log.log('deleteExpandedSelectionWhenCharacterOrDestructiveKeyPressed', 'Running...');
   if (editContext.composer.selection == null || editContext.composer.selection!.isCollapsed) {
     return ExecutionInstruction.continueExecution;
   }
 
+  // Specifically exclude situations where shift is pressed because shift
+  // needs to alter the selection, not delete content. We have to explicitly
+  // look for this because when shift is pressed along with an arrow key,
+  // Flutter reports a non-null character.
+  final isShiftPressed = keyEvent.isShiftPressed;
+
   final isDestructiveKey =
       keyEvent.logicalKey == LogicalKeyboardKey.backspace || keyEvent.logicalKey == LogicalKeyboardKey.delete;
-  final shouldDeleteSelection = isDestructiveKey || (keyEvent.character != null && keyEvent.character != '');
+
+  final shouldDeleteSelection =
+      !isShiftPressed && (isDestructiveKey || (keyEvent.character != null && keyEvent.character != ''));
   if (!shouldDeleteSelection) {
     return ExecutionInstruction.continueExecution;
   }
@@ -617,6 +626,7 @@ ExecutionInstruction moveUpDownLeftAndRightWithArrowKeys({
     return ExecutionInstruction.continueExecution;
   }
 
+  print('Running moveUpDownLeftAndRightWithArrowKeys');
   if (keyEvent.logicalKey == LogicalKeyboardKey.arrowLeft) {
     _log.log('moveUpDownLeftAndRightWithArrowKeys', ' - handling left arrow key');
 