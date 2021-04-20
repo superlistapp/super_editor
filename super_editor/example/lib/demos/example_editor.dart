superlistapp/super_editor

commit 252fdfa27cef449c016ebe3db94dd5570a7518e4

Added support for non-English characters when entered via keyboard or alt-codes. The OS long-press popup for 'e' and 'n' not yet supported. (#95)

super_editor/lib/src/default_editor/document_keyboard_actions.dart
@@ -379,7 +379,7 @@ ExecutionInstruction deleteExpandedSelectionWhenCharacterOrDestructiveKeyPressed
 
   final isDestructiveKey =
       keyEvent.logicalKey == LogicalKeyboardKey.backspace || keyEvent.logicalKey == LogicalKeyboardKey.delete;
-  final shouldDeleteSelection = isDestructiveKey || isCharacterKey(keyEvent.logicalKey);
+  final shouldDeleteSelection = isDestructiveKey || (keyEvent.character != null && keyEvent.character != '');
   if (!shouldDeleteSelection) {
     return ExecutionInstruction.continueExecution;
   }

super_editor/lib/src/default_editor/editor.dart
@@ -390,8 +390,6 @@ final defaultKeyboardActions = <DocumentKeyboardAction>[
   collapseSelectionWhenDirectionalKeyIsPressed,
   deleteExpandedSelectionWhenCharacterOrDestructiveKeyPressed,
   deleteBoxWhenBackspaceOrDeleteIsPressed,
-  insertCharacterInParagraph,
-  insertCharacterInTextComposable,
   insertNewlineInParagraph,
   splitParagraphWhenEnterPressed,
   deleteCharacterWhenBackspaceIsPressed,
@@ -401,4 +399,6 @@ final defaultKeyboardActions = <DocumentKeyboardAction>[
   deleteCharacterWhenDeleteIsPressed,
   mergeNodeWithNextWhenDeleteIsPressed,
   moveUpDownLeftAndRightWithArrowKeys,
+  insertCharacterInParagraph,
+  insertCharacterInTextComposable,
 ];

super_editor/lib/src/default_editor/paragraph.dart
@@ -150,7 +150,7 @@ ExecutionInstruction insertCharacterInParagraph({
   if (node is! ParagraphNode) {
     return ExecutionInstruction.continueExecution;
   }
-  if (!isCharacterKey(keyEvent.logicalKey)) {
+  if (keyEvent.character == null || keyEvent.character == '') {
     return ExecutionInstruction.continueExecution;
   }
   if (!editContext.composer.selection!.isCollapsed) {

super_editor/lib/src/core/document_editor.dart
@@ -96,8 +96,8 @@ class DocumentEditorTransaction {
 /// An in-memory, mutable `Document`.
 class MutableDocument with ChangeNotifier implements Document {
   MutableDocument({
-    List<DocumentNode> nodes = const [],
-  }) : _nodes = nodes {
+    List<DocumentNode>? nodes,
+  }) : _nodes = nodes ?? [] {
     // Register listeners for all initial nodes.
     for (final node in _nodes) {
       node.addListener(_forwardNodeChange);

super_editor/lib/src/default_editor/document_interaction.dart
@@ -204,8 +204,9 @@ class _DocumentInteractorState extends State<DocumentInteractor> with SingleTick
   }
 
   KeyEventResult _onKeyPressed(RawKeyEvent keyEvent) {
-    _log.log('_onKeyPressed', 'EditableDocument: onKeyPressed()');
+    _log.log('_onKeyPressed', 'keyEvent: ${keyEvent.character}');
     if (keyEvent is! RawKeyDownEvent) {
+      _log.log('_onKeyPressed', ' - not a "down" event. Ignoring.');
       return KeyEventResult.handled;
     }
 

super_editor/lib/src/default_editor/text.dart
@@ -184,7 +184,7 @@ class _TextComponentState extends State<TextComponent> with DocumentComponent im
 
       int newOffset = textPosition.offset;
       newOffset -= 1; // we always want to jump at least 1 character.
-      while (newOffset > 0 && latinCharacters.contains(text[newOffset])) {
+      while (newOffset > 0 && text[newOffset - 1] != ' ') {
         newOffset -= 1;
       }
       return TextPosition(offset: newOffset);
@@ -241,7 +241,7 @@ class _TextComponentState extends State<TextComponent> with DocumentComponent im
 
       int newOffset = textPosition.offset;
       newOffset += 1; // we always want to jump at least 1 character.
-      while (newOffset < text.length && latinCharacters.contains(text[newOffset])) {
+      while (newOffset < text.length && text[newOffset] != ' ') {
         newOffset += 1;
       }
       return TextPosition(offset: newOffset);
@@ -552,20 +552,20 @@ ExecutionInstruction insertCharacterInTextComposable({
   required EditContext editContext,
   required RawKeyEvent keyEvent,
 }) {
-  if (keyEvent.isMetaPressed || keyEvent.isAltPressed || keyEvent.isControlPressed) {
+  if (keyEvent.isMetaPressed || keyEvent.isControlPressed) {
     return ExecutionInstruction.continueExecution;
   }
 
   if (editContext.composer.selection == null) {
     return ExecutionInstruction.continueExecution;
   }
+  if (!editContext.composer.selection!.isCollapsed) {
+    return ExecutionInstruction.continueExecution;
+  }
   if (!_isTextEntryNode(document: editContext.editor.document, selection: editContext.composer.selection!)) {
     return ExecutionInstruction.continueExecution;
   }
-  if (!isCharacterKey(keyEvent.logicalKey)) {
-    return ExecutionInstruction.continueExecution;
-  }
-  if (!editContext.composer.selection!.isCollapsed) {
+  if (keyEvent.character == null || keyEvent.character == '') {
     return ExecutionInstruction.continueExecution;
   }
 

super_editor/lib/src/default_editor/text_tools.dart
@@ -9,19 +9,6 @@ import 'package:super_editor/src/infrastructure/_logging.dart';
 
 final _log = Logger(scope: 'text_tools.dart');
 
-const latinCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-/// Returns `true` if the given `key` represents a latin character, digit, or a
-/// symbol.
-bool isCharacterKey(LogicalKeyboardKey key) {
-  // keyLabel for a character should be: 'a', 'b',...,'A','B',...
-  if (key.keyLabel.length != 1) {
-    return false;
-  }
-  return 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890.,/;\'[]\\`~!@#\$%^&*()-=_+<>?:"{}|'
-      .contains(key.keyLabel);
-}
-
 /// Returns the word of text that contains the given `docPosition`, or `null` if
 /// no text exists at the given `docPosition`.
 ///

super_editor/pubspec.yaml
@@ -13,13 +13,10 @@ dependencies:
 
   pedantic: ^1.11.0
 
-#  uuid: ^2.2.2
   uuid: ^3.0.3
 
-#  http: ^0.12.2
   http: ^0.13.1
 
-#  linkify: ^3.0.0
   linkify: ^4.0.0
 
   # TODO: move markdown serialization to a separate package and
@@ -29,6 +26,8 @@ dependencies:
 dev_dependencies:
   flutter_test:
     sdk: flutter
+
+  mockito: ^5.0.4
 
 # For information on the generic Dart part of this file, see the
 # following page: https://dart.dev/tools/pub/pubspec

commit 65c376aea910551532511d8d110b9dc0d2db5030

Add blockquote support (#129)
* Implemented blockquote support. (#96)

* Fixed a destructive arrow key selection bug that was introduced in #95.

* Removed some print() statements.

super_editor/example/lib/demos/example_editor.dart
@@ -61,6 +61,15 @@ Document _createInitialDocument() {
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed sagittis urna. Aenean mattis ante justo, quis sollicitudin metus interdum id. Aenean ornare urna ac enim consequat mollis. In aliquet convallis efficitur. Phasellus convallis purus in fringilla scelerisque. Ut ac orci a turpis egestas lobortis. Morbi aliquam dapibus sem, vitae sodales arcu ultrices eu. Duis vulputate mauris quam, eleifend pulvinar quam blandit eget.',
         ),
       ),
+      ParagraphNode(
+        id: DocumentEditor.createNodeId(),
+        text: AttributedText(
+          text: 'This is a blockquote!',
+        ),
+        metadata: {
+          'blockType': 'blockquote',
+        },
+      ),
       ListItemNode.unordered(
         id: DocumentEditor.createNodeId(),
         text: AttributedText(

super_editor/lib/src/default_editor/blockquote.dart
@@ -1,0 +1,282 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:super_editor/src/core/document_layout.dart';
+import 'package:super_editor/src/core/edit_context.dart';
+import 'package:super_editor/src/infrastructure/_logging.dart';
+import 'package:super_editor/src/infrastructure/attributed_text.dart';
+
+import '../core/document.dart';
+import '../core/document_selection.dart';
+import '../core/document_editor.dart';
+import 'document_interaction.dart';
+import 'paragraph.dart';
+import 'styles.dart';
+import 'text.dart';
+
+final _log = Logger(scope: 'blockquote.dart');
+
+/// Displays a blockquote in a document.
+class BlockquoteComponent extends StatelessWidget {
+  const BlockquoteComponent({
+    Key? key,
+    required this.textKey,
+    required this.text,
+    required this.styleBuilder,
+    this.textSelection,
+    this.selectionColor = Colors.lightBlueAccent,
+    this.showCaret = false,
+    this.caretColor = Colors.black,
+    this.showDebugPaint = false,
+  }) : super(key: key);
+
+  final GlobalKey textKey;
+  final AttributedText text;
+  final AttributionStyleBuilder styleBuilder;
+  final TextSelection? textSelection;
+  final Color selectionColor;
+  final bool showCaret;
+  final Color caretColor;
+  final bool showDebugPaint;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4),
+        color: const Color(0xFFEEEEEE),
+      ),
+      child: TextComponent(
+        key: textKey,
+        text: text,
+        textStyleBuilder: styleBuilder,
+        textSelection: textSelection,
+        selectionColor: selectionColor,
+        showCaret: showCaret,
+        caretColor: caretColor,
+        showDebugPaint: showDebugPaint,
+      ),
+    );
+  }
+}
+
+ExecutionInstruction convertBlockquoteToParagraphWhenBackspaceIsPressed({
+  required EditContext editContext,
+  required RawKeyEvent keyEvent,
+}) {
+  if (keyEvent.logicalKey != LogicalKeyboardKey.backspace) {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  if (editContext.composer.selection == null) {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  final node = editContext.editor.document.getNodeById(editContext.composer.selection!.extent.nodeId);
+  if (node is! ParagraphNode) {
+    return ExecutionInstruction.continueExecution;
+  }
+  if (node.metadata['blockType'] != 'blockquote') {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  if (!editContext.composer.selection!.isCollapsed) {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  final textPosition = editContext.composer.selection!.extent.nodePosition;
+  if (textPosition is! TextPosition || textPosition.offset > 0) {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  editContext.editor.executeCommand(
+    ConvertBlockquoteToParagraphCommand(nodeId: node.id),
+  );
+
+  return ExecutionInstruction.haltExecution;
+}
+
+class ConvertBlockquoteToParagraphCommand implements EditorCommand {
+  ConvertBlockquoteToParagraphCommand({
+    required this.nodeId,
+  });
+
+  final String nodeId;
+
+  @override
+  void execute(Document document, DocumentEditorTransaction transaction) {
+    final node = document.getNodeById(nodeId);
+    final blockquote = node as ParagraphNode;
+    final newParagraphNode = ParagraphNode(
+      id: blockquote.id,
+      text: blockquote.text,
+    );
+    final blockquoteNodeIndex = document.getNodeIndex(blockquote);
+    transaction
+      ..deleteNodeAt(blockquoteNodeIndex)
+      ..insertNodeAt(blockquoteNodeIndex, newParagraphNode);
+  }
+}
+
+ExecutionInstruction insertNewlineInBlockquote({
+  required EditContext editContext,
+  required RawKeyEvent keyEvent,
+}) {
+  if (keyEvent.logicalKey != LogicalKeyboardKey.enter) {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  if (!keyEvent.isShiftPressed) {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  if (editContext.composer.selection == null) {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  if (!editContext.composer.selection!.isCollapsed) {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  final node = editContext.editor.document.getNodeById(editContext.composer.selection!.extent.nodeId);
+  if (node is! ParagraphNode) {
+    return ExecutionInstruction.continueExecution;
+  }
+  if (node.metadata['blockType'] != 'blockquote') {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  final textNode = editContext.editor.document.getNode(editContext.composer.selection!.extent) as TextNode;
+  final initialTextOffset = (editContext.composer.selection!.extent.nodePosition as TextPosition).offset;
+
+  editContext.editor.executeCommand(
+    InsertTextCommand(
+      documentPosition: editContext.composer.selection!.extent,
+      textToInsert: '\n',
+      attributions: editContext.composer.preferences.currentStyles,
+    ),
+  );
+
+  editContext.composer.selection = DocumentSelection.collapsed(
+    position: DocumentPosition(
+      nodeId: textNode.id,
+      nodePosition: TextPosition(
+        offset: initialTextOffset + 1,
+      ),
+    ),
+  );
+
+  return ExecutionInstruction.haltExecution;
+}
+
+ExecutionInstruction splitBlockquoteWhenEnterPressed({
+  required EditContext editContext,
+  required RawKeyEvent keyEvent,
+}) {
+  if (keyEvent.logicalKey != LogicalKeyboardKey.enter) {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  if (editContext.composer.selection == null) {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  if (!editContext.composer.selection!.isCollapsed) {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  final node = editContext.editor.document.getNodeById(editContext.composer.selection!.extent.nodeId);
+  if (node is! ParagraphNode) {
+    return ExecutionInstruction.continueExecution;
+  }
+  if (node.metadata['blockType'] != 'blockquote') {
+    return ExecutionInstruction.continueExecution;
+  }
+
+  final newNodeId = DocumentEditor.createNodeId();
+
+  editContext.editor.executeCommand(
+    SplitBlockquoteCommand(
+      nodeId: node.id,
+      splitPosition: editContext.composer.selection!.extent.nodePosition as TextPosition,
+      newNodeId: newNodeId,
+    ),
+  );
+
+  // Place the caret at the beginning of the new paragraph node.
+  editContext.composer.selection = DocumentSelection.collapsed(
+    position: DocumentPosition(
+      nodeId: newNodeId,
+      nodePosition: TextPosition(offset: 0),
+    ),
+  );
+
+  return ExecutionInstruction.haltExecution;
+}
+
+class SplitBlockquoteCommand implements EditorCommand {
+  SplitBlockquoteCommand({
+    required this.nodeId,
+    required this.splitPosition,
+    required this.newNodeId,
+  });
+
+  final String nodeId;
+  final TextPosition splitPosition;
+  final String newNodeId;
+
+  @override
+  void execute(Document document, DocumentEditorTransaction transaction) {
+    final node = document.getNodeById(nodeId);
+    final blockquote = node as ParagraphNode;
+    final text = blockquote.text;
+    final startText = text.copyText(0, splitPosition.offset);
+    final endText = splitPosition.offset < text.text.length ? text.copyText(splitPosition.offset) : AttributedText();
+
+    // Change the current node's content to just the text before the caret.
+    // TODO: figure out how node changes should work in terms of
+    //       a DocumentEditorTransaction (#67)
+    blockquote.text = startText;
+
+    // Create a new node that will follow the current node. Set its text
+    // to the text that was removed from the current node.
+    final isNewNodeABlockquote = endText.text.isNotEmpty;
+    final newNode = ParagraphNode(
+      id: newNodeId,
+      text: endText,
+      metadata: isNewNodeABlockquote ? {'blockType': 'blockquote'} : {},
+    );
+
+    // Insert the new node after the current node.
+    transaction.insertNodeAfter(
+      previousNode: node,
+      newNode: newNode,
+    );
+  }
+}
+
+Widget? blockquoteBuilder(ComponentContext componentContext) {
+  final blockquoteNode = componentContext.documentNode;
+  if (blockquoteNode is! ParagraphNode) {
+    return null;
+  }
+  if (blockquoteNode.metadata['blockType'] != 'blockquote') {
+    return null;
+  }
+
+  final textSelection = componentContext.nodeSelection?.nodeSelection as TextSelection;
+  final showCaret = componentContext.showCaret && (componentContext.nodeSelection?.isExtent ?? false);
+
+  return BlockquoteComponent(
+    textKey: componentContext.componentKey,
+    text: blockquoteNode.text,
+    styleBuilder: componentContext.extensions[textStylesExtensionKey],
+    textSelection: textSelection,
+    selectionColor: (componentContext.extensions[selectionStylesExtensionKey] as SelectionStyle).selectionColor,
+    showCaret: showCaret,
+    caretColor: (componentContext.extensions[selectionStylesExtensionKey] as SelectionStyle).textCaretColor,
+  );
+}

super_editor/lib/src/default_editor/document_keyboard_actions.dart
@@ -130,7 +130,6 @@ Future<void> _paste({
 }) async {
   final content = (await Clipboard.getData('text/plain'))?.text ?? '';
   _log.log('_paste', 'Content from clipboard:');
-  print(content);
 
   editor.executeCommand(
     _PasteEditorCommand(
@@ -400,13 +399,22 @@ ExecutionInstruction deleteExpandedSelectionWhenCharacterOrDestructiveKeyPressed
   required EditContext editContext,
   required RawKeyEvent keyEvent,
 }) {
+  _log.log('deleteExpandedSelectionWhenCharacterOrDestructiveKeyPressed', 'Running...');
   if (editContext.composer.selection == null || editContext.composer.selection!.isCollapsed) {
     return ExecutionInstruction.continueExecution;
   }
 
+  // Specifically exclude situations where shift is pressed because shift
+  // needs to alter the selection, not delete content. We have to explicitly
+  // look for this because when shift is pressed along with an arrow key,
+  // Flutter reports a non-null character.
+  final isShiftPressed = keyEvent.isShiftPressed;
+
   final isDestructiveKey =
       keyEvent.logicalKey == LogicalKeyboardKey.backspace || keyEvent.logicalKey == LogicalKeyboardKey.delete;
-  final shouldDeleteSelection = isDestructiveKey || (keyEvent.character != null && keyEvent.character != '');
+
+  final shouldDeleteSelection =
+      !isShiftPressed && (isDestructiveKey || (keyEvent.character != null && keyEvent.character != ''));
   if (!shouldDeleteSelection) {
     return ExecutionInstruction.continueExecution;
   }

super_editor/lib/src/default_editor/editor.dart
@@ -5,6 +5,7 @@ import 'package:super_editor/src/core/document_composer.dart';
 import 'package:super_editor/src/core/document_editor.dart';
 import 'package:super_editor/src/core/document_layout.dart';
 import 'package:super_editor/src/core/edit_context.dart';
+import 'package:super_editor/src/default_editor/blockquote.dart';
 import 'package:super_editor/src/default_editor/horizontal_rule.dart';
 import 'package:super_editor/src/default_editor/image.dart';
 import 'package:super_editor/src/default_editor/list_items.dart';
@@ -372,6 +373,7 @@ final defaultComponentBuilders = <ComponentBuilder>[
   paragraphBuilder,
   unorderedListItemBuilder,
   orderedListItemBuilder,
+  blockquoteBuilder,
   imageBuilder,
   horizontalRuleBuilder,
   unknownComponentBuilder,
@@ -383,6 +385,9 @@ final defaultKeyboardActions = <DocumentKeyboardAction>[
   indentListItemWhenBackspaceIsPressed,
   unindentListItemWhenBackspaceIsPressed,
   splitListItemWhenEnterPressed,
+  convertBlockquoteToParagraphWhenBackspaceIsPressed,
+  insertNewlineInBlockquote,
+  splitBlockquoteWhenEnterPressed,
   pasteWhenCmdVIsPressed,
   copyWhenCmdVIsPressed,
   selectAllWhenCmdAIsPressed,

super_editor/lib/src/default_editor/paragraph.dart
@@ -1,20 +1,19 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+import 'package:linkify/linkify.dart';
 import 'package:super_editor/src/core/document.dart';
 import 'package:super_editor/src/core/document_composer.dart';
 import 'package:super_editor/src/core/document_editor.dart';
 import 'package:super_editor/src/core/document_layout.dart';
 import 'package:super_editor/src/core/document_selection.dart';
 import 'package:super_editor/src/core/edit_context.dart';
-import 'package:super_editor/src/default_editor/text_tools.dart';
 import 'package:super_editor/src/default_editor/document_interaction.dart';
 import 'package:super_editor/src/default_editor/text.dart';
 import 'package:super_editor/src/infrastructure/_logging.dart';
 import 'package:super_editor/src/infrastructure/attributed_text.dart';
-import 'package:collection/collection.dart';
-import 'package:http/http.dart' as http;
-import 'package:linkify/linkify.dart';
 
 import 'horizontal_rule.dart';
 import 'image.dart';
@@ -258,6 +257,42 @@ bool _convertParagraphIfDesired({
       position: DocumentPosition(
         nodeId: node.id,
         nodePosition: TextPosition(offset: 0),
+      ),
+    );
+
+    return true;
+  }
+
+  final blockquoteMatch = RegExp(r'^>*\s$');
+  final hasBlockquoteMatch = blockquoteMatch.hasMatch(textBeforeCaret);
+  if (hasBlockquoteMatch) {
+    int startOfNewText = textBeforeCaret.length;
+    while (startOfNewText < node.text.text.length && node.text.text[startOfNewText] == ' ') {
+      startOfNewText += 1;
+    }
+    final adjustedText = node.text.copyText(startOfNewText);
+    final newNode = ParagraphNode(
+      id: node.id,
+      text: adjustedText,
+      metadata: {'blockType': 'blockquote'},
+    );
+    final nodeIndex = document.getNodeIndex(node);
+
+    editor.executeCommand(
+      EditorCommandFunction((document, transaction) {
+        transaction
+          ..deleteNodeAt(nodeIndex)
+          ..insertNodeAt(nodeIndex, newNode);
+      }),
+    );
+
+    // We removed some text at the beginning of the list item.
+    // Move the selection back by that same amount.
+    final textPosition = composer.selection!.extent.nodePosition as TextPosition;
+    composer.selection = DocumentSelection.collapsed(
+      position: DocumentPosition(
+        nodeId: node.id,
+        nodePosition: TextPosition(offset: textPosition.offset - startOfNewText),
       ),
     );
 

super_editor/lib/super_editor.dart
@@ -10,6 +10,7 @@ export 'src/core/document_editor.dart';
 export 'src/core/document_layout.dart';
 export 'src/core/document_selection.dart';
 export 'src/core/edit_context.dart';
+export 'src/default_editor/blockquote.dart';
 export 'src/default_editor/box_component.dart';
 export 'src/default_editor/document_interaction.dart';
 export 'src/default_editor/document_keyboard_actions.dart';