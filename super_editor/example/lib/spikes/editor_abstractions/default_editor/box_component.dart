3afd9d8cc841a75c848eb44f94228f4a21f0a698
Muhammed Salih Guler <muhammedsalihguler@gmail.com>
2021-04-26 10:09:36 +0300

    Add the missing imports. (#141)

--- super_editor/example/lib/spikes/editor_abstractions/default_editor/box_component.dart
+++ super_editor/example/lib/spikes/editor_abstractions/default_editor/box_component.dart
@@ -1,4 +1,5 @@
 import 'package:example/spikes/editor_abstractions/core/edit_context.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
