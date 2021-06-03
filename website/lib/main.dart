dffd6242d95b263ab1e882b8db2d9123e587865b
Iiro Krankka <iiro.krankka@gmail.com>
2021-06-03 09:13:35 +0300

    Another try for making the path work

--- website/lib/main.dart
+++ website/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:website/homepage/home_page.dart';
 
 void main() {
-  setUrlStrategy(PathUrlStrategy());
   runApp(const MyApp());
 }
 
