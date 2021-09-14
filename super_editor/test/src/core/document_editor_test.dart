81cec175dc0c661664851f4901119dd672c13ccb
Britannio <britanniojarrett@gmail.com>
2021-09-14 22:44:00 +0100

    Updated/Additional MutableDocument equality tests

--- super_editor/test/src/core/document_editor_test.dart
+++ super_editor/test/src/core/document_editor_test.dart
@@ -22,7 +22,16 @@ void main() {
       expect(document.nodes.indexOf(newNode), 1);
     });
 
-    test('it is equal to a similar document with the same content', () {
+    test('it is equal to another document when both documents are empty', () {
+      final document1 = MutableDocument(nodes: []);
+      final document2 = MutableDocument(nodes: []);
+
+      expect(document1 == document2, isTrue);
+    });
+
+    test(
+        'it is equal to another document when each content node is equal to the corresponding node in the other document',
+        () {
       final node = HorizontalRuleNode(id: '0');
 
       final document1 = MutableDocument(nodes: [node]);
@@ -31,11 +40,25 @@ void main() {
       expect(document1 == document2, isTrue);
     });
 
-    test('it is NOT equal to a similar document with different content', () {
-      final node = HorizontalRuleNode(id: '0');
+    test(
+        'it is NOT equal to a document with the same starting nodes but with additional nodes at the end',
+        () {
+      final node1 = HorizontalRuleNode(id: '1');
+      final node2 = HorizontalRuleNode(id: '2');
 
-      final document1 = MutableDocument(nodes: [node]);
-      final document2 = MutableDocument(nodes: [node, node]);
+      final document1 = MutableDocument(nodes: [node1]);
+      final document2 = MutableDocument(nodes: [node1, node2]);
+
+      expect(document1 == document2, isFalse);
+    });
+
+    test('it is NOT equal to a document when corresponding nodes are NOT equal',
+        () {
+      final node1 = HorizontalRuleNode(id: '1');
+      final node2 = HorizontalRuleNode(id: '2');
+
+      final document1 = MutableDocument(nodes: [node1]);
+      final document2 = MutableDocument(nodes: [node2]);
 
       expect(document1 == document2, isFalse);
     });
