b12ab3d8da4247086cf85c738003e94971aa2e01
Matt Carroll <matthew-carroll@users.noreply.github.com>
2021-10-31 20:56:03 -0700

    Add TextWithHintComponent (Resolves #330) (#331)
    * Add TextWithHintComponent
     - add ProxyTextComposable
     - add ProxyDocumentComponent

---
+++ super_editor/test_goldens/editor/components/text_with_hint_test.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/src/widgets/basic.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:super_editor/src/default_editor/text.dart';
+import 'package:super_editor/super_editor.dart';
+
+import '_components_test_utils.dart';
+
+void main() {
+  group('editor', () {
+    group('components', () {
+      testComponentGolden(
+        'text with hint',
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextWithHintComponent(
+              text: AttributedText(text: ''),
+              textStyleBuilder: _textStyleBuilder,
+              hintText: AttributedText(text: "this is a hint..."),
+              hintStyleAdjustment: _hintStyle,
+            ),
+            const SizedBox(height: 24),
+            TextWithHintComponent(
+              text: AttributedText(text: 'This is content text.'),
+              textStyleBuilder: _textStyleBuilder,
+              hintText: AttributedText(text: "this is a hint..."),
+              hintStyleAdjustment: _hintStyle,
+            ),
+          ],
+        ),
+        'text_with_hint',
+      );
+    });
+  });
+}
+
+const _hintStyle = TextStyle(
+  color: Color(0xFFDDDDDD),
+);
+
+TextStyle _textStyleBuilder(Set<Attribution> attributions) {
+  return const TextStyle(
+    color: Color(0xFF000000),
+    fontFamily: 'Roboto',
+    fontSize: 40,
+  );
+}
