1d09d30c4835e079972e863857f8d66c1709533a
Matt Carroll <matthew-carroll@users.noreply.github.com>
2021-12-10 17:34:51 -0800

    WIP: integrate IME

--- super_editor/lib/src/default_editor/document_input_ime.dart
+++ super_editor/lib/src/default_editor/document_input_ime.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:super_editor/src/core/edit_context.dart';
+import 'package:super_editor/src/infrastructure/_logging.dart';
 
-/// Governs document input that comes from a the operating systems
+/// Governs document input that comes from the operating system's
 /// Input Method Engine (IME).
 ///
 /// IME input is the only form of input that can come from a mobile
@@ -28,9 +30,153 @@ class DocumentImeInteractor extends StatefulWidget {
   _DocumentImeInteractorState createState() => _DocumentImeInteractorState();
 }
 
-class _DocumentImeInteractorState extends State<DocumentImeInteractor> {
+class _DocumentImeInteractorState extends State<DocumentImeInteractor> implements DeltaTextInputClient {
+  late FocusNode _focusNode;
+
+  TextInputConnection? _inputConnection;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _focusNode = (widget.focusNode ?? FocusNode())..addListener(_onFocusChange);
+
+    widget.editContext.composer.addListener(_onComposerChange);
+  }
+
+  @override
+  void didUpdateWidget(DocumentImeInteractor oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.focusNode != oldWidget.focusNode) {
+      _focusNode.removeListener(_onFocusChange);
+      _focusNode = (widget.focusNode ?? FocusNode())..addListener(_onFocusChange);
+    }
+
+    if (widget.editContext.composer != oldWidget.editContext.composer) {
+      oldWidget.editContext.composer.removeListener(_onComposerChange);
+      widget.editContext.composer.addListener(_onComposerChange);
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.editContext.composer.removeListener(_onComposerChange);
+
+    if (widget.focusNode == null) {
+      _focusNode.dispose();
+    }
+
+    super.dispose();
+  }
+
+  void _onFocusChange() {
+    if (_focusNode.hasFocus) {
+      editorImeLog.info('Gained focus');
+      _attachToIme();
+    } else {
+      editorImeLog.info('Lost focus');
+      _detachFromIme();
+    }
+  }
+
+  void _onComposerChange() {
+    final selection = widget.editContext.composer.selection;
+
+    if (selection == null) {
+      // TODO: null out the text editing value
+    } else {
+      // TODO: if within single text node, set normal text editing value
+      // TODO: if multiple nodes and all text, maybe combine the text into text editing value
+      // TODO: if multiple nodes and mixed content, figure out what makes sense
+    }
+  }
+
+  bool get isAttachedToIme => _inputConnection != null && _inputConnection!.attached;
+
+  void _attachToIme() {
+    if (isAttachedToIme) {
+      // We're already connected to the IME.
+      return;
+    }
+
+    editorImeLog.info('Attaching TextInputClient to TextInput');
+    _inputConnection = TextInput.attach(
+        this,
+        const TextInputConfiguration(
+          // TODO: make this configurable
+          autocorrect: true,
+          enableDeltaModel: true,
+          // TODO: make this configurable
+          enableSuggestions: true,
+          // TODO: make this configurable
+          inputAction: TextInputAction.newline,
+        ));
+
+    _inputConnection!
+      ..show()
+      ..setEditingState(currentTextEditingValue!);
+
+    editorImeLog.fine('Is attached to input client? ${_inputConnection!.attached}');
+  }
+
+  void _detachFromIme() {
+    if (!isAttachedToIme) {
+      return;
+    }
+
+    editorImeLog.info('Detaching TextInputClient from TextInput.');
+
+    widget.editContext.composer.selection = null;
+
+    _inputConnection!.close();
+  }
+
+  @override
+  // TODO: implement currentAutofillScope
+  AutofillScope? get currentAutofillScope => throw UnimplementedError();
+
+  @override
+  // TODO: implement currentTextEditingValue
+  TextEditingValue? get currentTextEditingValue => throw UnimplementedError();
+
+  @override
+  void updateEditingValue(TextEditingValue value) {
+    // TODO: implement updateEditingValue
+  }
+
+  @override
+  void updateEditingValueWithDeltas(List<TextEditingDelta> textEditingDeltas) {
+    // TODO: implement updateEditingValueWithDeltas
+  }
+
+  @override
+  void performAction(TextInputAction action) {
+    // TODO: implement performAction
+  }
+
+  @override
+  void performPrivateCommand(String action, Map<String, dynamic> data) {
+    // TODO: implement performPrivateCommand
+  }
+
+  @override
+  void showAutocorrectionPromptRect(int start, int end) {
+    // TODO: implement showAutocorrectionPromptRect
+  }
+
+  @override
+  void updateFloatingCursor(RawFloatingCursorPoint point) {
+    // TODO: implement updateFloatingCursor
+  }
+
+  @override
+  void connectionClosed() {
+    // TODO: implement connectionClosed
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ErrorWidget("DocumentImeInteractor is not yet implemented!");
+    return widget.child;
   }
 }
