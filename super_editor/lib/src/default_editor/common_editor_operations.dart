e80d41e45e633fae2cdc8a580cb4b9b6a41ae864
osaxma <46427323+osaxma@users.noreply.github.com>
2021-12-12 18:08:45 +0300

    fixes shift+tab exception for non ListItemNode

--- super_editor/lib/src/default_editor/common_editor_operations.dart
+++ super_editor/lib/src/default_editor/common_editor_operations.dart
@@ -1910,7 +1910,7 @@ class CommonEditorOperations {
 
     final baseNode = editor.document.getNodeById(composer.selection!.base.nodeId);
     final extentNode = editor.document.getNodeById(composer.selection!.extent.nodeId);
-    if (baseNode!.id != extentNode!.id) {
+    if (baseNode!.id != extentNode!.id || baseNode is! ListItemNode) {
       return false;
     }
 
