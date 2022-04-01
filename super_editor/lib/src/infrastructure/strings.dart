3e7a946de2017bd1981e6b00f3c2df7f3ae5734d
Josh Matthews <josh@jmatth.com>
2022-04-01 00:35:18 -0400

    Improve navigating by word (#454)

--- super_editor/lib/src/infrastructure/strings.dart
+++ super_editor/lib/src/infrastructure/strings.dart
@@ -1,52 +1,30 @@
 import 'package:characters/characters.dart';
 
+// Match any characters we want skip over while moving by word. This will match
+// Unicode graphemes with a General_Category value in:
+// - Punctuation (P), such as ".", "-", and "。"
+// - Separator (Z), such as " " (space), and "　" (ideographic space)
+// See http://www.unicode.org/reports/tr44/#GC_Values_Table for details on
+// on the Unicode General_Category property.
+final _separatorRegex = RegExp(r'^[\p{Z}\p{P}]$', unicode: true);
+
 extension CharacterMovement on String {
   /// Returns the code point index of the character that sits
-  /// one word upstream from the given [textOffset] code point index.
+  /// at the next start of word upstream from the given
+  /// [textOffset] code point index.
   ///
   /// Examples:
   ///   |word up -> `null`
   ///   wo|rd up -> `0`
   ///   word| up -> `0`
-  ///   word |up -> `4`
-  ///   word up| -> `6`
-  int? moveOffsetUpstreamByWord(int textOffset, {int characterCount = 1}) {
-    if (textOffset < 0 || textOffset > length) {
-      throw Exception("Index '$textOffset' is out of string range. Length: $length");
-    }
-
+  ///   word |up -> `0`
+  ///   word up| -> `5`
+  int? moveOffsetUpstreamByWord(int textOffset) {
     if (textOffset == 0) {
       return null;
     }
 
-    bool isInSpace = false;
-
-    int lastSpaceStartCodePointOffset = 0;
-    int lastSpaceEndCodePointOffset = 0;
-
-    int visitedCharacterCount = 0;
-    int codePointIndex = 0;
-    for (final character in characters) {
-      if (visitedCharacterCount >= textOffset - 1) {
-        // We're at the given text offset. The upstream word offset is
-        // at lastSpaceEndCodePointOffset.
-        break;
-      }
-
-      if (character == " " && !isInSpace) {
-        lastSpaceStartCodePointOffset = codePointIndex;
-      }
-
-      isInSpace = character == " ";
-      codePointIndex += character.length;
-      visitedCharacterCount += 1;
-
-      if (character == " ") {
-        lastSpaceEndCodePointOffset = codePointIndex;
-      }
-    }
-
-    return lastSpaceEndCodePointOffset < textOffset ? lastSpaceEndCodePointOffset : lastSpaceStartCodePointOffset;
+    return _moveOffsetByWord(textOffset, true);
   }
 
   /// Returns the code point index of the character that sits
@@ -59,79 +37,29 @@ extension CharacterMovement on String {
   ///   a💙|c -> `1`
   ///   a💙c| -> `3` (notice that we moved 2 units due to emoji length)
   int? moveOffsetUpstreamByCharacter(int textOffset, {int characterCount = 1}) {
-    if (textOffset < 0 || textOffset > length) {
-      throw Exception("Index '$textOffset' is out of string range. Length: $length");
-    }
-
     if (textOffset == 0) {
       return null;
     }
 
-    int codePointIndex = 0;
-    final characterIndices = <int>[];
-    for (final character in characters) {
-      if (codePointIndex == textOffset) {
-        break;
-      }
-
-      characterIndices.add(codePointIndex);
-      codePointIndex += character.length;
-    }
-
-    if (characterIndices.length < characterCount) {
-      return null;
-    }
-
-    return characterIndices[characterIndices.length - characterCount];
+    return _moveOffsetByCharacter(textOffset, characterCount, true);
   }
 
   /// Returns the code point index of the character that sits
-  /// one word downstream from given [textOffset] code point
-  /// index.
+  /// after the end of the next word downstream from the given
+  /// [textOffset] code point index.
   ///
   /// Examples:
   ///   |word up -> `4`
   ///   wo|rd up -> `4`
-  ///   word| up -> `5`
+  ///   word| up -> `7`
   ///   word |up -> `7`
   ///   word up| -> `null`
-  int? moveOffsetDownstreamByWord(int textOffset, {int characterCount = 1}) {
-    if (textOffset < 0 || textOffset > length) {
-      throw Exception("Index '$textOffset' is out of string range. Length: $length");
-    }
-
+  int? moveOffsetDownstreamByWord(int textOffset) {
     if (textOffset == length) {
       return null;
     }
 
-    bool isInSpace = false;
-
-    int lastSpaceStartCodePointOffset = 0;
-    int lastSpaceEndCodePointOffset = 0;
-
-    int codePointIndex = 0;
-    for (final character in characters) {
-      if (character == " " && !isInSpace) {
-        lastSpaceStartCodePointOffset = codePointIndex;
-      }
-
-      isInSpace = character == " ";
-      codePointIndex += character.length;
-
-      if (character == " ") {
-        lastSpaceEndCodePointOffset = codePointIndex;
-      }
-
-      if (lastSpaceStartCodePointOffset > textOffset) {
-        return lastSpaceStartCodePointOffset;
-      }
-      if (lastSpaceEndCodePointOffset > textOffset) {
-        return lastSpaceEndCodePointOffset;
-      }
-    }
-
-    // The end of this string is as far as we can go.
-    return length;
+    return _moveOffsetByWord(textOffset, false);
   }
 
   /// Returns the code point index of the character that sits
@@ -144,31 +72,73 @@ extension CharacterMovement on String {
   ///   a💙|c -> `4`
   ///   a💙c| -> `null`
   int? moveOffsetDownstreamByCharacter(int textOffset, {int characterCount = 1}) {
-    if (textOffset < 0 || textOffset > length) {
-      throw Exception("Index '$textOffset' is out of string range. Length: $length");
-    }
-
     if (textOffset == length) {
       return null;
     }
 
-    int visitedCharacterCodePointLength = 0;
-    int characterCountBeyondTextOffset = 0;
-    for (final character in characters) {
-      visitedCharacterCodePointLength += character.length;
-      if (visitedCharacterCodePointLength > textOffset) {
-        characterCountBeyondTextOffset += 1;
-      }
+    return _moveOffsetByCharacter(textOffset, characterCount, false);
+  }
 
-      if (characterCountBeyondTextOffset == characterCount) {
-        break;
-      }
+  int? _moveOffsetByWord(int textOffset, bool upstream) {
+    if (textOffset < 0 || textOffset > length) {
+      throw Exception("Index '$textOffset' is out of string range. Length: $length");
     }
 
-    if (characterCountBeyondTextOffset < characterCount) {
+    // Create a character range, initially with zero length
+    // Note that the getter for this object is confusingly named: it is an iterator but includes lots of functionality
+    // beyond that interface, most importantly for us a range over this string that can be manipulated in terms of
+    // characters
+    final range = characters.iterator;
+    // Expand the range so it reaches from the start of the string to the initial text offset. The text offset is passed
+    // to us in terms of code units but the iterator deals in grapheme clusters, so we need to manually count the length
+    // of each cluster until we reach the desired offset
+    var remainingOffset = textOffset;
+    range.expandWhile((char) {
+      remainingOffset -= char.length;
+      return remainingOffset >= 0;
+    });
+    final moveWhile = upstream ? range.dropBackWhile : range.expandWhile;
+    // Adjust the range in the requested direction as long it does not end in a word. This accounts for cases where the
+    // text offset starts in between words. After this we know the range ends on a word character
+    moveWhile((char) => _separatorRegex.hasMatch(char));
+    // Adjust the range in the requested direction until it reaches a non-word character. After this we know that the
+    // range ends at the start of the next word upstream or end of the next word downstream from the initial text offset
+    moveWhile((char) => !_separatorRegex.hasMatch(char));
+    // The range now reaches from the start of the string to our new text offset. Calculate that offset using the
+    // range's string length and return it
+    return range.current.length;
+  }
+
+  int? _moveOffsetByCharacter(int textOffset, int characterCount, bool upstream) {
+    if (textOffset < 0 || textOffset > length) {
+      throw Exception("Index '$textOffset' is out of string range. Length: $length");
+    }
+
+    // Create a character range, initially with zero length
+    // Note that the getter for this object is confusingly named: it is an iterator but includes lots of functionality
+    // beyond that interface, most importantly for us a range over this string that can be manipulated in terms of
+    // characters
+    final range = characters.iterator;
+    var remainingOffset = textOffset;
+    // Expand the range so it reaches from the start of the string to the initial text offset. The text offset is passed
+    // to us in terms of code units but the iterator deals in grapheme clusters, so we need to manually count the length
+    // of each cluster until we reach the desired offset
+    range.expandWhile((char) {
+      remainingOffset -= char.length;
+      return remainingOffset >= 0;
+    });
+    // Verify that the move is possible with the requested character count
+    if (upstream ? range.current.length < characterCount : range.stringAfterLength < characterCount) {
       return null;
     }
-
-    return visitedCharacterCodePointLength;
+    // Expand or contract the range by the requested number of characters
+    if (upstream) {
+      range.dropLast(characterCount);
+    } else {
+      range.expandNext(characterCount);
+    }
+    // The range now reaches from the start of the string to our new text offset. Calculate that offset using the
+    // range's string length and return it
+    return range.current.length;
   }
 }
--- super_editor/test/src/infrastructure/strings_test.dart
+++ super_editor/test/src/infrastructure/strings_test.dart
@@ -22,15 +22,64 @@ void main() {
         expect(() => "a💙c".moveOffsetUpstreamByCharacter(5, characterCount: 2), throwsException);
       });
 
-      test("a word", () {
-        expect("move a💙c words".moveOffsetUpstreamByWord(15), 10);
-        expect("move a💙c words".moveOffsetUpstreamByWord(10), 9);
-        expect("move a💙c words".moveOffsetUpstreamByWord(9), 5);
-        expect("move a💙c words".moveOffsetUpstreamByWord(8), 5);
-        expect("move a💙c words".moveOffsetUpstreamByWord(4), 0);
-        expect("move a💙c words".moveOffsetUpstreamByWord(0), null);
-        expect(() => "move a💙c words".moveOffsetUpstreamByWord(-1), throwsException);
-        expect(() => "move a💙c words".moveOffsetUpstreamByWord(16), throwsException);
+      group("a word", () {
+        test('separated by space', () {
+          expect("move a💙c words".moveOffsetUpstreamByWord(15), 10);
+          expect("move a💙c words".moveOffsetUpstreamByWord(10), 5);
+          expect("move a💙c words".moveOffsetUpstreamByWord(9), 5);
+          expect("move a💙c words".moveOffsetUpstreamByWord(8), 5);
+          expect("move a💙c words".moveOffsetUpstreamByWord(4), 0);
+          expect("move a💙c words".moveOffsetUpstreamByWord(0), null);
+          expect(() => "move a💙c words".moveOffsetUpstreamByWord(-1), throwsException);
+          expect(() => "move a💙c words".moveOffsetUpstreamByWord(16), throwsException);
+        });
+
+        test("separated by multiple spaces", () {
+          expect("move   words".moveOffsetUpstreamByWord(12), 7);
+          expect("move   words".moveOffsetUpstreamByWord(7), 0);
+          expect("move   words".moveOffsetUpstreamByWord(6), 0);
+          expect("move   words".moveOffsetUpstreamByWord(4), 0);
+          expect("move   words".moveOffsetUpstreamByWord(0), null);
+          expect(() => "move   words".moveOffsetUpstreamByWord(-1), throwsException);
+          expect(() => "move   words".moveOffsetUpstreamByWord(13), throwsException);
+        });
+
+        test("separated by punctuation", () {
+          expect("move.words".moveOffsetUpstreamByWord(10), 5);
+          expect("move.words".moveOffsetUpstreamByWord(5), 0);
+          expect("move.words".moveOffsetUpstreamByWord(4), 0);
+          expect("move.words".moveOffsetUpstreamByWord(0), null);
+          expect(() => "move.words".moveOffsetUpstreamByWord(-1), throwsException);
+          expect(() => "move.words".moveOffsetUpstreamByWord(11), throwsException);
+        });
+
+        test("separated by punctuation and spaces", () {
+          expect("move. words".moveOffsetUpstreamByWord(11), 6);
+          expect("move. words".moveOffsetUpstreamByWord(6), 0);
+          expect("move. words".moveOffsetUpstreamByWord(5), 0);
+          expect("move. words".moveOffsetUpstreamByWord(4), 0);
+          expect("move. words".moveOffsetUpstreamByWord(0), null);
+          expect(() => "move. words".moveOffsetUpstreamByWord(-1), throwsException);
+          expect(() => "move. words".moveOffsetUpstreamByWord(12), throwsException);
+        });
+
+        test("separated by multi-byte punctuation", () {
+          expect("move\u{10B3F}words".moveOffsetUpstreamByWord(11), 6);
+          expect("move\u{10B3F}words".moveOffsetUpstreamByWord(6), 0);
+          expect("move\u{10B3F}words".moveOffsetUpstreamByWord(4), 0);
+          expect("move\u{10B3F}words".moveOffsetUpstreamByWord(0), null);
+          expect(() => "move\u{10B3F}words".moveOffsetUpstreamByWord(-1), throwsException);
+          expect(() => "move\u{10B3F}words".moveOffsetUpstreamByWord(12), throwsException);
+        });
+
+        test("leading and trailing spaces", () {
+          expect("  move words  ".moveOffsetUpstreamByWord(14), 7);
+          expect("  move words  ".moveOffsetUpstreamByWord(7), 2);
+          expect("  move words  ".moveOffsetUpstreamByWord(2), 0);
+          expect("  move words  ".moveOffsetUpstreamByWord(0), null);
+          expect(() => "move\u{10B3F}words".moveOffsetUpstreamByWord(-1), throwsException);
+          expect(() => "move\u{10B3F}words".moveOffsetUpstreamByWord(15), throwsException);
+        });
       });
     });
 
@@ -53,17 +102,65 @@ void main() {
         expect(() => "a💙c".moveOffsetDownstreamByCharacter(5, characterCount: 2), throwsException);
       });
 
-      test("a word", () {
-        expect("move a💙c words".moveOffsetDownstreamByWord(0), 4);
-        expect("move a💙c words".moveOffsetDownstreamByWord(4), 5);
-        expect("move a💙c words".moveOffsetDownstreamByWord(5), 9);
-        expect("move a💙c words".moveOffsetDownstreamByWord(6), 9);
-        expect("move a💙c words".moveOffsetDownstreamByWord(8), 9);
-        expect("move a💙c words".moveOffsetDownstreamByWord(9), 10);
-        expect("move a💙c words".moveOffsetDownstreamByWord(10), 15);
-        expect("move a💙c words".moveOffsetDownstreamByWord(15), null);
-        expect(() => "move a💙c words".moveOffsetDownstreamByWord(-1), throwsException);
-        expect(() => "move a💙c words".moveOffsetDownstreamByWord(16), throwsException);
+      group('a word', () {
+        test("separated by space", () {
+          expect("move a💙c words".moveOffsetDownstreamByWord(0), 4);
+          expect("move a💙c words".moveOffsetDownstreamByWord(4), 9);
+          expect("move a💙c words".moveOffsetDownstreamByWord(5), 9);
+          expect("move a💙c words".moveOffsetDownstreamByWord(6), 9);
+          expect("move a💙c words".moveOffsetDownstreamByWord(8), 9);
+          expect("move a💙c words".moveOffsetDownstreamByWord(9), 15);
+          expect("move a💙c words".moveOffsetDownstreamByWord(10), 15);
+          expect("move a💙c words".moveOffsetDownstreamByWord(15), null);
+          expect(() => "move a💙c words".moveOffsetDownstreamByWord(-1), throwsException);
+          expect(() => "move a💙c words".moveOffsetDownstreamByWord(16), throwsException);
+        });
+
+        test("separated by multiple spaces", () {
+          expect("move   words".moveOffsetDownstreamByWord(0), 4);
+          expect("move   words".moveOffsetDownstreamByWord(4), 12);
+          expect("move   words".moveOffsetDownstreamByWord(5), 12);
+          expect("move   words".moveOffsetDownstreamByWord(12), null);
+          expect(() => "move   words".moveOffsetDownstreamByWord(-1), throwsException);
+          expect(() => "move   words".moveOffsetDownstreamByWord(13), throwsException);
+        });
+
+        test("separated by punctuation", () {
+          expect("move.words".moveOffsetDownstreamByWord(0), 4);
+          expect("move.words".moveOffsetDownstreamByWord(4), 10);
+          expect("move.words".moveOffsetDownstreamByWord(5), 10);
+          expect("move.words".moveOffsetDownstreamByWord(10), null);
+          expect(() => "move.words".moveOffsetDownstreamByWord(-1), throwsException);
+          expect(() => "move.words".moveOffsetDownstreamByWord(11), throwsException);
+        });
+
+        test("separated by punctuation and spaces", () {
+          expect("move. words".moveOffsetDownstreamByWord(0), 4);
+          expect("move. words".moveOffsetDownstreamByWord(4), 11);
+          expect("move. words".moveOffsetDownstreamByWord(5), 11);
+          expect("move. words".moveOffsetDownstreamByWord(6), 11);
+          expect("move. words".moveOffsetDownstreamByWord(11), null);
+          expect(() => "move. words".moveOffsetDownstreamByWord(-1), throwsException);
+          expect(() => "move. words".moveOffsetDownstreamByWord(12), throwsException);
+        });
+
+        test("separated by multi-byte punctuation", () {
+          expect("move\u{10B3F}words".moveOffsetDownstreamByWord(0), 4);
+          expect("move\u{10B3F}words".moveOffsetDownstreamByWord(4), 11);
+          expect("move\u{10B3F}words".moveOffsetDownstreamByWord(6), 11);
+          expect("move\u{10B3F}words".moveOffsetDownstreamByWord(11), null);
+          expect(() => "move\u{10B3F}words".moveOffsetDownstreamByWord(-1), throwsException);
+          expect(() => "move\u{10B3F}words".moveOffsetDownstreamByWord(12), throwsException);
+        });
+
+        test("leading and trailing spaces", () {
+          expect("  move words  ".moveOffsetDownstreamByWord(0), 6);
+          expect("  move words  ".moveOffsetDownstreamByWord(1), 6);
+          expect("  move words  ".moveOffsetDownstreamByWord(6), 12);
+          expect("  move words  ".moveOffsetDownstreamByWord(14), null);
+          expect(() => "move\u{10B3F}words".moveOffsetDownstreamByWord(-1), throwsException);
+          expect(() => "move\u{10B3F}words".moveOffsetDownstreamByWord(15), throwsException);
+        });
       });
     });
   });
