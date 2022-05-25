491aabf6047faf83006da1a8d6e37bebd37cd6ef
thangvu <thangvnv0806@gmail.com>
2022-05-25 22:52:08 +0700

    Fix test for text_tools.dart

--- super_editor/test/src/default_editor/test_text_tools.dart
+++ super_editor/test/src/default_editor/test_text_tools.dart
@@ -4,25 +4,25 @@ import 'package:super_editor/src/default_editor/text_tools.dart';
 
 void main() {
   group('text_tools.dart', () {
-    test('expand position to word', () {
-      // Notice there is a space at first
-      const text = ' SuperEditor is awesome';
-
-      // Place a collapsed selection at "| SuperEditor is awesome"
-      expect(
-        const TextSelection.collapsed(offset: 0),
-        expandPositionToWord(text: text, textPosition: const TextPosition(offset: 0)),
-      );
-      // Place a collapsed selection at " |SuperEditor is awesome"
-      expect(
-        const TextSelection(baseOffset: 1, extentOffset: 12),
-        expandPositionToWord(text: text, textPosition: const TextPosition(offset: 1)),
-      );
-      // Place a collapsed selection at " SuperEditor| is awesome"
-      expect(
-        const TextSelection(baseOffset: 1, extentOffset: 13),
-        expandPositionToWord(text: text, textPosition: const TextPosition(offset: 12)),
-      );
+    group('expand position to word', () {
+      test('does not expand when there is no word next to the caret', () {
+        // Notice there is a space at first
+        const text = ' SuperEditor is awesome';
+        // Place a collapsed selection at "| SuperEditor is awesome"
+        expect(
+          const TextSelection.collapsed(offset: 0),
+          expandPositionToWord(text: text, textPosition: const TextPosition(offset: 0)),
+        );
+      });
+      test('expand when there is a word next to the caret', () {
+        // Notice there is a space at first
+        const text = ' SuperEditor is awesome';
+        // Place a collapsed selection at " |SuperEditor is awesome"
+        expect(
+          const TextSelection(baseOffset: 1, extentOffset: 12),
+          expandPositionToWord(text: text, textPosition: const TextPosition(offset: 1)),
+        );
+      });
     });
   });
 }
