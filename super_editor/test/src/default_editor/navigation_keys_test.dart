5e0160cf5243d6183ed66c2882b3bd67203d2ebe
Chris D <faithoflifedev@gmail.com>
2022-05-25 16:46:17 -0400

    #351 - code abstractions

--- super_editor/test/src/default_editor/navigation_keys_test.dart
+++ super_editor/test/src/default_editor/navigation_keys_test.dart
@@ -3,358 +3,160 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:super_editor/super_editor.dart';
 
+enum Extent { top, bottom, pgDown, pgUp }
+
+enum ScrollTo { bottom }
+
 void main() {
   group('Navigation Keys', () {
+    double screenWidth = 640.0;
+
+    double screenHeight = 120.0;
+
+    Future<void> _testExtent(WidgetTester tester, Extent extent,
+        {required LogicalKeyboardKey key, bool scrollToBottom = false}) async {
+      final testDocument = TestDocument(tester, screenWidth: screenWidth, screenHeight: screenHeight);
+
+      await testDocument.buildDoc(tester);
+
+      //caret is at begining of doc
+      expect(testDocument.textNodePosition.offset == 0, true);
+
+      if (scrollToBottom) {
+        testDocument.scrollController.jumpTo(testDocument.extentBottom);
+      }
+
+      //activate key press
+      await tester.sendKeyEvent(key);
+
+      await tester.pumpAndSettle();
+
+      final offset = testDocument.scrollController.offset;
+
+      switch (extent) {
+        case Extent.top:
+          //confirm no scroll beyond top
+          expect(offset, equals(testDocument.extentTop));
+          break;
+
+        case Extent.bottom:
+          //confirm no scroll beyond bottom
+          expect(offset, equals(testDocument.extentBottom));
+          break;
+
+        case Extent.pgDown:
+          //confirm scroll down by screenHeight
+          expect(offset, equals(screenHeight));
+          break;
+
+        case Extent.pgUp:
+          //confirm scroll up one page
+          expect(offset, equals(testDocument.extentBottom - screenHeight));
+          break;
+      }
+
+      //caret should not move
+      expect(testDocument.textNodePosition.offset == 0, true);
+    }
+
     group('navigation', () {
-      double pageHeight = 120.0;
-
-      double _realOffset(WidgetTester tester) => tester.state<ScrollableState>(find.byType(Scrollable)).position.pixels;
-
-      void _setScreensize(WidgetTester tester) {
-        final screenSizeWithoutKeyboard = Size(640.0, pageHeight);
-
-        tester.binding.window
-          ..physicalSizeTestValue = screenSizeWithoutKeyboard
-          ..platformDispatcher.textScaleFactorTestValue = 1.0
-          ..devicePixelRatioTestValue = 1.0;
-      }
-
-      Future<void> _testTopExtent(WidgetTester tester, LogicalKeyboardKey key) async {
-        _setScreensize(tester);
-
-        final scrollController = ScrollController();
-
-        final composer = _testComposer();
-
-        await _buildDoc(tester, scrollController: scrollController, documentComposer: composer);
-
-        final extentTop = scrollController.position.minScrollExtent;
-
-        //get caret offset
-        var textNodePosition = composer.selection!.extent.nodePosition as TextNodePosition;
-
-        //caret is at begining of doc
-        expect(textNodePosition.offset == 0, true);
-
-        //confirm start from top of viewport
-        expect(scrollController.offset, equals(extentTop));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //activate pageUp key
-        await tester.sendKeyEvent(key);
-
-        await tester.pumpAndSettle();
-
-        //confirm no scroll beyond bottom
-        expect(scrollController.offset, equals(extentTop));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //caret should not move
-        expect(textNodePosition.offset == 0, true);
-
-        //get caret offset
-        textNodePosition = composer.selection!.extent.nodePosition as TextNodePosition;
-
-        //caret hasn't moved
-        expect(textNodePosition.offset == 0, true);
-      }
-
-      Future<void> _testBottomExtent(WidgetTester tester, LogicalKeyboardKey key) async {
-        _setScreensize(tester);
-
-        final scrollController = ScrollController();
-
-        final composer = _testComposer();
-
-        await _buildDoc(tester, scrollController: scrollController, documentComposer: composer);
-
-        final extentTop = scrollController.position.minScrollExtent;
-
-        final extentBottom = scrollController.position.maxScrollExtent;
-
-        //get caret offset
-        var textNodePosition = composer.selection!.extent.nodePosition as TextNodePosition;
-
-        //caret is at begining of doc
-        expect(textNodePosition.offset == 0, true);
-
-        //confirm start from top of viewport
-        expect(scrollController.offset, equals(extentTop));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //scroll to bottom of doc
-        scrollController.jumpTo(extentBottom);
-
-        //check for scroll to bottom
-        expect(scrollController.offset, equals(extentBottom));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //activate pageDown key
-        await tester.sendKeyEvent(key);
-
-        await tester.pumpAndSettle();
-
-        //confirm no scroll beyond bottom
-        expect(scrollController.offset, equals(extentBottom));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //caret should not move
-        expect(textNodePosition.offset == 0, true);
-
-        //get caret offset
-        textNodePosition = composer.selection!.extent.nodePosition as TextNodePosition;
-
-        //caret hasn't moved
-        expect(textNodePosition.offset == 0, true);
-      }
-
       testWidgets('pageDown key press at bottom of viewport',
-          (tester) async => _testBottomExtent(tester, LogicalKeyboardKey.pageDown));
-
-      testWidgets('pageDown key press at top of viewport', (tester) async {
-        _setScreensize(tester);
-
-        final scrollController = ScrollController();
-
-        final composer = _testComposer();
-
-        await _buildDoc(tester, scrollController: scrollController, documentComposer: composer);
-
-        //get caret offset
-        final extentTop = scrollController.position.minScrollExtent;
-
-        var textNodePosition = composer.selection!.extent.nodePosition as TextNodePosition;
-
-        //caret is at begining of doc
-        expect(textNodePosition.offset == 0, true);
-
-        //confirm start at top of viewport
-        expect(scrollController.offset, equals(extentTop));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //activate pageDown key
-        await tester.sendKeyEvent(LogicalKeyboardKey.pageDown);
-
-        await tester.pumpAndSettle();
-
-        //confirm no scroll beyond bottom
-        expect(scrollController.offset, equals(pageHeight));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //caret should not move
-        expect(textNodePosition.offset == 0, true);
-
-        //get caret offset
-        textNodePosition = composer.selection!.extent.nodePosition as TextNodePosition;
-
-        //caret hasn't moved
-        expect(textNodePosition.offset == 0, true);
-      });
+          (tester) async => _testExtent(tester, Extent.bottom, key: LogicalKeyboardKey.pageDown, scrollToBottom: true));
 
       testWidgets(
-          'pageUp key press at top of viewport', (tester) async => _testTopExtent(tester, LogicalKeyboardKey.pageUp));
+          'pageDown key press at top of viewport',
+          (tester) async => _testExtent(
+                tester,
+                Extent.pgDown,
+                key: LogicalKeyboardKey.pageDown,
+              ));
 
-      testWidgets('pageUp key press at bottom of viewport', (tester) async {
-        _setScreensize(tester);
+      testWidgets('pageUp key press at top of viewport',
+          (tester) async => _testExtent(tester, Extent.top, key: LogicalKeyboardKey.pageUp));
 
-        final scrollController = ScrollController();
+      testWidgets('pageUp key press at bottom of viewport',
+          (tester) async => _testExtent(tester, Extent.pgUp, key: LogicalKeyboardKey.pageUp, scrollToBottom: true));
 
-        final composer = _testComposer();
+      testWidgets('home key press at top of viewport',
+          (tester) async => _testExtent(tester, Extent.top, key: LogicalKeyboardKey.home));
 
-        await _buildDoc(tester, scrollController: scrollController, documentComposer: composer);
+      testWidgets('home key press at bottom of viewport',
+          (tester) async => _testExtent(tester, Extent.top, key: LogicalKeyboardKey.home, scrollToBottom: true));
 
-        final extentTop = scrollController.position.minScrollExtent;
-
-        final extentBottom = scrollController.position.maxScrollExtent;
-
-        //get caret offset
-        var textNodePosition = composer.selection!.extent.nodePosition as TextNodePosition;
-
-        //caret is at begining of doc
-        expect(textNodePosition.offset == 0, true);
-
-        //confirm start from top of viewport
-        expect(scrollController.offset, equals(extentTop));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //scroll to bottom of doc
-        scrollController.jumpTo(extentBottom);
-
-        //check for scroll to bottom
-        expect(scrollController.offset, equals(extentBottom));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //activate pageUp key
-        await tester.sendKeyEvent(LogicalKeyboardKey.pageUp);
-
-        await tester.pumpAndSettle();
-
-        //confirm scroll up one page
-        expect(scrollController.offset, equals(extentBottom - pageHeight));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //caret should not move
-        expect(textNodePosition.offset == 0, true);
-
-        //get caret offset
-        textNodePosition = composer.selection!.extent.nodePosition as TextNodePosition;
-
-        //caret hasn't moved
-        expect(textNodePosition.offset == 0, true);
-      });
+      testWidgets('end key press at bottom of viewport',
+          (tester) async => _testExtent(tester, Extent.bottom, key: LogicalKeyboardKey.end, scrollToBottom: true));
 
       testWidgets(
-          'home key press at top of viewport', (tester) async => _testTopExtent(tester, LogicalKeyboardKey.home));
-
-      testWidgets('home key press at bottom of viewport', (tester) async {
-        _setScreensize(tester);
-
-        final scrollController = ScrollController();
-
-        final composer = _testComposer();
-
-        await _buildDoc(tester, scrollController: scrollController, documentComposer: composer);
-
-        final extentTop = scrollController.position.minScrollExtent;
-
-        final extentBottom = scrollController.position.maxScrollExtent;
-
-        //get caret offset
-        var textNodePosition = composer.selection!.extent.nodePosition as TextNodePosition;
-
-        //caret is at begining of doc
-        expect(textNodePosition.offset == 0, true);
-
-        //confirm start from top of viewport
-        expect(scrollController.offset, equals(extentTop));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //scroll to bottom of doc
-        scrollController.jumpTo(extentBottom);
-
-        //check for scroll to bottom
-        expect(scrollController.offset, equals(extentBottom));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //activate home key
-        await tester.sendKeyEvent(LogicalKeyboardKey.home);
-
-        await tester.pumpAndSettle();
-
-        //confirm scroll to top of page
-        expect(scrollController.offset, equals(extentTop));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //caret should not move
-        expect(textNodePosition.offset == 0, true);
-
-        //get caret offset
-        textNodePosition = composer.selection!.extent.nodePosition as TextNodePosition;
-
-        //caret hasn't moved
-        expect(textNodePosition.offset == 0, true);
-      });
-
-      testWidgets(
-          'end key press at end of viewport', (tester) async => _testBottomExtent(tester, LogicalKeyboardKey.end));
-
-      testWidgets('end key press at top of viewport', (tester) async {
-        _setScreensize(tester);
-
-        final scrollController = ScrollController();
-
-        final composer = _testComposer();
-
-        await _buildDoc(tester, scrollController: scrollController, documentComposer: composer);
-
-        //get caret offset
-        final extentTop = scrollController.position.minScrollExtent;
-
-        final extentBottom = scrollController.position.maxScrollExtent;
-
-        var textNodePosition = composer.selection!.extent.nodePosition as TextNodePosition;
-
-        //caret is at begining of doc
-        expect(textNodePosition.offset == 0, true);
-
-        //confirm start at top of viewport
-        expect(scrollController.offset, equals(extentTop));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //activate pageDown key
-        await tester.sendKeyEvent(LogicalKeyboardKey.end);
-
-        await tester.pumpAndSettle();
-
-        //confirm no scroll beyond bottom
-        expect(scrollController.offset, equals(extentBottom));
-
-        expect(_realOffset(tester), equals(scrollController.offset));
-
-        //caret should not move
-        expect(textNodePosition.offset == 0, true);
-
-        //get caret offset
-        textNodePosition = composer.selection!.extent.nodePosition as TextNodePosition;
-
-        //caret hasn't moved
-        expect(textNodePosition.offset == 0, true);
-      });
+          'end key press at top of viewport',
+          (tester) async => _testExtent(
+                tester,
+                Extent.bottom,
+                key: LogicalKeyboardKey.end,
+              ));
     });
   });
 }
 
-DocumentComposer _testComposer() => DocumentComposer(
-      initialSelection: const DocumentSelection.collapsed(
-        position: DocumentPosition(
-          nodeId: '1',
-          nodePosition: TextNodePosition(offset: 0),
-        ),
-      ),
-    );
+class TestDocument {
+  final WidgetTester tester;
 
-Future<void> _buildDoc(WidgetTester tester,
-    {required ScrollController scrollController, required DocumentComposer documentComposer}) async {
-  await tester.pumpWidget(
-    MaterialApp(
-      home: Scaffold(
-        body: SuperEditor(
-          scrollController: scrollController,
-          editor: DocumentEditor(
-              document: MutableDocument(
-            nodes: [
-              ParagraphNode(
-                  id: '1',
-                  text: AttributedText(
-                      text:
-                          '1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')),
-              ParagraphNode(
-                  id: '2',
-                  text: AttributedText(
-                      text:
-                          '2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')),
-            ],
-          )),
-          inputSource: DocumentInputSource.keyboard,
-          focusNode: FocusNode()..requestFocus(),
-          composer: documentComposer,
-          autofocus: true,
-        ),
+  final double screenWidth;
+
+  final double screenHeight;
+
+  final scrollController = ScrollController();
+
+  final composer = DocumentComposer(
+    initialSelection: const DocumentSelection.collapsed(
+      position: DocumentPosition(
+        nodeId: '1',
+        nodePosition: TextNodePosition(offset: 0),
       ),
     ),
   );
+
+  //get caret offset
+  TextNodePosition get textNodePosition => composer.selection!.extent.nodePosition as TextNodePosition;
+
+  double get extentTop => scrollController.position.minScrollExtent;
+
+  double get extentBottom => scrollController.position.maxScrollExtent;
+
+  TestDocument(this.tester, {required this.screenWidth, required this.screenHeight}) {
+    tester.binding.window
+      ..physicalSizeTestValue = Size(screenWidth, screenHeight)
+      ..platformDispatcher.textScaleFactorTestValue = 1.0
+      ..devicePixelRatioTestValue = 1.0;
+  }
+
+  Future<void> buildDoc(WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: SuperEditor(
+            scrollController: scrollController,
+            editor: DocumentEditor(
+                document: MutableDocument(
+              nodes: [
+                ParagraphNode(
+                    id: '1',
+                    text: AttributedText(
+                        text:
+                            '1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')),
+                ParagraphNode(
+                    id: '2',
+                    text: AttributedText(
+                        text:
+                            '2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')),
+              ],
+            )),
+            inputSource: DocumentInputSource.keyboard,
+            focusNode: FocusNode()..requestFocus(),
+            composer: composer,
+            autofocus: true,
+          ),
+        ),
+      ),
+    );
+  }
 }
