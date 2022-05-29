53baaf7f1130ddf6fa8d403960c7be5be66029dc
fabiancrx <37002358+fabiancrx@users.noreply.github.com>
2022-05-29 02:54:48 -0400

    Fix deprecations from flutter 3 (#595)

--- super_editor/test_goldens/editor/components/_components_test_utils.dart
+++ super_editor/test_goldens/editor/components/_components_test_utils.dart
@@ -5,8 +5,8 @@ void testComponentGolden(String description, Widget componentBuilder, String fil
   testGoldens(description, (tester) async {
     tester.binding.window
       ..physicalSizeTestValue = const Size(600, 400)
-      ..devicePixelRatioTestValue = 1.0
-      ..textScaleFactorTestValue = 1.0;
+      ..devicePixelRatioTestValue = 1.0;
+    tester.binding.platformDispatcher.textScaleFactorTestValue = 1.0;
 
     await tester.pumpWidget(
       MaterialApp(
