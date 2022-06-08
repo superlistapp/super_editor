acc96e2aaacb539d0056434860a7ae7b9f1cbdd0
Miguel Medeiros <31278849+miguelcmedeiros@users.noreply.github.com>
2022-06-08 16:55:45 +0100

    "Fixed" failing test

--- super_editor/test/src/default_editor/document_input_ime_test.dart
+++ super_editor/test/src/default_editor/document_input_ime_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:logging/logging.dart';
 import 'package:super_editor/super_editor.dart';
 
 import '../_document_test_tools.dart';
@@ -166,6 +165,9 @@ void main() {
 
         // Ensure that the empty paragraph now reads "ü".
         expect((editContext.editor.document.nodes[1] as ParagraphNode).text.text, "ü");
+
+        // HACK: why is this making the test to pass?
+        await tester.pump();
       });
     });
 
