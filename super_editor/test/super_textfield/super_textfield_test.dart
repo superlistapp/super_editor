2a17af2c8774edca5588e8dbf363011b854f727e
iapicca <iapicca@gmail.com>
2022-06-23 11:14:03 +0300

    refactor padding's test using testWidgetsOnMobile

--- super_editor/test/super_textfield/super_textfield_test.dart
+++ super_editor/test/super_textfield/super_textfield_test.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:super_editor/super_editor.dart';
 
+import '../test_tools.dart';
+
 void main() {
   group("SuperTextField", () {
     group("configures for", () {
@@ -100,12 +102,168 @@ void main() {
       });
     });
 
-    group("tap & focus for", () {
-      group("android", () {
-        _testPaddingOnMobile(platform: TargetPlatform.android);
-      });
-      group("iOS", () {
-        _testPaddingOnMobile(platform: TargetPlatform.iOS);
+    group("padding for ", () {
+      group("mobile", () {
+        testWidgetsOnMobile(
+            'GIVEN Padding is default '
+            'WHEN tap on SuperTextField '
+            'THEN Focus is requested', (tester) async {
+          final focusNode = FocusNode(debugLabel: 'SuperTextField FocusNode');
+
+          await tester.pumpWidget(
+            _buildScaffold(
+              child: SuperTextField(
+                focusNode: focusNode,
+                lineHeight: 16,
+              ),
+            ),
+          );
+
+          final widgetFinder = find.byType(SuperTextField);
+          final center = tester.getCenter(widgetFinder);
+
+          expect(
+            focusNode.hasPrimaryFocus,
+            isFalse,
+            reason: '`FocusNode` should NOT have focus yet',
+          );
+
+          await tester.tapAt(center);
+          await tester.pumpAndSettle();
+
+          expect(
+            focusNode.hasPrimaryFocus,
+            isTrue,
+            reason: '`FocusNode` should receive focus',
+          );
+        });
+
+        testWidgetsOnMobile('GIVEN Padding != zero ' 'WHEN tap in padded area ' 'THEN Focus is NOT requested',
+            (tester) async {
+          final focusNode = FocusNode(debugLabel: 'SuperTextField FocusNode');
+          const padding = 20.0;
+
+          await tester.pumpWidget(
+            _buildScaffold(
+              child: SuperTextField(
+                focusNode: focusNode,
+                padding: const EdgeInsets.only(right: padding),
+                lineHeight: 16,
+              ),
+            ),
+          );
+
+          final widgetFinder = find.byType(SuperTextField);
+          final size = tester.getSize(widgetFinder);
+          final center = tester.getCenter(widgetFinder);
+          final centerOfPadding = (size.width - padding) / 2;
+          final target = Offset(
+            center.dx + centerOfPadding,
+            center.dy,
+          );
+
+          expect(
+            focusNode.hasPrimaryFocus,
+            isFalse,
+            reason: '`FocusNode` should NOT have focus yet',
+          );
+
+          await tester.tapAt(target);
+          await tester.pumpAndSettle();
+
+          expect(
+            focusNode.hasPrimaryFocus,
+            isFalse,
+            reason: '`FocusNode` should NOT receive focus',
+          );
+        });
+        testWidgetsOnMobile('GIVEN Padding != zero ' 'WHEN tap on Padding child ' 'THEN Focus is requested',
+            (tester) async {
+          final focusNode = FocusNode(debugLabel: 'SuperTextField FocusNode');
+          const padding = 20.0;
+
+          await tester.pumpWidget(
+            _buildScaffold(
+              child: SuperTextField(
+                focusNode: focusNode,
+                padding: const EdgeInsets.only(right: padding),
+                lineHeight: 16,
+              ),
+            ),
+          );
+
+          final widgetFinder = find.byType(SuperTextField);
+          final center = tester.getCenter(widgetFinder);
+
+          expect(
+            focusNode.hasPrimaryFocus,
+            isFalse,
+            reason: '`FocusNode` should NOT have focus yet',
+          );
+
+          await tester.tapAt(center);
+          await tester.pumpAndSettle();
+
+          expect(
+            focusNode.hasPrimaryFocus,
+            isTrue,
+            reason: '`FocusNode` should receive focus',
+          );
+        });
+
+        testWidgetsOnMobile('GIVEN Padding any ' 'WHEN tap next to SuperTextField ' 'THEN Focus is NOT requested',
+            (tester) async {
+          final focusNode = FocusNode(debugLabel: 'SuperTextField FocusNode');
+          const padding = 20.0;
+          const justEnoughToTheSide = .1;
+
+          await tester.pumpWidget(
+            _buildScaffold(
+              child: SuperTextField(
+                focusNode: focusNode,
+                padding: const EdgeInsets.only(right: padding),
+                lineHeight: 16,
+              ),
+            ),
+          );
+
+          final widgetFinder = find.byType(SuperTextField);
+          final center = tester.getCenter(widgetFinder);
+          final size = tester.getSize(widgetFinder);
+          final tapRight = Offset(
+            center.dx + size.width / 2 + justEnoughToTheSide,
+            center.dy,
+          );
+
+          expect(
+            focusNode.hasPrimaryFocus,
+            isFalse,
+            reason: '`FocusNode` should NOT have focus yet',
+          );
+
+          await tester.tapAt(tapRight);
+          await tester.pumpAndSettle();
+
+          expect(
+            focusNode.hasPrimaryFocus,
+            isFalse,
+            reason: '`FocusNode` should NOT receive focus',
+          );
+
+          final tapLeft = Offset(
+            center.dx - size.width / 2 - justEnoughToTheSide,
+            center.dy,
+          );
+
+          await tester.tapAt(tapLeft);
+          await tester.pumpAndSettle();
+
+          expect(
+            focusNode.hasPrimaryFocus,
+            isFalse,
+            reason: '`FocusNode` should NOT receive focus',
+          );
+        });
       });
     });
   });
@@ -119,190 +277,4 @@ Widget _buildScaffold({
       body: child,
     ),
   );
-}
-
-Iterable<void> _testPaddingOnMobile({required TargetPlatform platform}) {
-  return [
-    testWidgets(
-      'GIVEN Padding is default '
-      'WHEN tap on SuperTextField '
-      'THEN Focus is requested',
-      (tester) async {
-        debugDefaultTargetPlatformOverride = platform;
-        final focusNode = FocusNode(debugLabel: 'SuperTextField FocusNode');
-
-        await tester.pumpWidget(
-          _buildScaffold(
-            child: SuperTextField(
-              focusNode: focusNode,
-              lineHeight: 16,
-            ),
-          ),
-        );
-
-        final widgetFinder = find.byType(SuperTextField);
-        final center = tester.getCenter(widgetFinder);
-
-        expect(
-          focusNode.hasPrimaryFocus,
-          isFalse,
-          reason: '`FocusNode` should NOT have focus yet',
-        );
-
-        await tester.tapAt(center);
-        await tester.pumpAndSettle();
-
-        expect(
-          focusNode.hasPrimaryFocus,
-          isTrue,
-          reason: '`FocusNode` should receive focus',
-        );
-
-        debugDefaultTargetPlatformOverride = null;
-      },
-    ),
-    testWidgets(
-      'GIVEN Padding != zero ' 'WHEN tap in padded area ' 'THEN Focus is NOT requested',
-      (tester) async {
-        debugDefaultTargetPlatformOverride = platform;
-
-        final focusNode = FocusNode(debugLabel: 'SuperTextField FocusNode');
-        const padding = 20.0;
-
-        await tester.pumpWidget(
-          _buildScaffold(
-            child: SuperTextField(
-              focusNode: focusNode,
-              padding: const EdgeInsets.only(right: padding),
-              lineHeight: 16,
-            ),
-          ),
-        );
-
-        final widgetFinder = find.byType(SuperTextField);
-        final size = tester.getSize(widgetFinder);
-        final center = tester.getCenter(widgetFinder);
-        final centerOfPadding = (size.width - padding) / 2;
-        final target = Offset(
-          center.dx + centerOfPadding,
-          center.dy,
-        );
-
-        expect(
-          focusNode.hasPrimaryFocus,
-          isFalse,
-          reason: '`FocusNode` should NOT have focus yet',
-        );
-
-        await tester.tapAt(target);
-        await tester.pumpAndSettle();
-
-        expect(
-          focusNode.hasPrimaryFocus,
-          isFalse,
-          reason: '`FocusNode` should NOT receive focus',
-        );
-
-        debugDefaultTargetPlatformOverride = null;
-      },
-    ),
-    testWidgets(
-      'GIVEN Padding != zero ' 'WHEN tap on Padding child ' 'THEN Focus is requested',
-      (tester) async {
-        debugDefaultTargetPlatformOverride = platform;
-
-        final focusNode = FocusNode(debugLabel: 'SuperTextField FocusNode');
-        const padding = 20.0;
-
-        await tester.pumpWidget(
-          _buildScaffold(
-            child: SuperTextField(
-              focusNode: focusNode,
-              padding: const EdgeInsets.only(right: padding),
-              lineHeight: 16,
-            ),
-          ),
-        );
-
-        final widgetFinder = find.byType(SuperTextField);
-        final center = tester.getCenter(widgetFinder);
-
-        expect(
-          focusNode.hasPrimaryFocus,
-          isFalse,
-          reason: '`FocusNode` should NOT have focus yet',
-        );
-
-        await tester.tapAt(center);
-        await tester.pumpAndSettle();
-
-        expect(
-          focusNode.hasPrimaryFocus,
-          isTrue,
-          reason: '`FocusNode` should receive focus',
-        );
-
-        debugDefaultTargetPlatformOverride = null;
-      },
-    ),
-    testWidgets(
-      'GIVEN Padding any ' 'WHEN tap next to SuperTextField ' 'THEN Focus is NOT requested',
-      (tester) async {
-        debugDefaultTargetPlatformOverride = platform;
-
-        final focusNode = FocusNode(debugLabel: 'SuperTextField FocusNode');
-        const padding = 20.0;
-        const justEnoughToTheSide = .1;
-
-        await tester.pumpWidget(
-          _buildScaffold(
-            child: SuperTextField(
-              focusNode: focusNode,
-              padding: const EdgeInsets.only(right: padding),
-              lineHeight: 16,
-            ),
-          ),
-        );
-
-        final widgetFinder = find.byType(SuperTextField);
-        final center = tester.getCenter(widgetFinder);
-        final size = tester.getSize(widgetFinder);
-        final tapRight = Offset(
-          center.dx + size.width / 2 + justEnoughToTheSide,
-          center.dy,
-        );
-
-        expect(
-          focusNode.hasPrimaryFocus,
-          isFalse,
-          reason: '`FocusNode` should NOT have focus yet',
-        );
-
-        await tester.tapAt(tapRight);
-        await tester.pumpAndSettle();
-
-        expect(
-          focusNode.hasPrimaryFocus,
-          isFalse,
-          reason: '`FocusNode` should NOT receive focus',
-        );
-
-        final tapLeft = Offset(
-          center.dx - size.width / 2 - justEnoughToTheSide,
-          center.dy,
-        );
-
-        await tester.tapAt(tapLeft);
-        await tester.pumpAndSettle();
-
-        expect(
-          focusNode.hasPrimaryFocus,
-          isFalse,
-          reason: '`FocusNode` should NOT receive focus',
-        );
-
-        debugDefaultTargetPlatformOverride = null;
-      },
-    ),
-  ];
 }
