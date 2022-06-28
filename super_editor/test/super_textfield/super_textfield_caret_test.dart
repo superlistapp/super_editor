c7e52f344999b79f190d729978331fd5ed2e9f21
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2022-06-27 23:56:12 -0300

    Change blinkcontroller to blink when the ellapsed time is equal to or greater than the flash period and split test into two

--- super_editor/test/super_textfield/super_textfield_caret_test.dart
+++ super_editor/test/super_textfield/super_textfield_caret_test.dart
@@ -7,19 +7,15 @@ import 'package:flutter_test_robots/flutter_test_robots.dart';
 
 import '../test_tools.dart';
 
-void main() {
-  group("SuperTextField", () {
-    testWidgetsOnDesktop("keeps caret solid while typing", (tester) async{
+void main() { 
+  group("SuperTextField", () {    
+    // Duration to switch between visible and invisible
+    const flashPeriod = Duration(milliseconds: 500);
+
+    testWidgetsOnDesktop("caret blinks at rest", (tester) async {
       // Configure BlinkController to animate, otherwise it won't blink
       BlinkController.indeterminateAnimationsEnabled = true;
-
-      // duration to switch between visible and invisible
-      const flashPeriod = Duration(milliseconds: 500);
-      
-      // interval between each key is pressed
-      // here we add one millissecond because BlinkController checks if the 
-      // ellapsed time is bigger than the flash period
-      final typingInterval = (flashPeriod ~/ 2) + const Duration(milliseconds: 1);
+      addTearDown(() => BlinkController.indeterminateAnimationsEnabled = false);
 
       await tester.pumpWidget(
         MaterialApp(
@@ -38,48 +34,65 @@ void main() {
       // Ensure caret is visible at start      
       expect(_isCaretVisible(tester), true);
 
-      // Trigger a frame with an ellapsed time greater than the flashPeriod,
+      // Trigger a frame with an ellapsed time equal to the flashPeriod,
       // so the caret should change from visible to invisible
-      await tester.pump(flashPeriod + const Duration(milliseconds: 1));
+      await tester.pump(flashPeriod);
 
       // Ensure caret is invisible after the flash period
       expect(_isCaretVisible(tester), false);
 
       // Trigger another frame to make caret visible again
-      await tester.pump(flashPeriod + const Duration(milliseconds: 1));
+      await tester.pump(flashPeriod);
 
       // Ensure caret is visible
       expect(_isCaretVisible(tester), true);
+    });
+
+    testWidgetsOnDesktop("keeps caret solid while typing", (tester) async {
+      // Configure BlinkController to animate, otherwise it won't blink
+      BlinkController.indeterminateAnimationsEnabled = true;
+      addTearDown(() => BlinkController.indeterminateAnimationsEnabled = false);      
+      
+      // Interval between each key is pressed.      
+      final typingInterval = (flashPeriod ~/ 2);
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(            
+            body: SuperTextField(              
+              textStyleBuilder: (_) => const TextStyle(fontSize: 16),                        
+            ),
+          ),
+        ),
+      );
+
+      // Press tab to focus SuperTextField
+      await tester.sendKeyEvent(LogicalKeyboardKey.tab);
+      await tester.pump();
      
-      // Type and 'wait' for ~ half a period
+      // Type after half of the flash period
+      await tester.pump(typingInterval);
       await tester.typeKeyboardText("a");
-      await tester.pump(typingInterval);
-
-      // After typing caret should stay visible
+      // Ensure that typing keeps caret visible
       expect(_isCaretVisible(tester), true); 
 
-      // Type and 'wait' for ~ half a period to end the first full period
+      // Type after half of the flash period
+      await tester.pump(typingInterval);
       await tester.typeKeyboardText("b");
-      await tester.pump(typingInterval);
-
-      // Ensure typing prevented caret from disappearing
+      // Ensure that typing keeps caret visible
       expect(_isCaretVisible(tester), true); 
 
-      // Type and 'wait' for ~ half a period
+      // Type after half of the flash period
+      await tester.pump(typingInterval);
       await tester.typeKeyboardText("c");
-      await tester.pump(typingInterval);
-
-      // After typing caret should stay visible
+      // Ensure that typing keeps caret visible
       expect(_isCaretVisible(tester), true); 
 
-      // Type and 'wait' for ~ half a period to end the second full period
+      // Type after half of the flash period
+      await tester.pump(typingInterval);
       await tester.typeKeyboardText("d");
-      await tester.pump(typingInterval);
-
-      // Ensure typing prevented caret from disappearing
+      // Ensure that typing keeps caret visible
       expect(_isCaretVisible(tester), true); 
-
-      BlinkController.indeterminateAnimationsEnabled = false;
     });
   });
 }
