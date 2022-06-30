b58d481977b503a3f3d6878f7e3998a9810c5a99
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2022-06-29 22:30:31 -0300

    PR feedback

--- super_editor/lib/src/infrastructure/super_textfield/super_textfield.dart
+++ super_editor/lib/src/infrastructure/super_textfield/super_textfield.dart
@@ -176,6 +176,26 @@ class SuperTextFieldState extends State<SuperTextField> {
   @visibleForTesting
   ProseTextLayout get textLayout => (_platformFieldKey.currentState as ProseTextBlock).textLayout;
 
+  bool get _isMultiline => widget.minLines != 1 || widget.maxLines != 1;
+
+  SuperTextFieldPlatformConfiguration get _configuration {
+    if (widget.configuration != null) {
+      return widget.configuration!;
+    }
+
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return SuperTextFieldPlatformConfiguration.android;
+      case TargetPlatform.iOS:
+        return SuperTextFieldPlatformConfiguration.iOS;
+      case TargetPlatform.fuchsia:
+      case TargetPlatform.linux:
+      case TargetPlatform.macOS:
+      case TargetPlatform.windows:
+        return SuperTextFieldPlatformConfiguration.desktop;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     switch (_configuration) {
@@ -236,26 +256,6 @@ class SuperTextFieldState extends State<SuperTextField> {
         );
     }
   }
-
-  SuperTextFieldPlatformConfiguration get _configuration {
-    if (widget.configuration != null) {
-      return widget.configuration!;
-    }
-
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        return SuperTextFieldPlatformConfiguration.android;
-      case TargetPlatform.iOS:
-        return SuperTextFieldPlatformConfiguration.iOS;
-      case TargetPlatform.fuchsia:
-      case TargetPlatform.linux:
-      case TargetPlatform.macOS:
-      case TargetPlatform.windows:
-        return SuperTextFieldPlatformConfiguration.desktop;
-    }
-  }
-
-  bool get _isMultiline => widget.minLines != 1 || widget.maxLines != 1;
 }
 
 /// Configures a [SuperTextField] for the given platform.
