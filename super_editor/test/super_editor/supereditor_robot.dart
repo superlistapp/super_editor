81dc61ca33edfb91475c831d3513866b4af4bb72
Matt Carroll <matthew-carroll@users.noreply.github.com>
2022-07-07 16:38:10 -0700

    Made color of mobile handles configurable in SuperEditor (Resolves #425)

--- super_editor/test/super_editor/supereditor_robot.dart
+++ super_editor/test/super_editor/supereditor_robot.dart
@@ -38,6 +38,43 @@ extension SuperEditorRobot on WidgetTester {
     await pumpAndSettle();
   }
 
+  // TODO: de-dup the implementation between placing the caret, double tapping, and tripple tapping
+  Future<void> doubleTapInParagraph(String nodeId, int offset, [Finder? superEditorFinder]) async {
+    late final Finder layoutFinder;
+    if (superEditorFinder != null) {
+      layoutFinder = find.descendant(of: superEditorFinder, matching: find.byType(SingleColumnDocumentLayout));
+    } else {
+      layoutFinder = find.byType(SingleColumnDocumentLayout);
+    }
+    final documentLayoutElement = layoutFinder.evaluate().single as StatefulElement;
+    final documentLayout = documentLayoutElement.state as DocumentLayout;
+
+    // Collect the various text UI artifacts needed to find the
+    // desired caret offset.
+    final textComponentState = documentLayout.getComponentByNodeId(nodeId) as State;
+    final textComponentKey = textComponentState.widget.key as GlobalKey;
+    final textLayout = (textComponentKey.currentState as TextComponentState).textLayout;
+    final textRenderBox = textComponentKey.currentContext!.findRenderObject() as RenderBox;
+
+    // Calculate the global tap position based on the TextLayout and desired
+    // TextPosition.
+    final position = TextPosition(offset: offset);
+    // For the local tap offset, we add a small vertical adjustment downward. This
+    // prevents flaky edge effects, which might occur if we try to tap exactly at the
+    // top of the line. In general, we could use the caret height to choose a vertical
+    // offset, but the caret height is null when the text is empty. So we use a
+    // hard-coded value, instead.
+    final localTapOffset = textLayout.getOffsetForCaret(position) + const Offset(0, 5);
+    final globalTapOffset = localTapOffset + textRenderBox.localToGlobal(Offset.zero);
+
+    // Triple tap the SuperEditor at the desired position.
+    await tapAt(globalTapOffset);
+    await pump(kTapMinTime + const Duration(milliseconds: 1));
+    await tapAt(globalTapOffset);
+
+    await pumpAndSettle();
+  }
+
   Future<void> tripleTapInParagraph(String nodeId, int offset, [Finder? superEditorFinder]) async {
     late final Finder layoutFinder;
     if (superEditorFinder != null) {
