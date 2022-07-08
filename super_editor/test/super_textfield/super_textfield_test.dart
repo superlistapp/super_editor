b2c49245e9107fd893476db580837439babf5631
iapicca <iapicca@gmail.com>
2022-07-08 11:20:22 +0300

    resolve conflicts in super_textfield_test.dart

--- super_editor/test/super_textfield/super_textfield_test.dart
+++ super_editor/test/super_textfield/super_textfield_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:super_editor/super_editor.dart';
+import 'package:super_text_layout/super_text_layout.dart';
 
 import '../test_tools.dart';
 import 'super_textfield_inspector.dart';
@@ -16,9 +17,7 @@ void main() {
 
           await tester.pumpWidget(
             _buildScaffold(
-              child: const SuperTextField(
-                lineHeight: 16,
-              ),
+              child: const SuperTextField(),
             ),
           );
 
@@ -32,7 +31,6 @@ void main() {
             _buildScaffold(
               child: const SuperTextField(
                 configuration: SuperTextFieldPlatformConfiguration.desktop,
-                lineHeight: 16,
               ),
             ),
           );
@@ -47,9 +45,7 @@ void main() {
 
           await tester.pumpWidget(
             _buildScaffold(
-              child: const SuperTextField(
-                lineHeight: 16,
-              ),
+              child: const SuperTextField(),
             ),
           );
 
@@ -63,7 +59,6 @@ void main() {
             _buildScaffold(
               child: const SuperTextField(
                 configuration: SuperTextFieldPlatformConfiguration.android,
-                lineHeight: 16,
               ),
             ),
           );
@@ -78,9 +73,7 @@ void main() {
 
           await tester.pumpWidget(
             _buildScaffold(
-              child: const SuperTextField(
-                lineHeight: 16,
-              ),
+              child: const SuperTextField(),
             ),
           );
 
@@ -94,13 +87,117 @@ void main() {
             _buildScaffold(
               child: const SuperTextField(
                 configuration: SuperTextFieldPlatformConfiguration.iOS,
-                lineHeight: 16,
               ),
             ),
           );
 
           expect(find.byType(SuperIOSTextField), findsOneWidget);
         });
+      });
+    });
+
+    group("on mobile", () {
+      group("configures inner textfield textInputAction for newline when it's multiline", () {
+        testWidgetsOnAndroid('(on Android)', (tester) async {
+          await tester.pumpWidget(
+            _buildScaffold(
+              child: const SuperTextField(
+                minLines: 10,
+                maxLines: 10,
+              ),
+            ),
+          );
+          final innerTextField = tester.widget<SuperAndroidTextField>(find.byType(SuperAndroidTextField).first);
+
+          // Ensure inner textfield action is configured to newline
+          // so we are able to receive new lines
+          expect(innerTextField.textInputAction, TextInputAction.newline);
+        });
+
+        testWidgetsOnIos('(on iOS)', (tester) async {
+          await tester.pumpWidget(
+            _buildScaffold(
+              child: const SuperTextField(
+                minLines: 10,
+                maxLines: 10,
+              ),
+            ),
+          );
+          final innerTextField = tester.widget<SuperIOSTextField>(find.byType(SuperIOSTextField).first);
+
+          // Ensure inner textfield action is configured to newline
+          // so we are able to receive new lines
+          expect(innerTextField.textInputAction, TextInputAction.newline);
+        });
+      });
+
+      group("configures inner textfield textInputAction for done when it's singleline", () {
+        testWidgetsOnAndroid('(on Android)', (tester) async {
+          await tester.pumpWidget(
+            _buildScaffold(
+              child: const SuperTextField(
+                minLines: 1,
+                maxLines: 1,
+              ),
+            ),
+          );
+
+          final innerTextField = tester.widget<SuperAndroidTextField>(find.byType(SuperAndroidTextField).first);
+
+          // Ensure inner textfield action is configured to done
+          // because we should NOT receive new lines
+          expect(innerTextField.textInputAction, TextInputAction.done);
+        });
+
+        testWidgetsOnIos('(on iOS)', (tester) async {
+          await tester.pumpWidget(
+            _buildScaffold(
+              child: const SuperTextField(
+                minLines: 1,
+                maxLines: 1,
+              ),
+            ),
+          );
+          final innerTextField = tester.widget<SuperIOSTextField>(find.byType(SuperIOSTextField).first);
+
+          // Ensure inner textfield action is configured to done
+          // because we should NOT receive new lines
+          expect(innerTextField.textInputAction, TextInputAction.done);
+        });
+      });
+    });
+
+    group("selection", () {
+      testWidgetsOnAllPlatforms("is inserted automatically when the field is initialized with focus", (tester) async {
+        await tester.pumpWidget(
+          _buildScaffold(
+            child: SuperTextField(
+              focusNode: FocusNode()..requestFocus(),
+            ),
+          ),
+        );
+        await tester.pump();
+
+        expect(_isCaretPresent(tester), isTrue);
+      });
+
+      testWidgetsOnAllPlatforms("is inserted automatically when the field is given focus", (tester) async {
+        final focusNode = FocusNode();
+        await tester.pumpWidget(
+          _buildScaffold(
+            child: SuperTextField(
+              focusNode: focusNode,
+            ),
+          ),
+        );
+        await tester.pump();
+
+        expect(_isCaretPresent(tester), isFalse);
+
+        focusNode.requestFocus();
+        await tester.pumpAndSettle();
+
+        expect(_isCaretPresent(tester), isTrue);
       });
     });
 
@@ -164,7 +261,10 @@ void main() {
           );
         });
 
-        testWidgetsOnMobile('receive focus on tap on insets', (tester) async {
+        testWidgetsOnMobile(
+            'GIVEN Padding has Insets '
+            'WHEN tap on insets '
+            'THEN Focus is  requested', (tester) async {
           final focusNode = FocusNode(debugLabel: 'SuperTextField FocusNode');
           const padding = EdgeInsets.only(right: 20);
           await tester.pumpWidget(
@@ -209,7 +309,19 @@ Widget _buildScaffold({
 }) {
   return MaterialApp(
     home: Scaffold(
-      body: child,
+      body: SizedBox(
+        width: 300,
+        child: child,
+      ),
     ),
   );
+}
+
+bool _isCaretPresent(WidgetTester tester) {
+  final caretMatches = find.byType(TextLayoutCaret).evaluate();
+  if (caretMatches.isEmpty) {
+    return false;
+  }
+  final caretState = (caretMatches.single as StatefulElement).state as TextLayoutCaretState;
+  return caretState.isCaretPresent;
 }
