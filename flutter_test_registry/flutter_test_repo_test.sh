edeb6be55b198934631580d0cc9c4b96a8092c70
Matt Carroll <matthew-carroll@users.noreply.github.com>
2022-07-12 11:46:01 -0700

    Update flutter_test_repo_test.sh

--- flutter_test_registry/flutter_test_repo_test.sh
+++ flutter_test_registry/flutter_test_repo_test.sh
@@ -1,3 +1,3 @@
-(cd ../attributed_text; flutter test)
-(cd ../super_editor; flutter test)
-(cd ../super_text_layout; flutter test)
+(cd attributed_text; flutter test)
+(cd super_editor; flutter test)
+(cd super_text_layout; flutter test)
