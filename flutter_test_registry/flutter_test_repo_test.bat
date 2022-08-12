cea038f49390b580b7038bee194ee971de43f772
Matt Carroll <matthew-carroll@users.noreply.github.com>
2022-08-12 16:30:04 -0700

    Updated Flutter test registry scripts to exit on failure (Resolves #728)

--- flutter_test_registry/flutter_test_repo_test.bat
+++ flutter_test_registry/flutter_test_repo_test.bat
@@ -1,11 +1,17 @@
 PUSHD attributed_text
-CALL flutter test
+CALL flutter test || GOTO :END
 POPD
 
 PUSHD super_editor
-CALL flutter test
+CALL flutter test || GOTO :END
 POPD
 
 PUSHD super_text_layout
-CALL flutter test
+CALL flutter test || GOTO :END
 POPD
+
+:END
+@ECHO.
+@ECHO.
+@ECHO Testing failed.
+EXIT /B 1
