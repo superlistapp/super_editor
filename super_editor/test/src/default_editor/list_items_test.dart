7db7fcd84e8fc3715448eda6d7ec9cc754c29f0b
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2022-10-06 02:00:51 -0300

    [SuperEditor] Fix list item style when converting to paragraph and back again (Resolves #795) (#805)

--- super_editor/test/src/default_editor/list_items_test.dart
+++ super_editor/test/src/default_editor/list_items_test.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_test_robots/flutter_test_robots.dart';
 import 'package:super_editor/src/infrastructure/platform_detector.dart';
 import 'package:super_editor/super_editor.dart';
 import 'package:super_editor/super_editor_test.dart';
+import 'package:super_text_layout/super_text_layout.dart';
 
 import '../../super_editor/document_test_tools.dart';
 import '../../test_tools.dart';
@@ -236,6 +238,94 @@ void main() {
 
         Platform.setTestInstance(null);
       });
+
+      testWidgetsOnArbitraryDesktop("applies styles when unordered list item is converted to and from a paragraph",
+          (WidgetTester tester) async {
+        final testContext = await _pumpUnorderedList(
+          tester,
+          styleSheet: _styleSheet,
+        );
+        final doc = SuperEditorInspector.findDocument()!;
+
+        LayoutAwareRichText richText;
+
+        // Ensure that the textStyle for a list item was applied.
+        expect(find.byType(LayoutAwareRichText), findsWidgets);
+        richText = (find.byType(LayoutAwareRichText).evaluate().first.widget) as LayoutAwareRichText;
+        expect(richText.text.style!.color, Colors.blue);
+
+        // Tap to place caret.
+        await tester.placeCaretInParagraph(doc.nodes.first.id, 0);
+
+        // Convert the list item to a paragraph.
+        testContext.editContext.commonOps.convertToParagraph(
+          newMetadata: {
+            'blockType': const NamedAttribution("paragraph"),
+          },
+        );
+        await tester.pumpAndSettle();
+
+        // Ensure that the textStyle for a paragraph was applied.
+        expect(find.byType(LayoutAwareRichText), findsWidgets);
+        richText = (find.byType(LayoutAwareRichText).evaluate().first.widget) as LayoutAwareRichText;
+        expect(richText.text.style!.color, Colors.red);
+
+        // Convert the paragraph back to an unordered list item.
+        testContext.editContext.commonOps.convertToListItem(
+          ListItemType.unordered,
+          (doc.nodes.first as ParagraphNode).text,
+        );
+        await tester.pumpAndSettle();
+
+        // Ensure that the textStyle for a list item was applied.
+        expect(find.byType(LayoutAwareRichText), findsWidgets);
+        richText = (find.byType(LayoutAwareRichText).evaluate().first.widget) as LayoutAwareRichText;
+        expect(richText.text.style!.color, Colors.blue);
+      });
+
+      testWidgetsOnArbitraryDesktop("applies styles when ordered list item is converted to and from a paragraph",
+          (WidgetTester tester) async {
+        final testContext = await _pumpOrderedList(
+          tester,
+          styleSheet: _styleSheet,
+        );
+        final doc = SuperEditorInspector.findDocument()!;
+
+        LayoutAwareRichText richText;
+
+        // Ensure that the textStyle for a list item was applied.
+        expect(find.byType(LayoutAwareRichText), findsWidgets);
+        richText = (find.byType(LayoutAwareRichText).evaluate().first.widget) as LayoutAwareRichText;
+        expect(richText.text.style!.color, Colors.blue);
+
+        // Tap to place caret.
+        await tester.placeCaretInParagraph(doc.nodes.first.id, 0);
+
+        // Convert the list item to a paragraph.
+        testContext.editContext.commonOps.convertToParagraph(
+          newMetadata: {
+            'blockType': const NamedAttribution("paragraph"),
+          },
+        );
+        await tester.pumpAndSettle();
+
+        // Ensure that the textStyle for a paragraph was applied.
+        expect(find.byType(LayoutAwareRichText), findsWidgets);
+        richText = (find.byType(LayoutAwareRichText).evaluate().first.widget) as LayoutAwareRichText;
+        expect(richText.text.style!.color, Colors.red);
+
+        // Convert the paragraph back to an ordered list item.
+        testContext.editContext.commonOps.convertToListItem(
+          ListItemType.ordered,
+          (doc.nodes.first as ParagraphNode).text,
+        );
+        await tester.pumpAndSettle();
+
+        // Ensure that the textStyle for a list item was applied.
+        expect(find.byType(LayoutAwareRichText), findsWidgets);
+        richText = (find.byType(LayoutAwareRichText).evaluate().first.widget) as LayoutAwareRichText;
+        expect(richText.text.style!.color, Colors.blue);
+      });
     });
 
     group('unordered list', () {
@@ -399,16 +489,20 @@ void _typeKeys(EditContext editContext, List<FakeRawKeyEvent> keys) {
 /// The first two items have one level of indentation.
 ///
 /// The last two items have two levels of indentation.
-Future<void> _pumpUnorderedList(WidgetTester tester) async {
+Future<TestDocumentContext> _pumpUnorderedList(
+  WidgetTester tester, {
+  Stylesheet? styleSheet,
+}) async {
   const markdown = '''
  * list item 1
  * list item 2
    * list item 2.1
    * list item 2.2''';
 
-  await tester //
+  return await tester //
       .createDocument()
       .fromMarkdown(markdown)
+      .useStylesheet(styleSheet)
       .pump();
 }
 
@@ -417,15 +511,49 @@ Future<void> _pumpUnorderedList(WidgetTester tester) async {
 /// The first two items have one level of indentation.
 ///
 /// The last two items have two levels of indentation.
-Future<void> _pumpOrderedList(WidgetTester tester) async {
+Future<TestDocumentContext> _pumpOrderedList(
+  WidgetTester tester, {
+  Stylesheet? styleSheet,
+}) async {
   const markdown = '''
  1. list item 1
  1. list item 2
     1. list item 2.1
     1. list item 2.2''';
 
-  await tester //
+  return await tester //
       .createDocument()
       .fromMarkdown(markdown)
+      .useStylesheet(styleSheet)
       .pump();
 }
+
+TextStyle _inlineTextStyler(Set<Attribution> attributions, TextStyle base) => base;
+
+final _styleSheet = Stylesheet(
+  inlineTextStyler: _inlineTextStyler,
+  rules: [
+    StyleRule(
+      const BlockSelector("paragraph"),
+      (doc, docNode) {
+        return {
+          "textStyle": const TextStyle(
+            color: Colors.red,
+            fontSize: 16,
+          ),
+        };
+      },
+    ),
+    StyleRule(
+      const BlockSelector("listItem"),
+      (doc, docNode) {
+        return {
+          "textStyle": const TextStyle(
+            color: Colors.blue,
+            fontSize: 16,
+          ),
+        };
+      },
+    ),
+  ],
+);
--- super_editor/test/super_editor/document_test_tools.dart
+++ super_editor/test/super_editor/document_test_tools.dart
@@ -215,7 +215,7 @@ class TestDocumentConfigurator {
   }
 
   /// Configures the [SuperEditor] to use the given [stylesheet].
-  TestDocumentConfigurator useStylesheet(Stylesheet stylesheet) {
+  TestDocumentConfigurator useStylesheet(Stylesheet? stylesheet) {
     _stylesheet = stylesheet;
     return this;
   }
