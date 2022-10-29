d9660d7899f622a5e37cb72c8aa0295f75c3fa8a
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2022-10-29 12:36:39 -0300

    Add Tests

--- super_editor/test/super_editor/supereditor_style_test.dart
+++ super_editor/test/super_editor/supereditor_style_test.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:super_editor/src/default_editor/attributions.dart';
+import 'package:flutter_test_robots/flutter_test_robots.dart';
+import 'package:super_editor/super_editor.dart';
 import 'package:super_editor/super_editor_test.dart';
 import 'package:super_text_layout/super_text_layout.dart';
 
@@ -28,6 +31,58 @@ void main() {
         FontStyle.italic,
       );
     });
+
+    testWidgetsOnArbitraryDesktop('changes visual text style when style rule changes', (tester) async {
+      final testContext = await tester //
+          .createDocument()
+          .withTwoEmptyParagraphs()
+          .useStylesheet(_stylesheet)
+          .pump();
+
+      final doc = testContext.editContext.editor.document;
+
+      final firstParagraphId = doc.nodes[0].id;
+      final secondParagraphId = doc.nodes[1].id;
+
+      // Ensure the rule for paragraph is applied.
+      expect(SuperEditorInspector.findParagraphStyle(firstParagraphId)!.color, Colors.red);
+
+      // Remove the second paragraph.
+      testContext.editContext.editor.executeCommand(
+        DeleteNodeCommand(nodeId: secondParagraphId),
+      );
+      await tester.pump();
+
+      // The first paragraph is now the only paragraph in the document.
+      // Therefore, the rule for "last paragraph" should be applied.
+      expect(SuperEditorInspector.findParagraphStyle(firstParagraphId)!.color, Colors.blue);
+    });
+
+    testWidgetsOnArbitraryDesktop('rebuilds only changed nodes', (tester) async {
+      int componentChangedCount = 0;
+
+      await tester
+          .createDocument() //
+          .withLongTextContent()
+          .pump();
+
+      await tester.placeCaretInParagraph(SuperEditorInspector.findDocument()!.nodes.last.id, 0);
+
+      final presenter = tester.state<SuperEditorState>(find.byType(SuperEditor)).presenter;
+      presenter.addChangeListener(SingleColumnLayoutPresenterChangeListener(
+        onViewModelChange: ({required addedComponents, required changedComponents, required removedComponents}) {
+          // The listener is called two times. The first one for the text change,
+          // and the second one for the selection change.
+          componentChangedCount = max(componentChangedCount, changedComponents.length);
+        },
+      ));
+
+      // Type text, changing only one node.
+      await tester.typeKeyboardText('a');
+
+      // Ensure only the changed component was marked as dirty.
+      expect(componentChangedCount, 1);
+    });
   });
 }
 
@@ -37,4 +92,33 @@ InlineSpan _findSpanAtOffset(
 }) {
   final superTextWithSelection = tester.widget<SuperTextWithSelection>(find.byType(SuperTextWithSelection));
   return superTextWithSelection.richText.getSpanForPosition(TextPosition(offset: offset))!;
+}
+
+final _stylesheet = Stylesheet(
+  inlineTextStyler: inlineTextStyler,
+  rules: [
+    StyleRule(
+      const BlockSelector("paragraph"),
+      (doc, docNode) {
+        return {
+          "textStyle": const TextStyle(
+            color: Colors.red,
+          ),
+        };
+      },
+    ),
+    StyleRule(
+      const BlockSelector("paragraph").last(),
+      (doc, docNode) {
+        return {
+          "textStyle": const TextStyle(
+            color: Colors.blue,
+          )
+        };
+      },
+    ),
+  ],
+);
+TextStyle inlineTextStyler(Set<Attribution> attributions, TextStyle base) {
+  return base;
 }
