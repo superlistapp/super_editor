104f749773da18b532dddc9739f70ad36246de24
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2022-12-19 09:14:59 -0300

    [SuperEditor] Avoid scrolling when dragging over an image (Resolves #812) (#866)

--- super_editor/test/super_editor/supereditor_scrolling_test.dart
+++ super_editor/test/super_editor/supereditor_scrolling_test.dart
@@ -253,5 +253,57 @@ void main() {
       // Ensure the editor didn't scroll.
       expect(scrollController.position.pixels, 0.0);
     });
+
+    testWidgetsOnArbitraryDesktop("doesn't scroll when dragging over an image", (tester) async {
+      const editorSize = Size(300, 300);
+
+      await tester
+          .createDocument()
+          .withCustomContent(
+            MutableDocument(
+              nodes: [
+                ParagraphNode(
+                  id: "1",
+                  text: AttributedText(text: "First Paragraph"),
+                ),
+                ParagraphNode(
+                  id: "2",
+                  text: AttributedText(text: "Second Paragraph"),
+                ),
+                ImageNode(
+                  id: "img-node",
+                  imageUrl: 'https://this.is.a.fake.image',
+                  metadata: const SingleColumnLayoutComponentStyles(
+                    width: double.infinity,
+                  ).toMetadata(),
+                ),
+              ],
+            ),
+          )
+          .withAddedComponents([const FakeImageComponentBuilder(size: editorSize)])
+          .withEditorSize(editorSize)
+          .pump();
+
+      // Drag from the second paragraph to the image.
+      await tester.dragSelectDocumentFromPositionByOffset(
+        from: const DocumentPosition(
+          nodeId: '2',
+          nodePosition: TextNodePosition(offset: 1),
+        ),
+        delta: const Offset(0, 50),
+      );
+
+      // Ensure the bottom of the image isn't visible.
+      expect(
+        SuperEditorInspector.isPositionVisibleGlobally(
+          const DocumentPosition(
+            nodeId: 'img-node',
+            nodePosition: UpstreamDownstreamNodePosition.downstream(),
+          ),
+          editorSize,
+        ),
+        false,
+      );
+    });
   });
 }
