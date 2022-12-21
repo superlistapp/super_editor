da1092cd5e4a7abb4c7fb55b3d8660f1d498d684
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2022-12-20 22:09:05 -0300

    Extract base class for AndroidDocumentGestureEditingController and IosDocumentGestureEditingController

--- super_editor/lib/src/infrastructure/platforms/ios/ios_document_controls.dart
+++ super_editor/lib/src/infrastructure/platforms/ios/ios_document_controls.dart
@@ -477,22 +477,16 @@ class _IosDocumentTouchEditingControlsState extends State<IosDocumentTouchEditin
 /// Controls the display of drag handles, a magnifier, and a
 /// floating toolbar, assuming iOS-style behavior for the
 /// handles.
-class IosDocumentGestureEditingController with ChangeNotifier {
+class IosDocumentGestureEditingController extends GestureEditingController {
   IosDocumentGestureEditingController({
     required LayerLink documentLayoutLink,
     required LayerLink magnifierFocalPointLink,
     required MagnifierAndToolbarController overlayController,
   })  : _documentLayoutLink = documentLayoutLink,
-        _magnifierFocalPointLink = magnifierFocalPointLink,
-        _overlayController = overlayController {
-    _overlayController.addListener(_toolbarChanged);
-  }
-
-  @override
-  void dispose() {
-    _overlayController.removeListener(_toolbarChanged);
-    super.dispose();
-  }
+        super(
+          magnifierFocalPointLink: magnifierFocalPointLink,
+          overlayController: overlayController,
+        );
 
   /// Layer link that's aligned to the top-left corner of the document layout.
   ///
@@ -503,11 +497,6 @@ class IosDocumentGestureEditingController with ChangeNotifier {
   /// document layout.
   LayerLink get documentLayoutLink => _documentLayoutLink;
   final LayerLink _documentLayoutLink;
-
-  /// A `LayerLink` whose top-left corner sits at the location where the
-  /// magnifier should magnify.
-  LayerLink get magnifierFocalPointLink => _magnifierFocalPointLink;
-  final LayerLink _magnifierFocalPointLink;
 
   /// Whether or not a caret should be displayed.
   bool get hasCaret => caretTop != null;
@@ -524,17 +513,6 @@ class IosDocumentGestureEditingController with ChangeNotifier {
   /// The height of the caret, or `null` if no caret should be displayed.
   double? get caretHeight => _caretHeight;
   double? _caretHeight;
-
-  /// Controls the magnifier and the toolbar.
-  MagnifierAndToolbarController get overlayController => _overlayController;
-  late MagnifierAndToolbarController _overlayController;
-  set overlayController(MagnifierAndToolbarController value) {
-    if (_overlayController != value) {
-      _overlayController.removeListener(_toolbarChanged);
-      _overlayController = value;
-      _overlayController.addListener(_toolbarChanged);
-    }
-  }
 
   /// Updates the caret's size and position.
   ///
@@ -624,75 +602,6 @@ class IosDocumentGestureEditingController with ChangeNotifier {
       _downstreamHandleOffset = offset;
       notifyListeners();
     }
-  }
-
-  /// Whether the toolbar currently has a designated display position.
-  ///
-  /// The toolbar should not be displayed if this is `false`, even if
-  /// [shouldDisplayToolbar] is `true`.
-  bool get isToolbarPositioned => _overlayController.isToolbarPositioned;
-
-  /// Whether the toolbar should be displayed.
-  bool get shouldDisplayToolbar => _overlayController.shouldDisplayToolbar;
-
-  /// Whether the magnifier should be displayed.
-  bool get shouldDisplayMagnifier => _overlayController.shouldDisplayMagnifier;
-
-  /// The point about which the floating toolbar should focus, when the toolbar
-  /// appears above the selected content.
-  ///
-  /// It's the clients responsibility to determine whether there's room for the
-  /// toolbar above this point. If not, use [toolbarBottomAnchor].
-  Offset? get toolbarTopAnchor => _overlayController.toolbarTopAnchor;
-
-  /// The point about which the floating toolbar should focus, when the toolbar
-  /// appears below the selected content.
-  ///
-  /// It's the clients responsibility to determine whether there's room for the
-  /// toolbar below this point. If not, use [toolbarTopAnchor].
-  Offset? get toolbarBottomAnchor => _overlayController.toolbarBottomAnchor;
-
-  /// Shows the toolbar, and hides the magnifier.
-  void showToolbar() {
-    _overlayController.showToolbar();
-  }
-
-  /// Hides the toolbar.
-  void hideToolbar() {
-    _overlayController.hideToolbar();
-  }
-
-  /// Shows the magnify, and hides the toolbar.
-  void showMagnifier() {
-    _overlayController.showMagnifier();
-  }
-
-  /// Hides the magnifier.
-  void hideMagnifier() {
-    _overlayController.hideMagnifier();
-  }
-
-  /// Toggles the toolbar from visible to not visible, or vis-a-versa.
-  void toggleToolbar() {
-    _overlayController.toggleToolbar();
-  }
-
-  /// Sets the toolbar's position to the given [topAnchor] and [bottomAnchor].
-  ///
-  /// Setting the position will not cause the toolbar to be displayed on it's own.
-  /// To display the toolbar, call [showToolbar], too.
-  void positionToolbar({
-    required Offset topAnchor,
-    required Offset bottomAnchor,
-  }) {
-    _overlayController.positionToolbar(
-      topAnchor: topAnchor,
-      bottomAnchor: bottomAnchor,
-    );
-  }
-
-  void _toolbarChanged() {
-    notifyListeners();
   }
 }
 
