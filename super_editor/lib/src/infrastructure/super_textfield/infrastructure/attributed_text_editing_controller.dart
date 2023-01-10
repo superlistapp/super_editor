039f6060880b21211497a1e9a4473bc7cc85df4a
Matt Carroll <matthew-carroll@users.noreply.github.com>
2023-01-09 20:26:19 -0800

    Fixed SuperTextField selection glitch by never resending the previous TextEditingValue to the IME (Resolves #905) (#910)

--- super_editor/lib/src/infrastructure/super_textfield/infrastructure/attributed_text_editing_controller.dart
+++ super_editor/lib/src/infrastructure/super_textfield/infrastructure/attributed_text_editing_controller.dart
@@ -152,6 +152,10 @@ class AttributedTextEditingController with ChangeNotifier {
     required AttributedText text,
     required TextSelection selection,
   }) {
+    if (text == _text && selection == _selection) {
+      return;
+    }
+
     _switchText(text);
     _selection = selection;
 
@@ -583,7 +587,7 @@ class AttributedTextEditingController with ChangeNotifier {
       text: updatedText,
       selection: updatedSelection,
     );
-    
+
     _updateComposingAttributions();
     // TODO: do we need to implement composing region update behavior like selections?
     composingRegion = newComposingRegion ?? TextRange.empty;
@@ -625,6 +629,13 @@ class AttributedTextEditingController with ChangeNotifier {
     TextSelection? selection,
     TextRange? composingRegion,
   }) {
+    if ((text == null || text == _text) &&
+        (selection == null || selection == _selection) &&
+        (composingRegion == null || composingRegion == _composingRegion)) {
+      // The updated values are the same as existing values. Do nothing.
+      return;
+    }
+
     if (text != null) {
       _switchText(text);
     }
