cc3f4a6a81eeb7dc01ceeae556b77e4bb572ba61
Miguel Medeiros <31278849+miguelcmedeiros@users.noreply.github.com>
2023-01-20 02:40:10 +0000

    [SuperTextField] Implement missing methods in ImeAttributedTextEditingController (#977)
    Implement missing methods in ImeAttributedTextEditingController to forward to the real controller

--- super_editor/lib/src/infrastructure/super_textfield/input_method_engine/_ime_text_editing_controller.dart
+++ super_editor/lib/src/infrastructure/super_textfield/input_method_engine/_ime_text_editing_controller.dart
@@ -1,8 +1,10 @@
 import 'package:attributed_text/attributed_text.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
+import 'package:super_editor/src/core/document_layout.dart';
 import 'package:super_editor/src/infrastructure/attributed_text_styles.dart';
 import 'package:super_editor/src/infrastructure/super_textfield/super_textfield.dart';
+import 'package:super_text_layout/super_text_layout.dart';
 
 import '../../_logging.dart';
 
@@ -535,6 +537,69 @@ class ImeAttributedTextEditingController extends AttributedTextEditingController
   @override
   void clear() {
     _realController.clear();
+  }
+
+  @override
+  void deleteCharacter(TextAffinity direction) {
+    _realController.deleteCharacter(direction);
+  }
+
+  @override
+  void copySelectedTextToClipboard() {
+    _realController.copySelectedTextToClipboard();
+  }
+
+  @override
+  void deleteSelectedText() {
+    _realController.deleteSelectedText();
+  }
+
+  @override
+  void deleteTextOnLineBeforeCaret({required ProseTextLayout textLayout}) {
+    _realController.deleteTextOnLineBeforeCaret(textLayout: textLayout);
+  }
+
+  @override
+  void insertCharacter(String character) {
+    _realController.insertCharacter(character);
+  }
+
+  @override
+  void moveCaretHorizontally({
+    required ProseTextLayout textLayout,
+    required bool expandSelection,
+    required bool moveLeft,
+    required MovementModifier? movementModifier,
+  }) {
+    _realController.moveCaretHorizontally(
+      textLayout: textLayout,
+      expandSelection: expandSelection,
+      moveLeft: moveLeft,
+      movementModifier: movementModifier,
+    );
+  }
+
+  @override
+  void moveCaretVertically({
+    required ProseTextLayout textLayout,
+    required bool expandSelection,
+    required bool moveUp,
+  }) {
+    _realController.moveCaretVertically(
+      textLayout: textLayout,
+      expandSelection: expandSelection,
+      moveUp: moveUp,
+    );
+  }
+
+  @override
+  Future<void> pasteClipboard() {
+    return _realController.pasteClipboard();
+  }
+
+  @override
+  void selectAll() {
+    _realController.selectAll();
   }
 }
 
