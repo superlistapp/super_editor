00881d0715b8f0523b2f49d54faebbd749e2bbda
Miguel Medeiros <31278849+miguelcmedeiros@users.noreply.github.com>
2023-03-12 21:59:15 +0000

    Add a demo where the height of the content (e.g. task) can animate (#1056)

---
+++ super_editor/example/lib/demos/demo_animated_task_height.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+import 'package:super_editor/super_editor.dart';
+
+/// Example of a task component whose height is animated.
+class AnimatedTaskHeightDemo extends StatefulWidget {
+  @override
+  _AnimatedTaskHeightDemoState createState() => _AnimatedTaskHeightDemoState();
+}
+
+class _AnimatedTaskHeightDemoState extends State<AnimatedTaskHeightDemo> {
+  late MutableDocument _doc;
+  late DocumentEditor _docEditor;
+
+  @override
+  void initState() {
+    super.initState();
+    _doc = _createDocument();
+    _docEditor = DocumentEditor(document: _doc);
+  }
+
+  @override
+  void dispose() {
+    _doc.dispose();
+    super.dispose();
+  }
+
+  MutableDocument _createDocument() {
+    return MutableDocument(
+      nodes: [
+        ParagraphNode(
+          id: DocumentEditor.createNodeId(),
+          text: AttributedText(
+            text:
+                "Below are several tasks. These tasks will animate the appearance of a subtitle depending on whether they have selection. Try and find out:",
+          ),
+        ),
+        ...List.generate(
+          10,
+          (index) => TaskNode(
+            id: DocumentEditor.createNodeId(),
+            text: AttributedText(text: "Task ${index + 1}"),
+            isComplete: false,
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SuperEditor(
+      editor: _docEditor,
+      stylesheet: defaultStylesheet.copyWith(
+        documentPadding: const EdgeInsets.symmetric(vertical: 56, horizontal: 24),
+      ),
+      // Add a new component builder that creates a task that animates its height,
+      // instead of creating the usual static kind.
+      componentBuilders: [
+        const AnimatedTaskComponentBuilder(),
+        TaskComponentBuilder(_docEditor),
+        ...defaultComponentBuilders,
+      ],
+    );
+  }
+}
+
+/// SuperEditor [ComponentBuilder] that builds a task that is animates the appearance of
+/// a subtitle depending on whether it has selection.
+class AnimatedTaskComponentBuilder implements ComponentBuilder {
+  const AnimatedTaskComponentBuilder();
+
+  @override
+  SingleColumnLayoutComponentViewModel? createViewModel(Document document, DocumentNode node) {
+    // This builder can work with the standard task view model, so
+    // we'll defer to the standard task builder.
+    return null;
+  }
+
+  @override
+  Widget? createComponent(
+      SingleColumnDocumentComponentContext componentContext, SingleColumnLayoutComponentViewModel componentViewModel) {
+    if (componentViewModel is! TaskComponentViewModel) {
+      return null;
+    }
+
+    return _AnimatedTaskComponent(
+      key: componentContext.componentKey,
+      viewModel: componentViewModel,
+    );
+  }
+}
+
+class _AnimatedTaskComponent extends StatefulWidget {
+  const _AnimatedTaskComponent({
+    Key? key,
+    required this.viewModel,
+    this.showDebugPaint = false,
+  }) : super(key: key);
+
+  final TaskComponentViewModel viewModel;
+  final bool showDebugPaint;
+
+  @override
+  State<_AnimatedTaskComponent> createState() => _AnimatedTaskComponentState();
+}
+
+class _AnimatedTaskComponentState extends State<_AnimatedTaskComponent>
+    with ProxyDocumentComponent<_AnimatedTaskComponent>, ProxyTextComposable {
+  final _textKey = GlobalKey();
+
+  @override
+  GlobalKey<State<StatefulWidget>> get childDocumentComponentKey => _textKey;
+
+  @override
+  TextComposable get childTextComposable => childDocumentComponentKey.currentState as TextComposable;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 4),
+              child: Checkbox(
+                value: widget.viewModel.isComplete,
+                onChanged: (newValue) {
+                  widget.viewModel.setComplete(newValue!);
+                },
+              ),
+            ),
+            Expanded(
+              child: TextComponent(
+                key: _textKey,
+                text: widget.viewModel.text,
+                textStyleBuilder: (attributions) {
+                  // Show a strikethrough across the entire task if it's complete.
+                  final style = widget.viewModel.textStyleBuilder(attributions);
+                  return widget.viewModel.isComplete
+                      ? style.copyWith(
+                          decoration: style.decoration == null
+                              ? TextDecoration.lineThrough
+                              : TextDecoration.combine([TextDecoration.lineThrough, style.decoration!]),
+                        )
+                      : style;
+                },
+                textSelection: widget.viewModel.selection,
+                selectionColor: widget.viewModel.selectionColor,
+                highlightWhenEmpty: widget.viewModel.highlightWhenEmpty,
+                showDebugPaint: widget.showDebugPaint,
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 56),
+          child: AnimatedSize(
+            duration: const Duration(milliseconds: 100),
+            child: widget.viewModel.selection != null
+                ? SizedBox(
+                    height: 20,
+                    child: Row(
+                      children: [
+                        Icon(Icons.label_important_outline, size: 16),
+                        const SizedBox(width: 4),
+                        Icon(Icons.timelapse_sharp, size: 16),
+                      ],
+                    ),
+                  )
+                : const SizedBox.shrink(),
+          ),
+        ),
+      ],
+    );
+  }
+}
