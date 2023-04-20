83299eb95744afcb1fc29ff9c80d6f63c26fa02e
Matt Carroll <matthew-carroll@users.noreply.github.com>
2023-04-19 18:15:12 -0700

    Make super_text_layout releasable again (Resolves #1083) (#1092)

--- super_editor/test/super_editor/supereditor_text_layout_test.dart
+++ super_editor/test/super_editor/supereditor_text_layout_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:super_editor/super_editor_test.dart';
-import 'package:super_text_layout/super_text_layout.dart';
+import 'package:super_text_layout/super_text_layout_inspector.dart';
 
 import '../test_tools.dart';
 import 'document_test_tools.dart';
