5cfc404c5caf92519aafde3738c6c8fd8d4f75fa
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2023-04-26 21:44:44 -0300

    Use golden toolkit to build test scenarios

--- super_editor/test_goldens/super_textfield/super_textfield_scroll_test.dart
+++ super_editor/test_goldens/super_textfield/super_textfield_scroll_test.dart
@@ -5,103 +5,127 @@ import 'package:super_editor/super_editor.dart';
 
 void main() {
   group('SuperTextField', () {
-    testGoldens("multi-line accounts for padding when jumping scroll position vertically (on Android)", (tester) async {
+    testGoldens("multi-line accounts for padding when jumping scroll position down", (tester) async {
       final controller = AttributedTextEditingController(
         text: AttributedText(text: "First line\nSecond Line\nThird Line\nFourth Line"),
       );
 
-      // Pump the widget tree with a SuperTextField which is two lines tall.
-      await _pumpTestApp(
-        tester,
-        textController: controller,
-        minLines: 1,
-        maxLines: 2,
-        maxHeight: 50,
-        maxWidth: 200,
-        padding: const EdgeInsets.all(10.0),
-        configuration: SuperTextFieldPlatformConfiguration.android,
-      );
+      // Use a Row as a wrapper to fill the available width.
+      final builder = GoldenBuilder.column(
+        wrap: (child) => Row(
+          children: [child],
+        ),
+      )
+        ..addScenario(
+          'SuperTextField with padding, scrolled to the end (on Android)',
+          _buildTextField(
+            textController: controller,
+            minLines: 1,
+            maxLines: 2,
+            maxHeight: 50,
+            maxWidth: 200,
+            padding: const EdgeInsets.all(10.0),
+            configuration: SuperTextFieldPlatformConfiguration.android,
+          ),
+        )
+        ..addScenario(
+          'SuperTextField with padding, scrolled to the end (on iOS)',
+          _buildTextField(
+            textController: controller,
+            minLines: 1,
+            maxLines: 2,
+            maxHeight: 50,
+            maxWidth: 200,
+            padding: const EdgeInsets.all(10.0),
+            configuration: SuperTextFieldPlatformConfiguration.iOS,
+          ),
+        )
+        ..addScenario(
+          'SuperTextField with padding, scrolled to the end (on Desktop)',
+          _buildTextField(
+            textController: controller,
+            minLines: 1,
+            maxLines: 2,
+            maxHeight: 50,
+            maxWidth: 200,
+            padding: const EdgeInsets.all(10.0),
+            configuration: SuperTextFieldPlatformConfiguration.desktop,
+          ),
+        );
+      await tester.pumpWidgetBuilder(builder.build());
 
       // Move selection to the end of the text.
       // This will scroll the text field to the end.
       controller.selection = const TextSelection.collapsed(offset: 45);
       await tester.pumpAndSettle();
 
-      await screenMatchesGolden(tester, 'super_textfield_scrolled_down_android');
-
-      // Place the caret at the beginning of the text.
-      controller.selection = const TextSelection.collapsed(offset: 0);
-      await tester.pumpAndSettle();
-
-      await screenMatchesGolden(tester, 'super_textfield_scrolled_up_android');
+      await screenMatchesGolden(tester, 'super_textfield_scrolled_down');
     });
 
-    testGoldens("multi-line accounts for padding when jumping scroll position vertically (on iOS)", (tester) async {
+    testGoldens("multi-line accounts for padding when jumping scroll position up", (tester) async {
       final controller = AttributedTextEditingController(
         text: AttributedText(text: "First line\nSecond Line\nThird Line\nFourth Line"),
       );
 
-      // Pump the widget tree with a SuperTextField which is two lines tall.
-      await _pumpTestApp(
-        tester,
-        textController: controller,
-        minLines: 1,
-        maxLines: 2,
-        maxHeight: 50,
-        maxWidth: 200,
-        padding: const EdgeInsets.all(10.0),
-        configuration: SuperTextFieldPlatformConfiguration.iOS,
-      );
+      // Use a Row as a wrapper to fill the available width.
+      final builder = GoldenBuilder.column(
+        wrap: (child) => Row(
+          children: [child],
+        ),
+      )
+        ..addScenario(
+          'SuperTextField with padding, scrolled to the beginning (on Android)',
+          _buildTextField(
+            textController: controller,
+            minLines: 1,
+            maxLines: 2,
+            maxHeight: 50,
+            maxWidth: 200,
+            padding: const EdgeInsets.all(10.0),
+            configuration: SuperTextFieldPlatformConfiguration.android,
+          ),
+        )
+        ..addScenario(
+          'SuperTextField with padding, scrolled to the beginning (on iOS)',
+          _buildTextField(
+            textController: controller,
+            minLines: 1,
+            maxLines: 2,
+            maxHeight: 50,
+            maxWidth: 200,
+            padding: const EdgeInsets.all(10.0),
+            configuration: SuperTextFieldPlatformConfiguration.iOS,
+          ),
+        )
+        ..addScenario(
+          'SuperTextField with padding, scrolled to the beginning (on Desktop)',
+          _buildTextField(
+            textController: controller,
+            minLines: 1,
+            maxLines: 2,
+            maxHeight: 50,
+            maxWidth: 200,
+            padding: const EdgeInsets.all(10.0),
+            configuration: SuperTextFieldPlatformConfiguration.desktop,
+          ),
+        );
+      await tester.pumpWidgetBuilder(builder.build());
 
       // Move selection to the end of the text.
       // This will scroll the text field to the end.
       controller.selection = const TextSelection.collapsed(offset: 45);
       await tester.pumpAndSettle();
 
-      await screenMatchesGolden(tester, 'super_textfield_scrolled_down_ios');
-
       // Place the caret at the beginning of the text.
       controller.selection = const TextSelection.collapsed(offset: 0);
       await tester.pumpAndSettle();
 
-      await screenMatchesGolden(tester, 'super_textfield_scrolled_up_ios');
-    });
-
-    testGoldens("multi-line accounts for padding when jumping scroll position vertically (on iOS)", (tester) async {
-      final controller = AttributedTextEditingController(
-        text: AttributedText(text: "First line\nSecond Line\nThird Line\nFourth Line"),
-      );
-
-      // Pump the widget tree with a SuperTextField which is two lines tall.
-      await _pumpTestApp(
-        tester,
-        textController: controller,
-        minLines: 1,
-        maxLines: 2,
-        maxHeight: 50,
-        maxWidth: 200,
-        padding: const EdgeInsets.all(10.0),
-        configuration: SuperTextFieldPlatformConfiguration.desktop,
-      );
-
-      // Move selection to the end of the text.
-      // This will scroll the text field to the end.
-      controller.selection = const TextSelection.collapsed(offset: 45);
-      await tester.pumpAndSettle();
-
-      await screenMatchesGolden(tester, 'super_textfield_scrolled_down_desktop');
-
-      // Place the caret at the beginning of the text.
-      controller.selection = const TextSelection.collapsed(offset: 0);
-      await tester.pumpAndSettle();
-
-      await screenMatchesGolden(tester, 'super_textfield_scrolled_up_desktop');
+      await screenMatchesGolden(tester, 'super_textfield_scrolled_up');
     });
   });
 }
 
-Future<void> _pumpTestApp(
-  WidgetTester tester, {
+Widget _buildTextField({
   required AttributedTextEditingController textController,
   required int minLines,
   required int maxLines,
@@ -109,38 +133,26 @@ Future<void> _pumpTestApp(
   double? maxHeight,
   EdgeInsets? padding,
   SuperTextFieldPlatformConfiguration? configuration,
-}) async {
-  await tester.pumpWidget(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              maxWidth: maxWidth ?? double.infinity,
-              maxHeight: maxHeight ?? double.infinity,
-            ),
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                color: Colors.yellow,
-                border: Border.all(),
-              ),
-              child: SuperTextField(
-                textController: textController,
-                lineHeight: 20,
-                textStyleBuilder: (_) => const TextStyle(fontSize: 20, color: Colors.black, fontFamily: 'Roboto'),
-                minLines: minLines,
-                maxLines: maxLines,
-                padding: padding,
-                configuration: configuration,
-              ),
-            ),
-          ),
-        ),
+}) {
+  return ConstrainedBox(
+    constraints: BoxConstraints(
+      maxWidth: maxWidth ?? double.infinity,
+      maxHeight: maxHeight ?? double.infinity,
+    ),
+    child: DecoratedBox(
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        border: Border.all(),
+      ),
+      child: SuperTextField(
+        textController: textController,
+        lineHeight: 20,
+        textStyleBuilder: (_) => const TextStyle(fontSize: 20, color: Colors.black, fontFamily: 'Roboto'),
+        minLines: minLines,
+        maxLines: maxLines,
+        padding: padding,
+        configuration: configuration,
       ),
     ),
   );
-
-  // The first frame might have a zero viewport height. Pump a second frame to account for the final viewport size.
-  await tester.pump();
 }
