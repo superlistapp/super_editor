c05eaf8bd55655e39445b3208b574d1bfeb0e0c0
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2023-04-28 17:59:07 -0300

    [SuperTextField][Mobile] Fix scrolling when changing the selection to a visible position (Resolves #1096) (#1101)

--- super_editor/test/super_textfield/super_textfield_inspector.dart
+++ super_editor/test/super_textfield/super_textfield_inspector.dart
@@ -34,5 +34,83 @@ class SuperTextFieldInspector {
     return state.controller.selection;
   }
 
+  /// Finds and returns the scroll offset within a [SuperTextField].
+  ///
+  /// {@macro supertextfield_finder}
+  static double? findScrollOffset([Finder? superTextFieldFinder]) {
+    final finder = superTextFieldFinder ?? find.byType(SuperTextField);
+
+    final fieldFinder = findInnerPlatformTextField(finder);
+    final match = fieldFinder.evaluate().single.widget;
+
+    if (match is SuperDesktopTextField) {
+      final textScrollViewElement = find
+          .descendant(
+            of: finder,
+            matching: find.byType(SuperTextFieldScrollview),
+          )
+          .evaluate()
+          .single as StatefulElement;
+      final textScrollView = textScrollViewElement.widget as SuperTextFieldScrollview;
+
+      return textScrollView.scrollController.offset;
+    }
+
+    // Both mobile textfields use TextScrollView.
+    final textScrollViewElement = find
+        .descendant(
+          of: finder,
+          matching: find.byType(TextScrollView),
+        )
+        .evaluate()
+        .single as StatefulElement;
+    final textScrollView = textScrollViewElement.widget as TextScrollView;
+
+    return textScrollView.textScrollController.scrollOffset;
+  }
+
+  /// Finds and returns the platform textfield within a [SuperTextField].
+  ///
+  /// {@macro supertextfield_finder}
+  static Finder findInnerPlatformTextField([Finder? superTextFieldFinder]) {
+    final rootFieldFinder = superTextFieldFinder ?? find.byType(SuperTextField);
+
+    final rootMatches = rootFieldFinder.evaluate();
+    if (rootMatches.isEmpty) {
+      throw Exception("Couldn't find a super text field variant with the given finder: $rootFieldFinder");
+    }
+    if (rootMatches.length > 1) {
+      throw Exception("Found more than 1 super text field match with finder: $rootFieldFinder");
+    }
+
+    final rootMatch = rootMatches.single.widget;
+    if (rootMatch is! SuperTextField) {
+      // The match isn't a generic SuperTextField. Assume that it's a platform
+      // specific super text field, which is what we're looking for. Return it.
+      return rootFieldFinder;
+    }
+
+    final desktopFieldCandidates =
+        find.descendant(of: rootFieldFinder, matching: find.byType(SuperDesktopTextField)).evaluate();
+    if (desktopFieldCandidates.isNotEmpty) {
+      return find.descendant(of: rootFieldFinder, matching: find.byType(SuperDesktopTextField));
+    }
+
+    final androidFieldCandidates =
+        find.descendant(of: rootFieldFinder, matching: find.byType(SuperAndroidTextField)).evaluate();
+    if (androidFieldCandidates.isNotEmpty) {
+      return find.descendant(of: rootFieldFinder, matching: find.byType(SuperAndroidTextField));
+    }
+
+    final iosFieldCandidates =
+        find.descendant(of: rootFieldFinder, matching: find.byType(SuperIOSTextField)).evaluate();
+    if (iosFieldCandidates.isNotEmpty) {
+      return find.descendant(of: rootFieldFinder, matching: find.byType(SuperIOSTextField));
+    }
+
+    throw Exception(
+        "Couldn't find the platform-specific super text field within the root SuperTextField. Root finder: $rootFieldFinder");
+  }
+
   SuperTextFieldInspector._();
 }
---
+++ super_editor/test_goldens/super_textfield/super_textfield_scroll_test.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:super_editor/super_editor.dart';
+
+void main() {
+  group('SuperTextField', () {
+    testGoldens("multi-line accounts for padding when jumping scroll position down", (tester) async {
+      final controller = AttributedTextEditingController(
+        text: AttributedText(text: "First line\nSecond Line\nThird Line\nFourth Line"),
+      );
+
+      const description =
+          'A SuperTextField scrolled to the end should have the last line fully visible with space below it';
+
+      // Use a Row as a wrapper to fill the available width.
+      final builder = GoldenBuilder.column(
+        wrap: (child) => Row(
+          children: [child],
+        ),
+      )
+        ..addScenario(
+          '$description (on Android)',
+          _buildTextField(
+            textController: controller,
+            minLines: 1,
+            maxLines: 2,
+            maxHeight: 50,
+            maxWidth: 200,
+            padding: const EdgeInsets.all(10.0),
+            configuration: SuperTextFieldPlatformConfiguration.android,
+          ),
+        )
+        ..addScenario(
+          '$description (on iOS)',
+          _buildTextField(
+            textController: controller,
+            minLines: 1,
+            maxLines: 2,
+            maxHeight: 50,
+            maxWidth: 200,
+            padding: const EdgeInsets.all(10.0),
+            configuration: SuperTextFieldPlatformConfiguration.iOS,
+          ),
+        )
+        ..addScenario(
+          '$description  (on Desktop)',
+          _buildTextField(
+            textController: controller,
+            minLines: 1,
+            maxLines: 2,
+            maxHeight: 50,
+            maxWidth: 200,
+            padding: const EdgeInsets.all(10.0),
+            configuration: SuperTextFieldPlatformConfiguration.desktop,
+          ),
+        );
+      await tester.pumpWidgetBuilder(builder.build());
+
+      // Move selection to the end of the text.
+      // This will scroll the text field to the end.
+      controller.selection = const TextSelection.collapsed(offset: 45);
+      await tester.pumpAndSettle();
+
+      await screenMatchesGolden(tester, 'super_textfield_scrolled_down');
+    });
+
+    testGoldens("multi-line accounts for padding when jumping scroll position up", (tester) async {
+      final controller = AttributedTextEditingController(
+        text: AttributedText(text: "First line\nSecond Line\nThird Line\nFourth Line"),
+      );
+
+      const description =
+          'A SuperTextField scrolled to the beginning should have the first line fully visible with space above it';
+
+      // Use a Row as a wrapper to fill the available width.
+      final builder = GoldenBuilder.column(
+        wrap: (child) => Row(
+          children: [child],
+        ),
+      )
+        ..addScenario(
+          '$description (on Android)',
+          _buildTextField(
+            textController: controller,
+            minLines: 1,
+            maxLines: 2,
+            maxHeight: 50,
+            maxWidth: 200,
+            padding: const EdgeInsets.all(10.0),
+            configuration: SuperTextFieldPlatformConfiguration.android,
+          ),
+        )
+        ..addScenario(
+          '$description (on iOS)',
+          _buildTextField(
+            textController: controller,
+            minLines: 1,
+            maxLines: 2,
+            maxHeight: 50,
+            maxWidth: 200,
+            padding: const EdgeInsets.all(10.0),
+            configuration: SuperTextFieldPlatformConfiguration.iOS,
+          ),
+        )
+        ..addScenario(
+          '$description (on Desktop)',
+          _buildTextField(
+            textController: controller,
+            minLines: 1,
+            maxLines: 2,
+            maxHeight: 50,
+            maxWidth: 200,
+            padding: const EdgeInsets.all(10.0),
+            configuration: SuperTextFieldPlatformConfiguration.desktop,
+          ),
+        );
+      await tester.pumpWidgetBuilder(builder.build());
+
+      // Move selection to the end of the text.
+      // This will scroll the text field to the end.
+      controller.selection = const TextSelection.collapsed(offset: 45);
+      await tester.pumpAndSettle();
+
+      // Place the caret at the beginning of the text.
+      controller.selection = const TextSelection.collapsed(offset: 0);
+      await tester.pumpAndSettle();
+
+      await screenMatchesGolden(tester, 'super_textfield_scrolled_up');
+    });
+  });
+}
+
+Widget _buildTextField({
+  required AttributedTextEditingController textController,
+  required int minLines,
+  required int maxLines,
+  double? maxWidth,
+  double? maxHeight,
+  EdgeInsets? padding,
+  SuperTextFieldPlatformConfiguration? configuration,
+}) {
+  return ConstrainedBox(
+    constraints: BoxConstraints(
+      maxWidth: maxWidth ?? double.infinity,
+      maxHeight: maxHeight ?? double.infinity,
+    ),
+    child: DecoratedBox(
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        border: Border.all(),
+      ),
+      child: SuperTextField(
+        textController: textController,
+        lineHeight: 20,
+        textStyleBuilder: (_) => const TextStyle(fontSize: 20, color: Colors.black, fontFamily: 'Roboto'),
+        minLines: minLines,
+        maxLines: maxLines,
+        padding: padding,
+        configuration: configuration,
+      ),
+    ),
+  );
+}
