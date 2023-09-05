2eb9bd55257b27ee78e0b3935a040bee16f8bdd5
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2023-09-05 20:46:58 -0300

    Unify tests

--- super_editor/test/super_editor/supereditor_copy_and_paste_test.dart
+++ super_editor/test/super_editor/supereditor_copy_and_paste_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_test_robots/flutter_test_robots.dart';
 import 'package:flutter_test_runners/flutter_test_runners.dart';
@@ -58,7 +59,7 @@ void main() {
       expect(SuperEditorInspector.findTextInParagraph(nodeId).text, "Pasted text: This was pasted here");
     });
 
-    testAllInputsOnMac('pastes multiple paragraphs', (
+    testAllInputsOnDesktop('pastes multiple paragraphs', (
       tester, {
       required TextInputSource inputSource,
     }) async {
@@ -77,36 +78,11 @@ void main() {
         ..setSimulatedClipboardContent('''This is a paragraph
 This is a second paragraph
 This is the third paragraph''');
-      await tester.pressCmdV();
-
-      // Ensure three paragraphs were created.
-      final doc = testContext.document;
-      expect(doc.nodes.length, 3);
-      expect((doc.nodes[0] as ParagraphNode).text.text, 'This is a paragraph');
-      expect((doc.nodes[1] as ParagraphNode).text.text, 'This is a second paragraph');
-      expect((doc.nodes[2] as ParagraphNode).text.text, 'This is the third paragraph');
-    });
-
-    testAllInputsOnWindowsAndLinux('pastes multiple paragraphs', (
-      tester, {
-      required TextInputSource inputSource,
-    }) async {
-      final testContext = await tester //
-          .createDocument()
-          .withSingleEmptyParagraph()
-          .withInputSource(inputSource)
-          .pump();
-
-      // Place the caret at the empty paragraph.
-      await tester.placeCaretInParagraph('1', 0);
-
-      // Simulate pasting multiple lines.
-      tester
-        ..simulateClipboard()
-        ..setSimulatedClipboardContent('''This is a paragraph
-This is a second paragraph
-This is the third paragraph''');
-      await tester.pressCtlV();
+      if (defaultTargetPlatform == TargetPlatform.macOS) {
+        await tester.pressCmdV();
+      } else {
+        await tester.pressCtlV();
+      }
 
       // Ensure three paragraphs were created.
       final doc = testContext.document;
