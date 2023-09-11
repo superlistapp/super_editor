f7afddb4760a93c196219525c2b82998b1cf7d26
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2023-09-11 17:25:24 -0300

    [SuperReader] Migrate to DocumentScroller API (Resolves #1306) (#1423)

--- super_editor/lib/src/super_reader/super_reader.dart
+++ super_editor/lib/src/super_reader/super_reader.dart
@@ -266,7 +266,7 @@ class SuperReaderState extends State<SuperReader> {
       document: widget.document,
       getDocumentLayout: () => _docLayoutKey.currentState as DocumentLayout,
       selection: _selection,
-      scrollController: _autoScrollController,
+      scroller: _scroller,
     );
 
     _contentTapDelegate?.dispose();
@@ -345,7 +345,6 @@ class SuperReaderState extends State<SuperReader> {
         gestureBuilder: _buildGestureInteractor,
         scrollController: _scrollController,
         autoScrollController: _autoScrollController,
-        // TODO: Finish integrating the DocumentScroller in SuperReader (https://github.com/superlistapp/super_editor/issues/1306)
         scroller: _scroller,
         presenter: _docLayoutPresenter!,
         componentBuilders: widget.componentBuilders,
