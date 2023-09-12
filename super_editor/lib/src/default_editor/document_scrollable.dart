f4aae4776ee55afc7ffb00fc5b83abd4a2bc2301
Rutvik Tak <65209850+rutvik110@users.noreply.github.com>
2023-09-12 09:47:43 +0530

    update

--- super_editor/lib/src/default_editor/document_scrollable.dart
+++ super_editor/lib/src/default_editor/document_scrollable.dart
@@ -387,14 +387,10 @@ class AutoScrollController with ChangeNotifier {
       return;
     }
 
-    // There's no content to scroll. So, we can't go ballistic as it would result
-    // in overscroll.
-    if (pos.maxScrollExtent == 0) {
-      return;
-    }
-
     if (pos is ScrollPositionWithSingleContext) {
-      pos.goBallistic(pixelsPerSecond);
+      if (pos.maxScrollExtent > 0) {
+        pos.goBallistic(pixelsPerSecond);
+      }
       pos.context.setIgnorePointer(false);
     }
   }
