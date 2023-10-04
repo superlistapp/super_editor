bb118cf1cf7675788e00e07426b463887f08d12b
Matt Carroll <matthew-carroll@users.noreply.github.com>
2023-10-04 10:59:19 -0700

    Add iOS tap in empty space to show toolbar, Android long-press in empty space to show toolbar. (Resolves #1472) (#1476)

---
+++ super_editor/test/super_textfield/android/super_textfield_android_selection_test.dart
@@ -0,0 +1,107 @@
+import 'package:attributed_text/attributed_text.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test_runners/flutter_test_runners.dart';
+import 'package:super_editor/src/infrastructure/platforms/android/toolbar.dart';
+import 'package:super_editor/super_text_field.dart';
+
+import '../super_textfield_inspector.dart';
+
+void main() {
+  group("SuperTextField Android selection >", () {
+    testWidgetsOnAndroid("long-pressing in empty space shows the toolbar", (tester) async {
+      await _pumpTestApp(tester);
+
+      // Ensure there's no selection to begin with, and no toolbar is displayed.
+      expect(SuperTextFieldInspector.findSelection(), const TextSelection.collapsed(offset: -1));
+      expect(find.byType(AndroidTextEditingFloatingToolbar), findsNothing);
+
+      // Place the caret at the end of the text by tapping in empty space at the center
+      // of the text field.
+      await tester.tap(find.byType(SuperTextField));
+      await tester.pumpAndSettle(kDoubleTapTimeout);
+      expect(SuperTextFieldInspector.findSelection(), const TextSelection.collapsed(offset: 3));
+      expect(find.byType(AndroidTextEditingFloatingToolbar), findsNothing);
+
+      // Long-press in empty space at the center of the text field.
+      await tester.longPress(find.byType(SuperTextField));
+      await tester.pumpAndSettle(kDoubleTapTimeout);
+
+      // Ensure that the text field toolbar is visible.
+      expect(find.byType(AndroidTextEditingFloatingToolbar), findsOneWidget);
+
+      // Tap again to hide the toolbar.
+      await tester.tap(find.byType(SuperTextField));
+      await tester.pumpAndSettle(kDoubleTapTimeout);
+
+      // Ensure that the text field toolbar disappeared.
+      expect(find.byType(AndroidTextEditingFloatingToolbar), findsNothing);
+    });
+
+    testWidgetsOnAndroid("long-pressing in empty space when there is NO selection does NOT show the toolbar",
+        (tester) async {
+      await _pumpTestApp(tester);
+
+      // Ensure there's no selection to begin with, and no toolbar is displayed.
+      expect(SuperTextFieldInspector.findSelection(), const TextSelection.collapsed(offset: -1));
+      expect(find.byType(AndroidTextEditingFloatingToolbar), findsNothing);
+
+      // Long-press in empty space at the center of the text field.
+      await tester.longPress(find.byType(SuperTextField));
+      await tester.pumpAndSettle(kDoubleTapTimeout);
+
+      // Ensure that no toolbar is displayed.
+      expect(find.byType(AndroidTextEditingFloatingToolbar), findsNothing);
+    });
+  });
+}
+
+Future<void> _pumpTestApp(
+  WidgetTester tester, {
+  AttributedTextEditingController? controller,
+  EdgeInsets? padding,
+  TextAlign? textAlign,
+}) async {
+  final textFieldFocusNode = FocusNode();
+  const tapRegionGroupdId = "test_super_text_field";
+
+  await tester.pumpWidget(
+    MaterialApp(
+      home: Scaffold(
+        body: TapRegion(
+          groupId: tapRegionGroupdId,
+          onTapOutside: (_) {
+            // Unfocus on tap outside so that we're sure that all gesture tests
+            // pass when using TapRegion's for focus, because apps should be able
+            // to do that.
+            textFieldFocusNode.unfocus();
+          },
+          child: SizedBox.expand(
+            child: Align(
+              alignment: Alignment.center,
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 250),
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: SuperTextField(
+                    focusNode: textFieldFocusNode,
+                    tapRegionGroupId: tapRegionGroupdId,
+                    padding: padding,
+                    textAlign: textAlign ?? TextAlign.left,
+                    textController: controller ??
+                        AttributedTextEditingController(
+                          text: AttributedText('abc'),
+                        ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
