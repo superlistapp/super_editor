4aa6f47074e4e8211dce7f731435d7828d87111a
Matt Carroll <matthew-carroll@users.noreply.github.com>
2023-10-14 10:14:52 -0700

    Brought selected text color control to SuperReader - added golden tests for SuperEdtiro and SuperReader (Resolves #1488) (#1506)

---
+++ super_editor/test_goldens/editor/supereditor_selection_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:super_editor/src/test/super_editor_test/supereditor_robot.dart';
+import 'package:super_editor/super_editor.dart';
+
+import '../../test/super_editor/supereditor_test_tools.dart';
+import '../test_tools_goldens.dart';
+
+void main() {
+  group("SuperEditor selection >", () {
+    group("color >", () {
+      testGoldensOnMac("default selection color", (tester) async {
+        await tester //
+            .createDocument()
+            .withSingleParagraph()
+            .pump();
+
+        // Select the whole paragraph so that the selection color is clearly visible.
+        await tester.tripleTapInParagraph("1", 0);
+
+        await screenMatchesGolden(tester, "super-editor_selection-color_default");
+      });
+
+      testGoldensOnMac("custom selection color", (tester) async {
+        await tester //
+            .createDocument()
+            .withSingleParagraph()
+            .withSelectionStyles(const SelectionStyles(selectionColor: Colors.deepPurple))
+            .useStylesheet(defaultStylesheet.copyWith(
+              selectedTextColorStrategy: ({
+                required Color originalTextColor,
+                required Color selectionHighlightColor,
+              }) =>
+                  Colors.white,
+            ))
+            .pump();
+
+        // Select the whole paragraph so that the selection color is clearly visible.
+        await tester.tripleTapInParagraph("1", 0);
+
+        await screenMatchesGolden(tester, "super-editor_selection-color_custom");
+      });
+    });
+  });
+}
