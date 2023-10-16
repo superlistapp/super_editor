ac6ea262c4a46abac5e58625ba11d38e4d8d6e65
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2023-10-16 18:56:59 -0300

    Remove import

--- super_editor/test/super_editor/supereditor_test_tools.dart
+++ super_editor/test/super_editor/supereditor_test_tools.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 import 'dart:ui' as ui;
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
