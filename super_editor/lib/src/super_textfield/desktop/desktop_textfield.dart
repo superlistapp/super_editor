79564e321c89e62f2ebce9467e14ff31028b3b37
Rutvik Tak <65209850+rutvik110@users.noreply.github.com>
2023-10-18 12:04:43 +0530

    reverted unwanted change while rebasing against main

--- super_editor/lib/src/super_textfield/desktop/desktop_textfield.dart
+++ super_editor/lib/src/super_textfield/desktop/desktop_textfield.dart
@@ -1804,7 +1804,6 @@ const defaultTextFieldImeKeyboardHandlers = <TextFieldKeyboardHandler>[
   DefaultSuperTextFieldKeyboardHandlers.scrollOnCtrlOrCmdAndEndKeyPress,
   DefaultSuperTextFieldKeyboardHandlers.scrollOnHomeOnMacOrWeb,
   DefaultSuperTextFieldKeyboardHandlers.scrollOnEndOnMacOrWeb,
-  DefaultSuperTextFieldKeyboardHandlers.insertNewlineWhenEnterIsPressed,
   DefaultSuperTextFieldKeyboardHandlers.moveCaretToStartOrEnd,
   DefaultSuperTextFieldKeyboardHandlers.moveUpDownLeftAndRightWithArrowKeys,
   DefaultSuperTextFieldKeyboardHandlers.moveToLineStartWithHome,
