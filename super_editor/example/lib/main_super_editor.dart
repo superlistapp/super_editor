f39937054ab5df0f033800941395ea35827ab75d
Matt Carroll <matthew-carroll@users.noreply.github.com>
2023-10-24 20:26:02 -0700

    Fix: iOS and magnifier not appearing when expected (Resolves #1550) (#1559)

--- super_editor/example/lib/main_super_editor.dart
+++ super_editor/example/lib/main_super_editor.dart
@@ -1,3 +1,4 @@
+import 'package:example/demos/example_editor/_example_document.dart';
 import 'package:example/demos/example_editor/example_editor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -45,4 +46,53 @@ void main() {
       debugShowCheckedModeBanner: false,
     ),
   );
+}
+
+class _StandardEditor extends StatefulWidget {
+  const _StandardEditor();
+
+  @override
+  State<_StandardEditor> createState() => _StandardEditorState();
+}
+
+class _StandardEditorState extends State<_StandardEditor> {
+  final GlobalKey _docLayoutKey = GlobalKey();
+
+  late MutableDocument _doc;
+  late MutableDocumentComposer _composer;
+  late Editor _docEditor;
+
+  late FocusNode _editorFocusNode;
+
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _doc = createInitialDocument();
+    _composer = MutableDocumentComposer();
+    _docEditor = createDefaultDocumentEditor(document: _doc, composer: _composer);
+    _editorFocusNode = FocusNode();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    _editorFocusNode.dispose();
+    _composer.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SuperEditor(
+      editor: _docEditor,
+      document: _doc,
+      composer: _composer,
+      focusNode: _editorFocusNode,
+      scrollController: _scrollController,
+      documentLayoutKey: _docLayoutKey,
+    );
+  }
 }
