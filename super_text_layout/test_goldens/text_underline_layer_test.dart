03005304ace234a72f4c69fd1f35aa28bb8dccd9
Matt Carroll <matthew-carroll@users.noreply.github.com>
2023-12-01 13:32:12 -0800

    [SuperText] - Added an underline layer (Resolves #1634) (#1635)

---
+++ super_text_layout/test_goldens/text_underline_layer_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:super_text_layout/src/text_underline_layer.dart';
+
+import 'test_tools.dart';
+import 'test_tools_goldens.dart';
+
+void main() {
+  testGoldensOnAndroid("Underline layer paints an underline", (tester) async {
+    await pumpThreeLinePlainSuperText(
+      tester,
+      beneathBuilder: (context, textLayout) {
+        return TextUnderlineLayer(
+          textLayout: textLayout,
+          underlines: const [
+            TextLayoutUnderline(
+              style: UnderlineStyle(
+                color: Colors.lightBlue,
+                thickness: 4,
+              ),
+              range: TextSelection(
+                baseOffset: 36,
+                extentOffset: 79,
+              ),
+            ),
+            TextLayoutUnderline(
+              style: UnderlineStyle(
+                color: Colors.lightBlue,
+                thickness: 4,
+              ),
+              range: TextSelection(
+                baseOffset: 88,
+                extentOffset: 110,
+              ),
+            ),
+          ],
+        );
+      },
+    );
+
+    await screenMatchesGolden(tester, "TextUnderlineLayer_paints-underline");
+  });
+}
