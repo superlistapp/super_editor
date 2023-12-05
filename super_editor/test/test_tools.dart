f8f19b9a06f65d5c74ae94320486c0e045ac6c75
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2023-12-05 00:54:37 -0300

    [SuperEditor][iOS] Add tests for drag to select (Resolves #1613) (#1627)

--- super_editor/test/test_tools.dart
+++ super_editor/test/test_tools.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:super_editor/src/core/document_selection.dart';
 import 'package:super_editor/src/infrastructure/links.dart';
 
 /// A [UrlLauncher] that logs each attempt to launch a URL, but doesn't
@@ -44,5 +45,76 @@ extension DragExtensions on WidgetTester {
     }
 
     return dragGesture;
+  }
+}
+
+/// Compares two selections, ignoring selection affinities.
+///
+/// Some node positions, like [TextNodePosition], have a concept of affinity (upstream/downstream),
+/// which is used when making particular selection decisions, but doesn't impact equivalency.
+Matcher selectionEquivalentTo(DocumentSelection expectedSelection) => EquivalentSelectionMatcher(expectedSelection);
+
+/// A [Matcher] that compares two selections, ignoring selection affinities.
+///
+/// Some node positions, like [TextNodePosition], have a concept of affinity (upstream/downstream),
+/// which is used when making particular selection decisions, but doesn't impact equivalency.
+class EquivalentSelectionMatcher extends Matcher {
+  EquivalentSelectionMatcher(
+    this.expectedSelection,
+  );
+
+  final DocumentSelection expectedSelection;
+
+  @override
+  Description describe(Description description) {
+    return description.add("given selection is equivalent to expected selection");
+  }
+
+  @override
+  bool matches(covariant Object target, Map<dynamic, dynamic> matchState) {
+    return _calculateMismatchReason(target, matchState) == null;
+  }
+
+  @override
+  Description describeMismatch(
+    covariant Object target,
+    Description mismatchDescription,
+    Map matchState,
+    bool verbose,
+  ) {
+    final mismatchReason = _calculateMismatchReason(target, matchState);
+    if (mismatchReason != null) {
+      mismatchDescription.add(mismatchReason);
+    }
+    return mismatchDescription;
+  }
+
+  String? _calculateMismatchReason(
+    Object target,
+    Map<dynamic, dynamic> matchState,
+  ) {
+    if (target is! DocumentSelection) {
+      return "the given target isn't a DocumentSelection";
+    }
+
+    if (target.base.nodeId != expectedSelection.base.nodeId) {
+      return "The selection doesn't start at the expected node.\nExpected: $expectedSelection\nActual: $target";
+    }
+
+    if (target.extent.nodeId != expectedSelection.extent.nodeId) {
+      return "The selection doesn't end at the expected node.\nExpected: $expectedSelection\nActual: $target";
+    }
+
+    if (!target.base.nodePosition.isEquivalentTo(expectedSelection.base.nodePosition)) {
+      // The base node positions aren't the same.
+      return 'The selection starts at the correct node, but at a wrong position.\nExpected: $expectedSelection\nActual: $target';
+    }
+
+    if (!target.extent.nodePosition.isEquivalentTo(expectedSelection.extent.nodePosition)) {
+      // The extent node positions aren't the same.
+      return 'The selection ends at the correct node, but at a wrong position.\nExpected: $expectedSelection\nActual: $target';
+    }
+
+    return null;
   }
 }
