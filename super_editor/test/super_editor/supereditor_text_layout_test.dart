5a34ed4b0a94f0e2d7e0f9a7bbf7242f0f71494e
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2024-01-18 22:53:23 -0300

    Fix tests

--- super_editor/test/super_editor/supereditor_text_layout_test.dart
+++ super_editor/test/super_editor/supereditor_text_layout_test.dart
@@ -109,7 +109,7 @@ void main() {
       _ensureOnlyExpectedNodesRebuilt(
         previousBuildCount: buildCountPerNode,
         currentBuildCount: newBuildCountPerNode,
-        expectedRebuiltNodes: {'paragraph-1', 'unordered-1'},
+        expectedRebuiltNodes: {'paragraph-1'},
       );
 
       // Update the current build count to perform the subsequent expectations.
@@ -124,7 +124,7 @@ void main() {
       _ensureOnlyExpectedNodesRebuilt(
         previousBuildCount: buildCountPerNode,
         currentBuildCount: newBuildCountPerNode,
-        expectedRebuiltNodes: {'unordered-1', 'ordered-1'},
+        expectedRebuiltNodes: {},
       );
 
       // Update the current build count to perform the subsequent expectations.
@@ -139,7 +139,7 @@ void main() {
       _ensureOnlyExpectedNodesRebuilt(
         previousBuildCount: buildCountPerNode,
         currentBuildCount: newBuildCountPerNode,
-        expectedRebuiltNodes: {'ordered-1', 'task-1'},
+        expectedRebuiltNodes: {'task-1'},
       );
     });
   });
