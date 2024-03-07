0b496a58d722ba8913e791ba984c3270288a5be3
Iiro Krankka <iiro.krankka@gmail.com>
2024-03-07 10:07:22 +0200

    Initialize the super_editor_quill project

---
+++ super_editor_quill/example/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('example app'),
+        ),
+        body: const Center(
+          child: Text('Running on'),
+        ),
+      ),
+    );
+  }
+}
