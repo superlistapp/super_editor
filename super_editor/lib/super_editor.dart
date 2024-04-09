e38c450848cb5e44ec4a16e23e5b1bf57bdd9765
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2024-04-09 17:22:18 -0300

    [SuperEditor][ExampleApp] Replace PopoverScaffold with overlord version (Resolves #1892) (#1922)

--- super_editor/lib/super_editor.dart
+++ super_editor/lib/super_editor.dart
@@ -87,9 +87,7 @@ export 'src/infrastructure/touch_controls.dart';
 export 'src/infrastructure/text_input.dart';
 export 'src/infrastructure/viewport_size_reporting.dart';
 export 'src/infrastructure/popovers.dart';
-export 'src/infrastructure/popover_scaffold.dart';
 export 'src/infrastructure/selectable_list.dart';
-export 'src/infrastructure/default_popovers.dart';
 
 // Super Reader
 export 'src/super_reader/read_only_document_android_touch_interactor.dart';
