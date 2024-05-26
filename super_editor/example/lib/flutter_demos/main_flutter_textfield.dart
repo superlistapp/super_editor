a95873b21ad0f5934764bbe7fd109a9cd40a2d98
Matt Carroll <matthew-carroll@users.noreply.github.com>
2024-05-26 01:35:10 -0700

    Use native iOS edit menu when available (Resolves #2032)

---
+++ super_editor/example/lib/flutter_demos/main_flutter_textfield.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(_FlutterTextFieldDemoApp());
+}
+
+class _FlutterTextFieldDemoApp extends StatelessWidget {
+  const _FlutterTextFieldDemoApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Center(
+            child: _DemoTextField(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _DemoTextField extends StatefulWidget {
+  const _DemoTextField();
+
+  @override
+  State<_DemoTextField> createState() => _DemoTextFieldState();
+}
+
+class _DemoTextFieldState extends State<_DemoTextField> {
+  final _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        controller: _textController,
+        decoration: InputDecoration(
+          hintText: "Enter text...",
+        ),
+        contextMenuBuilder: (BuildContext context, EditableTextState editableTextState) {
+          // return AdaptiveTextSelectionToolbar.editableText(
+          //   editableTextState: editableTextState,
+          // );
+        });
+  }
+}
