6c0224b9a549e917179a6b1e9f9405dd74a7bb2b
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2024-05-29 20:06:48 -0300

    Missing file

--- super_editor/lib/src/default_editor/text.dart
+++ super_editor/lib/src/default_editor/text.dart
@@ -1274,7 +1274,7 @@ class AddTextAttributionsCommand implements EditCommand {
               newAttribution: attribution,
               start: range.start,
               end: range.end,
-              autoMerge: autoMerge,
+              ignoreMergeableOverlaps: autoMerge,
             ),
         );
 
@@ -1557,7 +1557,7 @@ class ToggleTextAttributionsCommand implements EditCommand {
                 newAttribution: attribution,
                 start: range.start,
                 end: range.end,
-                autoMerge: true,
+                ignoreMergeableOverlaps: true,
               ),
           );
         }
