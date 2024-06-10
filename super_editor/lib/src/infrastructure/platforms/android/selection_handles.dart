055d91f3110b039d9796873a6f5bf4cf65be3c65
Matt Carroll <matthew-carroll@users.noreply.github.com>
2024-06-10 16:12:33 -0700

    Fix tests

--- super_editor/lib/src/infrastructure/platforms/android/selection_handles.dart
+++ super_editor/lib/src/infrastructure/platforms/android/selection_handles.dart
@@ -3,8 +3,19 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:super_editor/src/infrastructure/touch_controls.dart';
 
+/// An Android-style mobile selection drag handle.
+///
+/// Android renders three different types of handles: collapsed, upstream, and downstream.
+///
+/// All three types of handles look like 3/4 of a circle combined with 1/4 of a square (with
+/// a pointy corner). The primary difference between each handle appearance is the way the
+/// pointy corner is directed.
+///
+///  * Collapsed: The pointy corner points up.
+///  * Upstream: The pointy corner points to the upper right (marking the start of a selection).
+///  * Downstream: The pointy corner points to the upper left (marking the end of a selection).
 class AndroidSelectionHandle extends StatelessWidget {
-  static const defaultTouchRegionExpansion = EdgeInsets.all(16);
+  static const defaultTouchRegionExpansion = EdgeInsets.only(left: 16, right: 16, bottom: 16);
 
   const AndroidSelectionHandle({
     Key? key,
@@ -15,10 +26,26 @@ class AndroidSelectionHandle extends StatelessWidget {
     this.showDebugTouchRegion = false,
   }) : super(key: key);
 
+  /// The type of handle, e.g., collapsed, upstream, downstream.
   final HandleType handleType;
+
+  /// The color of the handle.
   final Color color;
+
+  /// The radius of the handle - each handle is essentially a circle with one pointy
+  /// corner.
   final double radius;
+
+  /// Invisible space added around the handle to increase the touch area the handle.
+  ///
+  /// This invisible area expands the intrinsic size of the handle, and therefore the
+  /// visual handle will no longer be aligned exactly with the content that's following.
+  /// The parent layout needs to adjust the positioning of the handle to account for
+  /// the [touchRegionExpansion].
   final EdgeInsets touchRegionExpansion;
+
+  /// Whether to render the [touchRegionExpansion] with a translucent color for visual
+  /// debugging.
   final bool showDebugTouchRegion;
 
   @override
