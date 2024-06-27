58467a083c30e761c5fe20f692cc273225e50026
Matej Knopp <matej.knopp@gmail.com>
2024-06-27 22:30:23 +0200

    [SuperEditor] - Ignore spurious metrics change notification (#2127)

--- super_editor/lib/src/default_editor/document_gestures_touch_ios.dart
+++ super_editor/lib/src/default_editor/document_gestures_touch_ios.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
@@ -317,6 +318,10 @@ class _IosDocumentTouchInteractorState extends State<IosDocumentTouchInteractor>
   bool get _isLongPressInProgress => _longPressStrategy != null;
   IosLongPressSelectionStrategy? _longPressStrategy;
 
+  // Cached view metrics to ignore unnecessary didChangeMetrics calls.
+  Size? _lastSize;
+  ViewPadding? _lastInsets;
+
   @override
   void initState() {
     super.initState();
@@ -343,6 +348,10 @@ class _IosDocumentTouchInteractorState extends State<IosDocumentTouchInteractor>
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
+
+    final view = View.of(context);
+    _lastSize = view.physicalSize;
+    _lastInsets = view.viewInsets;
 
     if (_controlsController != null) {
       _controlsController!.floatingCursorController.removeListener(_floatingCursorListener);
@@ -407,6 +416,21 @@ class _IosDocumentTouchInteractorState extends State<IosDocumentTouchInteractor>
 
   @override
   void didChangeMetrics() {
+    // DidChangeMetrics is sometimes called even when metrics doesn't change
+    // (i.e. on iOS with keyboard visible)
+    final view = View.of(context);
+    final size = view.physicalSize;
+    final insets = view.viewInsets;
+    if (size == _lastSize &&
+        _lastInsets?.left == insets.left &&
+        _lastInsets?.right == insets.right &&
+        _lastInsets?.top == insets.top &&
+        _lastInsets?.bottom == insets.bottom) {
+      return;
+    }
+    _lastSize = size;
+    _lastInsets = insets;
+
     // The available screen dimensions may have changed, e.g., due to keyboard
     // appearance/disappearance. Ensure the extent is still visible. Use a
     // post-frame callback to give the rest of the UI a chance to reflow, first.
