2b31b56d7845e1738280f0c3435997651c5b5d1c
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2024-07-30 21:07:35 -0300

    [SuperEditor][SuperTextField][Android] Implement selection by word using drag handle (Resolves #2112)

--- super_editor/lib/src/default_editor/text_tools.dart
+++ super_editor/lib/src/default_editor/text_tools.dart
@@ -139,6 +139,63 @@ TextSelection expandPositionToParagraph({
   );
 }
 
+/// Returns the text in the given [document] that is selected by the given [documentSelection].
+String textInSelection({
+  required Document document,
+  required DocumentSelection documentSelection,
+}) {
+  final selectedNodes = document.getNodesInside(
+    documentSelection.base,
+    documentSelection.extent,
+  );
+
+  final buffer = StringBuffer();
+  for (int i = 0; i < selectedNodes.length; ++i) {
+    final selectedNode = selectedNodes[i];
+    dynamic nodeSelection;
+
+    if (i == 0) {
+      // This is the first node and it may be partially selected.
+      final baseSelectionPosition = selectedNode.id == documentSelection.base.nodeId
+          ? documentSelection.base.nodePosition
+          : documentSelection.extent.nodePosition;
+
+      final extentSelectionPosition =
+          selectedNodes.length > 1 ? selectedNode.endPosition : documentSelection.extent.nodePosition;
+
+      nodeSelection = selectedNode.computeSelection(
+        base: baseSelectionPosition,
+        extent: extentSelectionPosition,
+      );
+    } else if (i == selectedNodes.length - 1) {
+      // This is the last node and it may be partially selected.
+      final nodePosition = selectedNode.id == documentSelection.base.nodeId
+          ? documentSelection.base.nodePosition
+          : documentSelection.extent.nodePosition;
+
+      nodeSelection = selectedNode.computeSelection(
+        base: selectedNode.beginningPosition,
+        extent: nodePosition,
+      );
+    } else {
+      // This node is fully selected. Copy the whole thing.
+      nodeSelection = selectedNode.computeSelection(
+        base: selectedNode.beginningPosition,
+        extent: selectedNode.endPosition,
+      );
+    }
+
+    final nodeContent = selectedNode.copyContent(nodeSelection);
+    if (nodeContent != null) {
+      buffer.write(nodeContent);
+      if (i < selectedNodes.length - 1) {
+        buffer.writeln();
+      }
+    }
+  }
+  return buffer.toString();
+}
+
 // copied from: flutter/lib/src/widgets/editable_text.dart
 // RTL covers Arabic, Hebrew, and other RTL languages such as Urdu,
 // Aramic, Farsi, Dhivehi.
