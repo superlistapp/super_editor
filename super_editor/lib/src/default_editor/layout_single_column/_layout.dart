4e5bd161930fb2a5d11479a101d5db5d392d53c4
Sam Rawlins <sam.rawlins@gmail.com>
2024-08-16 14:48:33 -0700

    Ignore unused_element_parameter for unused parameters (#2239)

--- super_editor/lib/src/default_editor/layout_single_column/_layout.dart
+++ super_editor/lib/src/default_editor/layout_single_column/_layout.dart
@@ -944,7 +944,10 @@ class _Component extends StatelessWidget {
     required this.componentBuilders,
     required this.componentViewModel,
     required this.componentKey,
-    // ignore: unused_element
+    // TODO(srawlins): `unused_element`, when reporting a parameter, is being
+    // renamed to `unused_element_parameter`. For now, ignore each; when the SDK
+    // constraint is >= 3.6.0, just ignore `unused_element_parameter`.
+    // ignore: unused_element, unused_element_parameter
     this.showDebugPaint = false,
   }) : super(key: key);
 
