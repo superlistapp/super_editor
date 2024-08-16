a20524a0b398717846533d81185a9599a0d311d1
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2024-08-16 18:29:03 -0300

    [SuperEditor][SuperReader][iOS] Fix horizontal drag scrolling the editor (Resolves #2081) (#2155)

---
+++ super_editor/lib/src/infrastructure/flutter/eager_pan_gesture_recognizer.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart' hide DragGestureRecognizer;
+
+import 'package:super_editor/src/infrastructure/flutter/monodrag.dart';
+
+/// Recognizes movement both horizontally and vertically.
+///
+/// Flutter's `PanGestureRecognizer` loses the gesture arena if there
+/// is a `VerticalDragGestureRecognizer` in the tree.
+///
+/// This recognizer uses the same minimum distance as the `VerticalDragGestureRecognizer`
+/// to accept a gesture
+class EagerPanGestureRecognizer extends DragGestureRecognizer {
+  EagerPanGestureRecognizer({
+    super.debugOwner,
+    super.supportedDevices,
+    super.allowedButtonsFilter,
+  });
+
+  @override
+  bool isFlingGesture(VelocityEstimate estimate, PointerDeviceKind kind) {
+    final minVelocity = minFlingVelocity ?? kMinFlingVelocity;
+    final minDistance = minFlingDistance ?? computeHitSlop(kind, gestureSettings);
+    return estimate.pixelsPerSecond.distanceSquared > minVelocity * minVelocity &&
+        estimate.offset.distanceSquared > minDistance * minDistance;
+  }
+
+  @override
+  DragEndDetails? considerFling(VelocityEstimate estimate, PointerDeviceKind kind) {
+    if (!isFlingGesture(estimate, kind)) {
+      return null;
+    }
+    final maxVelocity = maxFlingVelocity ?? kMaxFlingVelocity;
+    final dy = clampDouble(estimate.pixelsPerSecond.dy, -maxVelocity, maxVelocity);
+    return DragEndDetails(
+      velocity: Velocity(pixelsPerSecond: Offset(0, dy)),
+      primaryVelocity: dy,
+      globalPosition: finalPosition.global,
+      localPosition: finalPosition.local,
+    );
+  }
+
+  @override
+  bool hasSufficientGlobalDistanceToAccept(PointerDeviceKind pointerDeviceKind, double? deviceTouchSlop) {
+    // Flutter's PanGestureRecognizer uses the pan slop, which is twice bigger than the hit slop,
+    // to determine if the gesture should be accepted. Use the same distance used by the
+    // VerticalDragGestureRecognizer.
+    return globalDistanceMoved.abs() > computeHitSlop(pointerDeviceKind, gestureSettings);
+  }
+
+  @override
+  Offset getDeltaForDetails(Offset delta) => delta;
+
+  @override
+  double? getPrimaryValueFromOffset(Offset value) => null;
+
+  @override
+  String get debugDescription => 'pan';
+}
