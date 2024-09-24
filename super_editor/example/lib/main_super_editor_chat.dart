2eb7537020a9a9b14d5d99ffa5d40170808d7dc5
Matt Carroll <matthew-carroll@users.noreply.github.com>
2024-09-24 11:17:08 -0700

    Create standalone demo for mobile chat

---
+++ super_editor/example/lib/main_super_editor_chat.dart
@@ -0,0 +1,34 @@
+import 'package:example/demos/mobile_chat/demo_mobile_chat.dart';
+import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'package:super_editor/super_editor.dart';
+
+/// A demo of a chat experience that uses [SuperEditor].
+void main() {
+  initLoggers(Level.FINEST, {
+    // editorScrollingLog,
+    // editorGesturesLog,
+    // longPressSelectionLog,
+    // editorImeLog,
+    // editorImeDeltasLog,
+    // editorIosFloatingCursorLog,
+    // editorKeyLog,
+    // editorEditsLog,
+    // editorOpsLog,
+    // editorLayoutLog,
+    // editorDocLog,
+    // editorStyleLog,
+    // textFieldLog,
+    // editorUserTagsLog,
+    // contentLayersLog,
+  });
+
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: MobileChatDemo(),
+      ),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
