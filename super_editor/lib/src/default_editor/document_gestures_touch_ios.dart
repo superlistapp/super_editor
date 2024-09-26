4a4d5ced2e8e29af1140942d2373e0679bb388d5
Sam Rawlins <srawlins@google.com>
2024-09-26 08:45:39 -0700

    Flutter Team: Ignore unused_element_parameter for unused parameters (#2308)
    In dart-lang/sdk#49025, we are splitting the `unused_element` code, so that the
    warning reported is `unused_element_parameter`.
    
    In this change, I change each ignored case to ignore each warning name, to make
    the code forwards-compatible.

--- super_editor/lib/src/default_editor/document_gestures_touch_ios.dart
+++ super_editor/lib/src/default_editor/document_gestures_touch_ios.dart
@@ -1920,7 +1920,10 @@ class _EditorFloatingCursorState extends State<EditorFloatingCursor> {
 /// iOS floating toolbar.
 class SuperEditorIosToolbarFocalPointDocumentLayerBuilder implements SuperEditorLayerBuilder {
   const SuperEditorIosToolbarFocalPointDocumentLayerBuilder({
-    // ignore: unused_element
+    // TODO(srawlins): `unused_element`, when reporting a parameter, is being
+    // renamed to `unused_element_parameter`. For now, ignore each; when the SDK
+    // constraint is >= 3.6.0, just ignore `unused_element_parameter`.
+    // ignore: unused_element, unused_element_parameter
     this.showDebugLeaderBounds = false,
   });
 
