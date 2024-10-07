84aacd2b4a798c0523a6832b94a28846b1f2c227
Matt Carroll <matthew-carroll@users.noreply.github.com>
2024-10-06 17:00:55 -0700

    Spelling error decorations and suggestions (Partial for #388) (#2324)

--- super_editor_spellcheck/lib/super_editor_spellcheck.dart
+++ super_editor_spellcheck/lib/super_editor_spellcheck.dart
@@ -1,3 +1,7 @@
-export 'src/messages.g.dart';
-export 'src/spell_checker.dart';
-export 'src/spell_checker_mac.dart';
+export 'src/platform/messages.g.dart';
+export 'src/platform/spell_checker.dart';
+export 'src/platform/spell_checker_mac.dart';
+
+export 'src/super_editor/spelling_and_grammar_plugin.dart';
+export 'src/super_editor/spelling_error_suggestion_overlay.dart';
+export 'src/super_editor/spelling_error_suggestions.dart';
