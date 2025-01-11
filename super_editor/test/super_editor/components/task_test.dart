9b6240136b0fed0d6fd91f6035b54978d4acbe10
Matt Carroll <matthew-carroll@users.noreply.github.com>
2025-01-10 21:16:35 -0800

    PR Updates

--- super_editor/test/super_editor/components/task_test.dart
+++ super_editor/test/super_editor/components/task_test.dart
@@ -677,10 +677,6 @@ Future<Editor> _pumpScaffold(WidgetTester tester, {MutableDocument? document}) a
       home: Scaffold(
         body: SuperEditor(
           editor: editor,
-          componentBuilders: [
-            TaskComponentBuilder(editor),
-            ...defaultComponentBuilders,
-          ],
         ),
       ),
     ),
