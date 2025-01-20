f7daffef794faedd5d4b45e287fccbc259df598c
Miguel Medeiros <31278849+miguelcmedeiros@users.noreply.github.com>
2025-01-20 19:27:37 +0000

    [Infrastructure] - Export src/infrastructure/platforms/platform.dart from the test lib (#2514)

--- super_editor/lib/super_editor_test.dart
+++ super_editor/lib/super_editor_test.dart
@@ -1,5 +1,6 @@
 library super_editor_test;
 
+export 'src/infrastructure/platforms/platform.dart';
 export 'src/test/ime.dart';
 export 'src/test/super_editor_test/supereditor_inspector.dart';
 export 'src/test/super_editor_test/supereditor_robot.dart';
