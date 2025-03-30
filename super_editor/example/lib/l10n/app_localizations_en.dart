cb50a58c94337d0e5c3a496000e1aa1b0132cb04
Rutvik Tak <takrutvik@gmail.com>
2025-03-30 13:26:35 +0530

    updates

---
+++ super_editor/example/lib/l10n/app_localizations_en.dart
@@ -0,0 +1,64 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get labelTextBlockType => 'Text block type';
+
+  @override
+  String get labelBold => 'Bold';
+
+  @override
+  String get labelItalics => 'Italics';
+
+  @override
+  String get labelStrikethrough => 'Strikethrough';
+
+  @override
+  String get labelSuperscript => 'Superscript';
+
+  @override
+  String get labelSubscript => 'Subscript';
+
+  @override
+  String get labelLink => 'Link';
+
+  @override
+  String get labelTextAlignment => 'Text Alignment';
+
+  @override
+  String get labelMoreOptions => 'More Options (not implemented)';
+
+  @override
+  String get labelHeader1 => 'Header 1';
+
+  @override
+  String get labelHeader2 => 'Header 2';
+
+  @override
+  String get labelHeader3 => 'Header 3';
+
+  @override
+  String get labelParagraph => 'Paragraph';
+
+  @override
+  String get labelBlockquote => 'Blockquote';
+
+  @override
+  String get labelOrderedListItem => 'Ordered List Item';
+
+  @override
+  String get labelUnorderedListItem => 'Unordered List Item';
+
+  @override
+  String get labelLimitedWidth => 'Limited width';
+
+  @override
+  String get labelFullWidth => 'Full width';
+}
