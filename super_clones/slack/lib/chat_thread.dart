18b6cabc0ab6e5c38a3d675c4d8cc0248b60e483
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2025-05-21 21:52:46 -0300

    [Clones] Add Slack clone

---
+++ super_clones/slack/lib/chat_thread.dart
@@ -0,0 +1,329 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:slack/domain/message.dart';
+import 'package:slack/domain/user.dart';
+import 'package:slack/fake_data/fake_users.dart';
+import 'package:slack/styles.dart';
+import 'package:super_editor/super_editor.dart';
+
+/// A vertical list of messages.
+///
+/// Each message contains a user avatar, the message content and the user name.
+class ChatThread extends StatelessWidget {
+  const ChatThread({
+    super.key,
+    required this.messages,
+    required this.backgroundColor,
+    this.scrollController,
+  });
+
+  /// The messages to display.
+  final List<Message> messages;
+
+  /// The background color of each message.
+  final Color backgroundColor;
+
+  /// The `ScrollController` that controls the list scrolling.
+  final ScrollController? scrollController;
+
+  String _formatDate(DateTime date) {
+    final today = DateTime.now();
+    if (date.day == today.day && date.month == today.month && date.year == today.year) {
+      return 'Today';
+    }
+
+    // Get the full month name
+    final month = DateFormat('MMMM').format(date);
+
+    final day = date.day;
+    final suffix = _getDaySuffix(day);
+
+    return '$month $day$suffix';
+  }
+
+  String _getDaySuffix(int day) {
+    if (day >= 11 && day <= 13) {
+      return 'th';
+    }
+    switch (day % 10) {
+      case 1:
+        return 'st';
+      case 2:
+        return 'nd';
+      case 3:
+        return 'rd';
+      default:
+        return 'th';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final reversedList = messages.reversed.toList();
+    return ListView.separated(
+      controller: scrollController,
+      physics: const ClampingScrollPhysics(),
+      reverse: true,
+      itemBuilder: (context, index) => _buildMessage(reversedList, index),
+      separatorBuilder: (context, index) {
+        final message = reversedList[index];
+        final previousMessage = index < reversedList.length - 1 //
+            ? reversedList[index + 1]
+            : null;
+        return _buildSeparator(message, previousMessage);
+      },
+      itemCount: reversedList.length,
+    );
+  }
+
+  Widget _buildMessage(List<Message> messages, int index) {
+    if (index == messages.length - 1) {
+      // This is the oldest message in the list.
+      // Show the date above it.
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _buildDividerForDate(messages[index].sentAt),
+          _MessageTile(
+            messages: messages,
+            index: index,
+            backgroundColor: backgroundColor,
+          ),
+        ],
+      );
+    }
+
+    if (index == 0) {
+      // This is the newest message in the list.
+      // Add some space below it.
+      return Padding(
+        padding: const EdgeInsets.only(bottom: 20.0),
+        child: _MessageTile(
+          messages: messages,
+          index: index,
+          backgroundColor: backgroundColor,
+        ),
+      );
+    }
+
+    return _MessageTile(
+      messages: messages,
+      index: index,
+      backgroundColor: backgroundColor,
+    );
+  }
+
+  Widget _buildSeparator(Message message, Message? nextMessage) {
+    if (nextMessage == null) {
+      // This is the last message in the list.
+      // Don't show the separator.
+      return const SizedBox(height: 0);
+    }
+    if (message.userId == nextMessage.userId && _isSameDate(message.sentAt, nextMessage.sentAt)) {
+      // The message is from the same user and date as the previous message.
+      // Don't show the separator and don't add more space between messages.
+      return const SizedBox(height: 0);
+    }
+    if (message.userId != nextMessage.userId && _isSameDate(message.sentAt, nextMessage.sentAt)) {
+      // The message is on the same day as the previous message.
+      // Don't show the separator.
+      return const SizedBox(height: 5);
+    }
+    return _buildDividerForDate(message.sentAt);
+  }
+
+  Widget _buildDividerForDate(DateTime date) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: const Divider(color: dividerColor),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: dividerColor,
+                width: 1,
+              ),
+            ),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 8.0,
+              vertical: 4.0,
+            ),
+            child: Text(_formatDate(date)),
+          ),
+          Expanded(
+            child: const Divider(color: dividerColor),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/// Displays a single message from a list.
+///
+/// Shows the user avatar, the message content and the user's name.
+class _MessageTile extends StatefulWidget {
+  const _MessageTile({
+    required this.messages,
+    required this.index,
+    required this.backgroundColor,
+  });
+
+  /// List containing all messages.
+  ///
+  /// We take the whole list instead of a single message because
+  /// render the message differently depending on the previous message.
+  final List<Message> messages;
+
+  /// The index of the message to be displayed.
+  final int index;
+
+  final Color backgroundColor;
+
+  @override
+  State<_MessageTile> createState() => _MessageTileState();
+}
+
+class _MessageTileState extends State<_MessageTile> {
+  final ValueNotifier<DocumentSelection?> _selection = ValueNotifier(null);
+  final GlobalKey _documentLayoutKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    final message = widget.messages[widget.index];
+    // The list is reversed, so the previous message is at index + 1.
+    final previousMessage = widget.index < widget.messages.length - 1 //
+        ? widget.messages[widget.index + 1]
+        : null;
+
+    final user = fakeUsers.where((e) => e.id == message.userId).first;
+    final sameUserAsLastMessage = previousMessage != null && previousMessage.userId == message.userId;
+    final isSameDate = _isSameDate(message.sentAt, previousMessage?.sentAt ?? DateTime.now());
+
+    final shouldDisplayAvatar = !sameUserAsLastMessage || !isSameDate;
+
+    return ColoredBox(
+      color: widget.backgroundColor,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16.0, right: 8.0, top: 2, bottom: 2),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            shouldDisplayAvatar
+                ? _buildUserAvatar(user.avatarUrl)
+                : const SizedBox(
+                    width: 46,
+                    height: 0,
+                  ),
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (shouldDisplayAvatar) _buildMessageHeader(message, user),
+                  _buildMessageContent(message),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserAvatar(String avatarUrl) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0, right: 10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(6),
+        child: Image.network(
+          avatarUrl,
+          height: 36,
+          width: 36,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMessageHeader(Message message, User user) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, top: 5.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            user.displayName,
+            selectionColor: Colors.blue,
+            style: const TextStyle(
+              fontSize: 15,
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(width: 15),
+          _buildMessageTime(message.sentAt),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMessageContent(Message message) {
+    return IntrinsicWidth(
+      child: IgnorePointer(
+        child: SuperEditorDryLayout(
+          superEditor: SuperReader(
+            document: message.content,
+            selection: _selection,
+            documentLayoutKey: _documentLayoutKey,
+            stylesheet: defaultStylesheet.copyWith(
+              documentPadding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 0.0,
+              ),
+              selectedTextColorStrategy: makeSelectedTextBlack,
+              addRulesAfter: messageListStyles,
+              inlineTextStyler: _inlineStyler,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMessageTime(DateTime dateTime) {
+    return Text(
+      DateFormat('h:mm a').format(dateTime),
+      style: const TextStyle(
+        fontSize: 14,
+        color: Colors.white54,
+      ),
+    );
+  }
+
+  TextStyle _inlineStyler(Set<Attribution> attributions, TextStyle existingStyle) {
+    TextStyle style = defaultInlineTextStyler(attributions, existingStyle);
+
+    if (attributions.contains(stableTagComposingAttribution)) {
+      style = style.copyWith(
+        color: Colors.blue,
+      );
+    }
+
+    if (attributions.whereType<CommittedStableTagAttribution>().isNotEmpty) {
+      style = style.copyWith(
+        color: Colors.orange,
+      );
+    }
+
+    return style;
+  }
+}
+
+bool _isSameDate(DateTime date1, DateTime date2) {
+  return date1.year == date2.year && date1.month == date2.month && date1.day == date2.day;
+}
--- super_clones/slack/lib/main.dart
+++ super_clones/slack/lib/main.dart
@@ -1,125 +1,289 @@
 import 'package:flutter/material.dart';
+import 'package:slack/domain/message.dart';
+import 'package:slack/domain/user.dart';
+import 'package:slack/fake_data/fake_messages.dart';
+import 'package:slack/fake_data/fake_users.dart';
+import 'package:slack/chat_thread.dart';
+import 'package:slack/mobile_message_editor.dart';
+import 'package:slack/styles.dart';
+import 'package:super_editor/super_editor.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: SlackChatPage(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+/// A Slack-like UI containing a list of messages and a message editor.
+///
+/// Messages are loaded from markdown and can contain rich content, like list items,
+/// headers, images, user mentions, etc.
+class SlackChatPage extends StatefulWidget {
+  const SlackChatPage({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<SlackChatPage> createState() => _SlackChatPageState();
+}
+
+class _SlackChatPageState extends State<SlackChatPage> {
+  final ScrollController _messageListScrollController = ScrollController();
+  final GlobalKey _scaffoldKey = GlobalKey();
+
+  final User _user = fakeUsers[1];
+  final List<Message> _messages = [...fakeMessages];
+
+  @override
+  void dispose() {
+    _messageListScrollController.dispose();
+    super.dispose();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  void _onSendMessage(Document document) {
+    if (document.length == 1 &&
+        document.first is ParagraphNode &&
+        (document.first as ParagraphNode).text.toPlainText().trim().isEmpty) {
+      // The message editor is empty. Fizzle.
+      return;
+    }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _messages.add(Message(
+        userId: '1',
+        sentAt: DateTime.now(),
+        content: document,
+      ));
+    });
+
+    // Scroll the message list to display the newly added message.
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      if (!mounted) {
+        return;
+      }
+
+      _messageListScrollController.animateTo(
+        _messageListScrollController.position.minScrollExtent,
+        // ^ minScrollExtent because the list is reversed.
+        duration: const Duration(milliseconds: 200),
+        curve: Curves.easeInOut,
+      );
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      key: _scaffoldKey,
+      backgroundColor: backgroundColor,
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        bottom: false,
+        // ^ Don't add padding at the bottom of the screen because
+        // we handle it ourselves.
+        child: DefaultTextStyle(
+          style: const TextStyle(color: Colors.white),
+          child: _buildBody(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildBody() {
+    // TODO: implement desktop screen.
+    return _SlackChatMobile(
+      scaffoldKey: _scaffoldKey,
+      onSendMessage: _onSendMessage,
+      user: _user,
+      messages: _messages,
+      scrollController: _messageListScrollController,
+    );
+  }
+}
+
+class _SlackChatMobile extends StatefulWidget {
+  const _SlackChatMobile({
+    required this.scaffoldKey,
+    required this.onSendMessage,
+    required this.user,
+    required this.messages,
+    required this.scrollController,
+  });
+
+  final GlobalKey scaffoldKey;
+  final OnSendMessage onSendMessage;
+  final User user;
+  final List<Message> messages;
+  final ScrollController scrollController;
+
+  @override
+  State<_SlackChatMobile> createState() => _SlackChatMobileState();
+}
+
+class _SlackChatMobileState extends State<_SlackChatMobile> {
+  final _messagePageController = MessagePageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MessagePageScaffold(
+      controller: _messagePageController,
+      bottomSheetMinimumTopGap: 150,
+      bottomSheetMinimumHeight: 120,
+      contentBuilder: (contentContext, bottomSpacing) {
+        return MediaQuery.removePadding(
+          context: contentContext,
+          removeBottom: true,
+          // ^ Remove bottom padding because if we don't, when the keyboard
+          //   opens to edit the bottom sheet, this content behind the bottom
+          //   sheet adds some phantom space at the bottom, slightly pushing
+          //   it up for no reason.
+          child: Stack(
+            children: [
+              Positioned(
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: bottomSpacing,
+                child: _buildChatThread(),
+              ),
+            ],
+          ),
+        );
+      },
+      bottomSheetBuilder: (messageContext) {
+        return _buildBottomSheet();
+      },
+    );
+  }
+
+  Widget _buildChatThread() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        _buildTopSection(),
+        Divider(color: dividerColor),
+        Expanded(
+          child: ChatThread(
+            messages: widget.messages,
+            backgroundColor: backgroundColor,
+            scrollController: widget.scrollController,
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// Builds the top section of the chat, containing the back button, the user avatar with the
+  /// user's name, and the headphones icon.
+  Widget _buildTopSection() {
+    return SizedBox(
+      height: 60,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Container(
+                height: 50,
+                padding: EdgeInsets.all(2.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF21252A),
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+                child: Row(
+                  children: [
+                    Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image.network(
+                            widget.user.avatarUrl,
+                            fit: BoxFit.cover,
+                            height: 50,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: -1,
+                          right: -4,
+                          child: Container(
+                            height: 18,
+                            width: 18,
+                            padding: EdgeInsets.all(3.0),
+                            decoration: BoxDecoration(
+                              color: Color(0xFF21252A),
+                              shape: BoxShape.circle,
+                            ),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Color(0xFF3CAA7B),
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(width: 8),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      spacing: 2,
+                      children: [
+                        Text(
+                          widget.user.displayName,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              '3 tabs',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Transform.rotate(
+                              angle: 270 * 3.14 / 180,
+                              child: Icon(
+                                Icons.chevron_left,
+                                size: 19,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.headphones_outlined),
+            color: Colors.white,
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBottomSheet() {
+    return MobileMessageEditor(
+      hintText: 'Message ${widget.user.displayName}',
+      messagePageController: _messagePageController,
+      onSendMessage: widget.onSendMessage,
     );
   }
 }
