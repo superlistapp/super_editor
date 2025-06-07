adb4b7cc08cfe69fe117ab3b5259fc4412c942f5
Matt Carroll <matthew-carroll@users.noreply.github.com>
2025-06-06 21:19:45 -0700

    [Spellcheck] - Finish missed work on spellcheck delay (Resolves #2651) (#2701)

--- super_editor_spellcheck/lib/super_editor_spellcheck.dart
+++ super_editor_spellcheck/lib/super_editor_spellcheck.dart
@@ -2,6 +2,8 @@ export 'src/platform/messages.g.dart';
 export 'src/platform/spell_checker.dart';
 export 'src/platform/spell_checker_mac.dart';
 
+export 'src/super_editor/spell_checker_popover_controller.dart';
+export 'src/super_editor/spellcheck_clock.dart';
 export 'src/super_editor/spelling_and_grammar_plugin.dart';
 export 'src/super_editor/spelling_error_suggestion_overlay.dart';
 export 'src/super_editor/spelling_error_suggestions.dart';
